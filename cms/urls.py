103baff84b11b47aee740eef83d24a65aa1eaf75
zerolab <dan@zerolab.org>
2024-10-18 15:57:30 +0100

    Use the alpha pylint config

--- cms/urls.py
+++ cms/urls.py
@@ -63,7 +63,7 @@ if settings.DEBUG:
 
     # Try to install the django debug toolbar, if exists
     if apps.is_installed("debug_toolbar"):
-        import debug_toolbar
+        import debug_toolbar  # pylint: disable=import-error
 
         debug_urlpatterns = [path("__debug__/", include(debug_toolbar.urls)), *debug_urlpatterns]
 
