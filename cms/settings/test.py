b4b59d0cab4134b07ea4cbb0bf54d7ad3cc00f39
Jake Howard <jake.howard@torchbox.com>
2024-11-01 13:16:39 +0000

    Read replica should be a mirror of the default DB
    https://docs.djangoproject.com/en/5.1/topics/testing/advanced/#tests-and-multiple-databases

--- cms/settings/test.py
+++ cms/settings/test.py
@@ -42,6 +42,6 @@ DEFENDER_DISABLE_USERNAME_LOCKOUT = True
 DEFENDER_DISABLE_IP_LOCKOUT = True
 
 
-# Set a different test database name for the replica, in case it's the same
-# as the default.
-DATABASES["read_replica"].setdefault("TEST", {"NAME": "test_read_replica"})  # noqa: F405
+# Read replica should mirror the default database during tests.
+# https://docs.djangoproject.com/en/stable/topics/testing/advanced/#tests-and-multiple-databases
+DATABASES["read_replica"].setdefault("TEST", {"MIRROR": "default"})  # noqa: F405
