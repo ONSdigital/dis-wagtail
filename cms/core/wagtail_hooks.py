0355301ed7cfc11097f41d24ba5c974e5b8cd0c5
zerolab <dan@zerolab.org>
2024-11-04 17:33:49 +0000

    Use a custom icon for the contact detail snippet

--- cms/core/wagtail_hooks.py
+++ cms/core/wagtail_hooks.py
@@ -1,4 +1,8 @@
 from wagtail import hooks
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet
+
+from cms.core.models import ContactDetails
 
 
 @hooks.register("register_icons")
@@ -11,6 +15,21 @@ def register_icons(icons: list[str]) -> list[str]:
     return [
         *icons,
         "data-analysis.svg",
+        "identity.svg",
         "news.svg",
-        "pass.svg",
     ]
+
+
+class ContactDetailsViewSet(SnippetViewSet):
+    """A snippet viewset for ContactDetails.
+
+    See:
+     - https://docs.wagtail.org/en/stable/topics/snippets/registering.html
+     - https://docs.wagtail.org/en/stable/topics/snippets/customizing.html#icon
+    """
+
+    model = ContactDetails
+    icon = "identity"
+
+
+register_snippet(ContactDetailsViewSet)
