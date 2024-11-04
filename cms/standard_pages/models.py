d4a31ec7d8b7f984bc6a6131f6f279e5710ab834
nehakerung <nehakerung@gmail.com>
2024-11-04 13:32:22 +0000

    Addition of InformationPage class in models

--- cms/standard_pages/models.py
+++ cms/standard_pages/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.db.models import DateField
+from openpyxl.drawing.text import TextField
+from wagtail.admin.panels import FieldPanel
+from wagtail.blocks import RichTextBlock
+from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.models import Page
 
-# Create your models here.
+from cms.core.blocks import ONSEmbedBlock, RelatedLinksBlock
+from cms.core.fields import StreamField
+
+
+class InformationPage(Page):
+
+    description = TextField(max_lenth=255)
+    last_updated = DateField(blank=True)
+    content = StreamField(
+        [
+            ('Heading', RichTextBlock()),
+            ('Paragraph', RichTextBlock()),
+            ('Image', ImageChooserBlock()),
+            ('Document', DocumentChooserBlock()),
+            ('Embed', ONSEmbedBlock()),#ONSEmbedBlock
+            ('Related_links', RelatedLinksBlock()),
+        ]
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('description'),
+        FieldPanel('last_updated'),
+        FieldPanel('content')
+    ]
