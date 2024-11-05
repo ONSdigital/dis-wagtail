be34d8d66112f5be02bfefa1fbbe04182a1b7ac2
nehakerung <nehakerung@gmail.com>
2024-11-05 09:08:48 +0000

    Updated apps.py for standard_pages to be similar to pre-existing apps

--- cms/standard_pages/apps.py
+++ cms/standard_pages/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class StandardPagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'standard_pages'
+    """The standard_pages app config."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cms.standard_pages"
