35ff3cbbef72614c9fbd33978dadd859e26cd3de
Kacper Prywata <kacper.prywata@ons.gov.uk>
2024-11-05 17:19:35 +0000

    Checkpoint before migration

--- cms/core/tests/test_context_processors.py
+++ cms/core/tests/test_context_processors.py
@@ -1,29 +1,41 @@
+import os
+
 from django.test import RequestFactory, TestCase
-from wagtail.models import Site
 
 from cms.core.context_processors import global_vars
-from cms.core.models import Tracking
 
 
 class ContextProcessorTestCase(TestCase):
     """Tests for context processors."""
+
     def setUp(self):
         request_factory = RequestFactory()
 
         # Request is created with each test to avoid mutation side-effects
         self.request = request_factory.get("/")
 
-    def test_when_no_tracking_settings_defined(self):
-        """Check the global vars include sensible defaults when no Tracking settings defined."""
-        self.assertEqual(global_vars(self.request)["GOOGLE_TAG_MANAGER_ID"], "")
-
-
-    def test_when_tracking_settings_defined(self):
-        """Confirm the global vars include Tracking settings when defined."""
-        Tracking.objects.create(
-            site=Site.objects.get(is_default_site=True),
-            google_tag_manager_id="GTM-123456",
+    def test_defaults_when_no_environment_settings_defined(self):
+        """Check the global vars include sensible defaults when no environment settings defined."""
+        self.assertEqual(
+            global_vars(self.request),
+            {
+                "GOOGLE_TAG_MANAGER_ID": "",
+                "SEO_NOINDEX": False,
+                "LANGUAGE_CODE": "en-gb",
+                "IS_EXTERNAL_ENV": False,
+            },
         )
-        self.assertEqual(global_vars(self.request)["GOOGLE_TAG_MANAGER_ID"], "GTM-123456")
 
+    def test_custom_values_when_environment_settings_defined(self):
+        """Confirm the global vars include environment settings when defined."""
+        os.environ["GOOGLE_TAG_MANAGER_ID"] = "GTM-123456"
 
+        self.assertEqual(
+            global_vars(self.request),
+            {
+                "GOOGLE_TAG_MANAGER_ID": "GTM-123456",
+                "SEO_NOINDEX": False,
+                "LANGUAGE_CODE": "en-gb",
+                "IS_EXTERNAL_ENV": False,
+            },
+        )
