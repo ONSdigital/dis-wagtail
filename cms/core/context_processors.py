b7bf58f8c21893284c6535e16e86347dbf6230d9
Kacper Prywata <kacper.prywata@ons.gov.uk>
2024-11-05 16:17:05 +0000

    Formatting

--- cms/core/context_processors.py
+++ cms/core/context_processors.py
@@ -13,11 +13,11 @@ def global_vars(request: "HttpRequest") -> dict[str, str | bool | None]:
     tracking = Tracking.for_request(request)
     return {
         "GOOGLE_TAG_MANAGER_CONTAINER_ID": settings.GOOGLE_TAG_MANAGER_CONTAINER_ID,
-        
+
         # Cookie banner settings
         "ONS_COOKIE_BANNER_SERVICE_NAME": settings.ONS_COOKIE_BANNER_SERVICE_NAME,
         "MANAGE_COOKIE_SETTINGS_URL": settings.MANAGE_COOKIE_SETTINGS_URL,
-        
+
         "SEO_NOINDEX": settings.SEO_NOINDEX,
         "LANGUAGE_CODE": settings.LANGUAGE_CODE,
         "IS_EXTERNAL_ENV": settings.IS_EXTERNAL_ENV,
