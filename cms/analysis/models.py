83dbf1f0709bdb8ce5d561c08d67c241af38049b
zerolab <dan@zerolab.org>
2024-11-05 15:20:43 +0000

    Wire in the analysis page template

--- cms/analysis/models.py
+++ cms/analysis/models.py
@@ -1,4 +1,5 @@
-from typing import TYPE_CHECKING, ClassVar
+from functools import cached_property
+from typing import TYPE_CHECKING, Any, ClassVar
 
 from django.conf import settings
 from django.db import models
@@ -6,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 from wagtail.admin.panels import FieldPanel, FieldRowPanel, MultiFieldPanel, TitleFieldPanel
 from wagtail.fields import RichTextField
 from wagtail.models import Page
+from wagtail.search import index
 
 from cms.analysis.blocks import AnalysisStoryBlock
 from cms.core.blocks import HeadlineFiguresBlock
@@ -13,6 +15,7 @@ from cms.core.fields import StreamField
 from cms.core.models import BasePage
 
 if TYPE_CHECKING:
+    from django.http import HttpRequest
     from wagtail.admin.panels import Panel
 
 
@@ -109,3 +112,58 @@ class AnalysisPage(BasePage):  # type: ignore[django-manager-missing]
         FieldPanel("headline_figures", icon="data-analysis"),
         FieldPanel("content", icon="list-ul"),
     ]
+
+    search_fields: ClassVar[list[index.SearchField | index.AutocompleteField]] = [
+        *BasePage.search_fields,
+        index.SearchField("summary"),
+        index.SearchField("headline_figures"),
+        index.SearchField("content"),
+        index.SearchField("get_admin_display_title", boost=2),
+        index.AutocompleteField("get_admin_display_title"),
+        index.SearchField("news_headline"),
+        index.AutocompleteField("news_headline"),
+    ]
+
+    def get_context(self, request: "HttpRequest", *args: Any, **kwargs: Any) -> dict:
+        """Additional context for the template."""
+        context: dict = super().get_context(request, *args, **kwargs)
+        context["table_of_contents"] = self.table_of_contents
+        return context
+
+    def get_admin_display_title(self) -> str:
+        """Changes the admin display title to include the parent title."""
+        return f"{self.get_parent().title}: {self.draft_title or self.title}"
+
+    @property
+    def display_title(self) -> str:
+        """Returns the page display title. If the news headline is set, it takes precedence over the series+title."""
+        return self.news_headline.strip() or self.get_admin_display_title()
+
+    @cached_property
+    def table_of_contents(self) -> list[dict[str, str | object]]:
+        """Table of contents formatted to Design System specs."""
+        items = []
+        for block in self.content:  # pylint: disable=not-an-iterable,useless-suppression
+            if hasattr(block.block, "to_table_of_contents_items"):
+                items += block.block.to_table_of_contents_items(block.value)
+        if self.show_cite_this_page:
+            items += [{"url": "#cite-this-page", "text": _("Cite this analysis")}]
+        if self.contact_details_id:
+            items += [{"url": "#contact-details", "text": _("Contact details")}]
+        return items
+
+    @cached_property
+    def has_equations(self) -> bool:
+        """Checks if there are any equation blocks."""
+        return any(
+            block.value["content"].first_block_by_name("equation") is not None
+            for block in self.content  # pylint: disable=not-an-iterable
+        )
+
+    @cached_property
+    def has_ons_embed(self) -> bool:
+        """Checks if there are any ONS embed blocks."""
+        return any(
+            block.value["content"].first_block_by_name("ons_embed") is not None
+            for block in self.content  # pylint: disable=not-an-iterable
+        )
