eda9da9b5cd5231cad3dcc96f854da99d8d3844c
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-11-07 15:47:12 +0000

    TODO comment removed and docstrings added

--- cms/standard_pages/models.py
+++ cms/standard_pages/models.py
@@ -9,6 +9,16 @@ from cms.core.models import BasePage
 
 
 class InformationPage(BasePage):  # type: ignore[django-manager-missing]
+    """A Django model representing an information page.
+
+    Attributes:
+        template (str): The path to the template used to render this page.
+        summary (TextField): A brief summary of the page content, with a maximum length of 255 characters.
+        last_updated (DateField): The date when the page was last updated.
+        body (StreamField): The main content of the page, using a custom StreamField block.
+        content_panels (list[FieldPanel]): The list of content panels for the Wagtail admin interface.
+    """
+
     template = "templates/pages/information_page.html"
 
     summary = models.TextField(max_length=255, null=True)
