6d8b2d393a5feffd1dd5e35f609d34d3c1b58838
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-11-07 14:12:53 +0000

    Django models for information page updated and the template for the information page tweaked

--- cms/standard_pages/models.py
+++ cms/standard_pages/models.py
@@ -1,51 +1,31 @@
+from typing import ClassVar
+
 from django.db import models
 from wagtail.admin.panels import FieldPanel
-from wagtail.blocks import RichTextBlock, URLBlock
-from wagtail.documents.blocks import DocumentChooserBlock
-from wagtail.images.blocks import ImageChooserBlock
+
+# from cms.core.models import BasePage
+from wagtail.fields import StreamField
 from wagtail.models import Page
 
-from cms.core.blocks import (
-    ONSEmbedBlock,
-    RelatedLinksBlock,
-    RelatedContentBlock,
-)
-
-#could use...
 from cms.core.blocks.stream_blocks import CoreStoryBlock
 
-from cms.core.fields import StreamField
 
 class InformationPage(Page):
-
     template = "templates/pages/information_page.html"
 
-    description = models.TextField(max_length=255)
+    description = models.TextField(max_length=255, null=True)
     last_updated = models.DateField(blank=True, null=True)
-    content = StreamField([
-            ('heading', RichTextBlock()),
-            ('paragraph', RichTextBlock()),
-            ('image', ImageChooserBlock()),
-            ('document', DocumentChooserBlock()),
-            ('embed', ONSEmbedBlock()),
-            ('related_links', RelatedLinksBlock(RelatedContentBlock())),
-        ],
-        blank=True,
-        null=True,
-    )
+    body = StreamField(CoreStoryBlock(), use_json_field=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('description'),
-        FieldPanel('last_updated'),
-        FieldPanel('content')
+    # content_panels = Page.content_panels + [
+    #     FieldPanel("description"),
+    #     FieldPanel("last_updated"),
+    #     FieldPanel("body"),
+    # ]
+
+    content_panels: ClassVar[list[FieldPanel]] = [
+        *Page.content_panels,
+        FieldPanel("description"),
+        FieldPanel("last_updated"),
+        FieldPanel("body"),
     ]
-
-    def get_context(self, request, *args, **kwargs):
-        context = super().get_context(request, *args, **kwargs)
-
-        list =[]
-        for block in self.content:
-            if block.block_type == 'related_links':
-                list.extend(block.value)
-        context['links_list'] = list
-        return context
