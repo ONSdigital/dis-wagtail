4fac1fbb48e8205eff837618f85f3144bc1ab47c
kacperpONS <113175775+kacperpONS@users.noreply.github.com>
2024-11-12 14:21:44 +0000

    Add Cookie banner and Google Tag Manager snippet (#19)

--- cms/core/tests/test_views.py
+++ cms/core/tests/test_views.py
@@ -1,10 +1,12 @@
 import logging
 
+from django.conf import settings
 from django.test import Client, TestCase
 
 
 class CSRFTestCase(TestCase):
     """Tests for CSRF enforcement."""
+
     def setUp(self):
         # Client is created with each test to avoid mutation side-effects
         self.client = Client(enforce_csrf_checks=True)
@@ -17,3 +19,16 @@ class CSRFTestCase(TestCase):
             self.client.post("/admin/login/", {})
 
         self.assertIn("CSRF Failure: CSRF cookie", logs.output[0])
+
+
+class TestGoogleTagManagerTestCase(TestCase):
+    """Tests for the Google Tag Manager."""
+
+    def test_google_tag_manager_script_present(self):
+        """Check that the Google Tag Manager script and ID are present on the page.
+        Note: this doesn't check the functionality, only presence.
+        """
+        response = self.client.get("/")
+
+        self.assertIn("https://www.googletagmanager.com/gtm.js?id=", response.rendered_content)
+        self.assertIn(settings.GOOGLE_TAG_MANAGER_CONTAINER_ID, response.rendered_content)
--- cms/settings/base.py
+++ cms/settings/base.py
@@ -736,8 +736,7 @@ WAGTAIL_PASSWORD_REQUIRED_TEMPLATE = "templates/pages/wagtail/password_required.
 DEFAULT_PER_PAGE = 20
 
 # Google Tag Manager ID from env
-GOOGLE_TAG_MANAGER_ID = env.get("GOOGLE_TAG_MANAGER_ID")
-
+GOOGLE_TAG_MANAGER_CONTAINER_ID = env.get("GOOGLE_TAG_MANAGER_CONTAINER_ID", "")
 
 # Allows us to toggle search indexing via an environment variable.
 SEO_NOINDEX = env.get("SEO_NOINDEX", "false").lower() == "true"
@@ -778,3 +777,7 @@ DATE_FORMAT = "j F Y"
 DATETIME_FORMAT = "j F Y g:ia"  # 1 November 2024, 1 p.m.
 
 ONS_EMBED_PREFIX = env.get("ONS_EMBED_PREFIX", "https://www.ons.gov.uk/visualisations/")
+
+# ONS Cookie banner settings
+ONS_COOKIE_BANNER_SERVICE_NAME = env.get("ONS_COOKIE_BANNER_SERVICE_NAME", "www.ons.gov.uk")
+MANAGE_COOKIE_SETTINGS_URL = env.get("MANAGE_COOKIE_SETTINGS_URL", "https://www.ons.gov.uk/cookies")
