dd084aef0cbd36c686dfd61390305e8244da1f6c
Jake Howard <jake.howard@torchbox.com>
2024-11-20 12:03:42 +0000

    Rewrite tests using Django's test runner

--- cms/core/tests/test_db_router.py
+++ cms/core/tests/test_db_router.py
@@ -1,54 +1,51 @@
-import pytest
 from django.contrib.contenttypes.models import ContentType
 from django.db import router, transaction
+from django.test import TransactionTestCase
 from wagtail.models import Page
 
-pytestmark = pytest.mark.django_db(transaction=True)
 
+class DBRouterTestCase(TransactionTestCase):
+    databases = frozenset({"default", "read_replica"})
 
-def test_uses_replica_for_read():
-    """Check the read replica is used for reads, and the default for writes."""
-    assert router.db_for_write(Page) == "default"
-    assert router.db_for_read(Page) == "read_replica"
+    def test_uses_replica_for_read(self):
+        """Check the read replica is used for reads, and the default for writes."""
+        self.assertEqual(router.db_for_write(Page), "default")
+        self.assertEqual(router.db_for_read(Page), "read_replica")
 
+    def test_uses_write_db_during_transaction(self):
+        """Check the default is used for reads in a transaction."""
+        self.assertEqual(router.db_for_read(Page), "read_replica")
 
-def test_uses_write_db_during_transaction():
-    """Check the default is used for reads in a transaction."""
-    assert router.db_for_read(Page) == "read_replica"
+        with transaction.atomic():
+            self.assertEqual(router.db_for_read(Page), "default")
 
-    with transaction.atomic():
-        assert router.db_for_read(Page) == "default"
+        self.assertEqual(router.db_for_read(Page), "read_replica")
 
-    assert router.db_for_read(Page) == "read_replica"
+    def test_uses_write_db_when_autocommit_disabled(self):
+        """Check the default is used for reads when not in an autocommit context."""
+        self.assertEqual(router.db_for_read(Page), "read_replica")
 
+        try:
+            transaction.set_autocommit(False)
+            self.assertEqual(router.db_for_read(Page), "default")
+        finally:
+            transaction.set_autocommit(True)
 
-def test_uses_write_db_when_autocommit_disabled():
-    """Check the default is used for reads when not in an autocommit context."""
-    assert router.db_for_read(Page) == "read_replica"
+        self.assertEqual(router.db_for_read(Page), "read_replica")
 
-    try:
-        transaction.set_autocommit(False)
-        assert router.db_for_read(Page) == "default"
-    finally:
-        transaction.set_autocommit(True)
-
-    assert router.db_for_read(Page) == "read_replica"
-
-
-def test_uses_correct_db_in_query():
-    """Check the read replica is used when running an actual query."""
-    # Choose a model which definitely exists
-    content_type = ContentType.objects.first()
-
-    assert content_type is not None
-    assert content_type._state.db == "read_replica"  # pylint: disable=protected-access
-
-
-def test_uses_correct_db_in_transaction():
-    """Check the read replica is used when running a query inside a transaction."""
-    # Choose a model which definitely exists
-    with transaction.atomic():
+    def test_uses_correct_db_in_query(self):
+        """Check the read replica is used when running an actual query."""
+        # Choose a model which definitely exists
         content_type = ContentType.objects.first()
 
-    assert content_type is not None
-    assert content_type._state.db == "default"  # pylint: disable=protected-access
+        self.assertIsNotNone(content_type)
+        self.assertEqual(content_type._state.db, "read_replica")  # pylint: disable=protected-access
+
+    def test_uses_correct_db_in_transaction(self):
+        """Check the read replica is used when running a query inside a transaction."""
+        # Choose a model which definitely exists
+        with transaction.atomic():
+            content_type = ContentType.objects.first()
+
+        self.assertIsNotNone(content_type)
+        self.assertEqual(content_type._state.db, "default")  # pylint: disable=protected-access
