2b152c17e236e3b657de2c5f8431cbfc49d2bd52
AWitcherONS <ann.witcher@ons.gov.uk>
2024-11-22 12:23:09 +0000

    add lint changes v1

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   // See https://github.com/wagtail/stylelint-config-wagtail for rules.
   extends: '@wagtail/stylelint-config-wagtail',
+
+    "rules": {
+      "indentation": 2
+    }
+
 };
