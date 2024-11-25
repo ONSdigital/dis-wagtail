9ffc81214c4daee45fb3c1285f8cf102c9795892
Jake Howard <jake.howard@torchbox.com>
2024-11-25 11:40:08 +0000

    Add tests for IAM credentials provider

---
+++ cms/core/tests/test_cache.py
@@ -0,0 +1,53 @@
+import time
+from urllib.parse import parse_qs
+
+from django.core.cache import caches
+from django.test import SimpleTestCase
+from moto import mock_aws
+
+from cms.core.cache import ElastiCacheIAMCredentialProvider
+
+
+@mock_aws
+class ElastiCacheIAMCredentialProviderTestCase(SimpleTestCase):
+    def setUp(self):
+        caches["memory"].clear()
+
+    def test_credentials(self):
+        """Test generated credentials."""
+        provider = ElastiCacheIAMCredentialProvider("user", "cluster", "eu-west-2")
+
+        user, signed_url = provider.get_credentials()
+
+        self.assertEqual(user, provider.user)
+        self.assertTrue(signed_url.startswith("cluster"))
+
+        querystring = parse_qs(signed_url.split("/", 1)[1][1:])
+
+        self.assertEqual(querystring["X-Amz-Expires"], [str(provider.TOKEN_TTL)])
+        self.assertEqual(querystring["Action"], ["connect"])
+        self.assertEqual(querystring["User"], [provider.user])
+
+    def test_cache(self):
+        """Test cache is used."""
+        provider = ElastiCacheIAMCredentialProvider("user", "cluster", "eu-west-2")
+
+        self.assertFalse(caches["memory"].has_key(provider.cache_key))
+
+        credentials = provider.get_credentials()
+
+        self.assertEqual(caches["memory"].get(provider.cache_key), credentials[1])
+
+        # The credentials should be the same, as they're read from a cache
+        self.assertEqual(provider.get_credentials(), credentials)
+
+        # Wait until the next second for the timestamp to change, so the signature is definitely different
+        time.sleep(1 - time.time() % 1)
+
+        # The credentials should still be the same
+        self.assertEqual(provider.get_credentials(), credentials)
+
+        caches["memory"].delete(provider.cache_key)
+
+        # After deleting the cache, the signature should be different
+        self.assertNotEqual(provider.get_credentials(), credentials)
