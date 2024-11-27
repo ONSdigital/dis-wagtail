97e0605af9d856fdfe1a3dfe751d76a2e028a731
Andy Babic <andy.babic@torchbox.com>
2024-11-27 08:12:26 +0000

    Use 'before_serve_document' hook to enforce permission checking for private documents

--- cms/private_media/wagtail_hooks.py
+++ cms/private_media/wagtail_hooks.py
@@ -1,11 +1,16 @@
 from typing import TYPE_CHECKING
 
+from django.core.exceptions import PermissionDenied
 from django.templatetags.static import static
 from django.utils.html import format_html
 from wagtail import hooks
+from wagtail.documents.permissions import permission_policy
 
 if TYPE_CHECKING:
+    from django.http import HttpRequest
     from django.utils.safestring import SafeString
+
+    from cms.private_media.models import AbstractPrivateDocument
 
 
 @hooks.register("insert_editor_js")
@@ -22,3 +27,15 @@ def load_chooser_modal_override_js() -> "SafeString":
         static("private_media/js/image-modal-overrides.js"),
         static("private_media/js/document-modal-overrides.js"),
     )
+
+
+@hooks.register("before_serve_document")
+def protect_private_documents(document: "AbstractPrivateDocument", request: "HttpRequest") -> None:
+    """Block access to private documents if the user has insufficient permissions."""
+    if document.is_private and (
+        not request.user.is_authenticated
+        or not permission_policy.user_has_any_permission_for_instance(
+            request.user, ["choose", "add", "change"], document
+        )
+    ):
+        raise PermissionDenied
