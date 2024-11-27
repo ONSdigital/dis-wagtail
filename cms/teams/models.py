bed50647f7e3e5b66df58a5e852c4bfd2c417d78
Mebin Abraham <mebin95+work@gmail.com>
2024-11-27 22:52:24 +0000

    Enable searching of teams + make teams view only

--- cms/teams/models.py
+++ cms/teams/models.py
@@ -1,9 +1,11 @@
+from functools import cached_property
 from typing import ClassVar
 
 from django.db import models
+from wagtail.search import index
 
 
-class Team(models.Model):
+class Team(index.Indexed, models.Model):
     identifier = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     precedence = models.PositiveIntegerField(null=True)
@@ -11,8 +13,22 @@ class Team(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
 
+    search_fields: ClassVar[list[index.BaseField]] = [
+        index.SearchField("name"),
+        index.AutocompleteField("name"),
+    ]
+
     class Meta:
-        ordering: ClassVar[list[str]] = ["precedence", "name"]
+        ordering: ClassVar[list[str]] = ["-is_active", "precedence", "name"]
 
     def __str__(self) -> str:
         return self.name
+
+    @cached_property
+    def total_members(self) -> int:
+        """Return the total number of users in the team."""
+        return self.users.count()
+
+    def get_users_display(self) -> str:
+        """Return a comma separated list of users in the team."""
+        return ", ".join(user.get_full_name() for user in self.users.all())
--- cms/teams/views.py
+++ cms/teams/views.py
@@ -1,9 +1,26 @@
 # Create TeamsViewSet
-from typing import ClassVar
+from typing import TYPE_CHECKING, ClassVar
 
 from wagtail.admin.viewsets.model import ModelViewSet
+from wagtail.permission_policies import ModelPermissionPolicy
 
 from .models import Team
+
+if TYPE_CHECKING:
+    from cms.users.models import User
+
+
+class ViewOnlyModelPermissionPolicy(ModelPermissionPolicy):
+    """A permission policy that enforces view only permission at the model level, by consulting
+    the standard django.contrib.auth permission model directly.
+    """
+
+    def user_has_permission(self, user: "User", action: str) -> bool:
+        """Ensure only view action is allowed."""
+        if action != "view":
+            return False
+
+        return user.has_perm(self._get_permission_name(action))
 
 
 class TeamsViewSet(ModelViewSet):
@@ -15,6 +32,20 @@ class TeamsViewSet(ModelViewSet):
     add_to_admin_menu = True
     menu_order = 200
     inspect_view_enabled = True
+    list_display: ClassVar[list[str]] = ["name", "total_members", "created_at", "updated_at"]
+    list_filter: ClassVar[list[str]] = ["name", "is_active", "created_at", "updated_at"]
+    inspect_view_fields: ClassVar[list[str]] = [
+        "name",
+        "created_at",
+        "updated_at",
+        "is_active",
+        "total_members",
+        "users",
+    ]
+
+    @property
+    def permission_policy(self) -> ViewOnlyModelPermissionPolicy:
+        return ViewOnlyModelPermissionPolicy(self.model)
 
 
 teams_viewset = TeamsViewSet("teams")
