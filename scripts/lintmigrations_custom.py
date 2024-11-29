c0bfbc7247357d17543c66b98171390ffed891b5
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-11-29 09:49:25 +0000

    Custom django lint migrations script added for cleaner output

---
+++ scripts/lintmigrations_custom.py
@@ -0,0 +1,46 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import os
+import subprocess
+
+
+# Extract the app label from the directory structure
+def get_app_labels():
+    app_labels = []
+    for root, dirs, files in os.walk("../cms"):
+        if "migrations" in dirs:
+            app_label = root.split(os.sep)[-1]
+            app_labels.append(app_label)
+    return app_labels
+
+
+# Run the lintmigrations command for a given app_label
+def run_lintmigration(app_label):
+    try:
+        result = subprocess.run(
+            ["python", "../manage.py", "lintmigrations", app_label],
+            text=True,
+            capture_output=True,
+            check=True,
+        )
+        output = f"\nRunning lintmigrations for {app_label}\n{result.stdout}"
+    except subprocess.CalledProcessError as e:
+        output = f"\nRunning lintmigrations for {app_label}\n{e.stdout or ''}{e.stderr or ''}"
+    return output
+
+
+def run_lintmigrations_in_parallel(app_labels):
+    # Use ThreadPoolExecutor for parallel execution
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(run_lintmigration, app_label): app_label for app_label in app_labels}
+
+        for future in as_completed(futures):
+            # Print the output of each completed future in order
+            print(future.result())
+
+
+if __name__ == "__main__":
+    app_labels = get_app_labels()
+
+    print("Apps we are linting: \n", app_labels)
+
+    run_lintmigrations_in_parallel(app_labels)
