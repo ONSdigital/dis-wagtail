ca74dfcaffdc28ccceebf6ae038bc50882b7b385
Jake Howard <jake.howard@torchbox.com>
2024-11-29 10:36:25 +0000

    Add tests that pages load in external env

--- cms/home/tests/test_models.py
+++ cms/home/tests/test_models.py
@@ -1,6 +1,5 @@
 from django.test import TestCase, override_settings
 
-from cms.core.tests import TransactionTestCase
 from cms.home.models import HomePage
 
 
@@ -23,26 +22,6 @@ class HomePageTestCase(TestCase):
     @override_settings(IS_EXTERNAL_ENV=True)
     def test_external_env(self):
         with self.assertNumQueries(9):
-            response = self.client.get(self.url)
-
-        self.assertEqual(response.status_code, 200)
-
-
-class HomePageReadReplicaTestCase(TransactionTestCase):
-    def setUp(self):
-        self.home_page = HomePage.objects.get()
-
-        self.url = self.home_page.get_url()
-
-    def test_loads(self):
-        with self.assertNumQueriesConnection(4, 9):
-            response = self.client.get(self.url)
-
-        self.assertEqual(response.status_code, 200)
-
-    @override_settings(IS_EXTERNAL_ENV=True)
-    def test_loads_external(self):
-        with self.assertNumQueriesConnection(0, 8):
             response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
