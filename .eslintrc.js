f851ab2fc0e1536ebe6569a235ec7ea4652138b8
AWitcherONS <ann.witcher@ons.gov.uk>
2024-12-02 17:53:51 +0000

    css stylelint changes due to pr comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,10 @@ module.exports = {
   root: true,
   // See https://github.com/wagtail/eslint-config-wagtail for rules.
   extends: ['@wagtail/eslint-config-wagtail', 'plugin:@typescript-eslint/recommended']
+  env: {
+      browser: true,
+      commonjs: true,
+      es6: true,
+      jest: true,
+    },
 }
