22007bb8e792158d26ac80db340411781cd91e95
Andy Babic <andy.babic@torchbox.com>
2024-12-03 20:02:57 +0000

    Use mock_aws instead

--- cms/private_media/tests/test_storages.py
+++ cms/private_media/tests/test_storages.py
@@ -1,51 +1,60 @@
 from unittest import mock
 
 from botocore.exceptions import ClientError
-from django.test import TestCase
+from django.conf import settings
+from django.test import TestCase, override_settings
+from moto import mock_aws
 from wagtail_factories import DocumentFactory
 
 from cms.private_media.storages import PrivacySettingS3Storage
 
 
+@mock_aws
+@override_settings(AWS_STORAGE_BUCKET_NAME="test-bucket")
 class PrivacySettingS3StorageTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.document = DocumentFactory()
 
     def setUp(self):
+        super().setUp()
         self.storage = PrivacySettingS3Storage()
+        # Create a bucket to allow '_set_file_acl' to at least reach the 'put' stage
+        bucket = self.storage.connection.Bucket(settings.AWS_STORAGE_BUCKET_NAME)
+        bucket.create()
 
-        bucket_mock = mock.MagicMock()
-        bucket_mock.Object = mock.MagicMock()
+    def test_make_private_client_error_on_acl_set(self):
+        with self.assertLogs("cms.private_media.storages", level="WARNING") as logs:
+            self.assertFalse(self.storage.make_private(self.document.file))
+        self.assertIn(
+            f"WARNING:cms.private_media.storages:Failed to set ACL for {self.document.file.name}: ",
+            logs.output[0],
+        )
+
+    def test_make_private_client_error_on_acl_get(self):
         object_mock = mock.MagicMock()
-        bucket_mock.Object.return_value = object_mock
+        object_mock.Acl.side_effect = ClientError({"Error": {"Code": "AccessDenied"}}, "GetObjectAcl")
+        # initialize the bucket to allow it to be patched
+        self.storage.bucket  # noqa: B018 # pylint: disable=pointless-statement
+        with mock.patch.object(self.storage._bucket, "Object", return_value=object_mock):  # pylint: disable=protected-access  # noqa: SIM117
+            with self.assertLogs("cms.private_media.storages", level="WARNING") as logs:
+                self.assertFalse(self.storage.make_private(self.document.file))
+        self.assertIn(
+            f"WARNING:cms.private_media.storages:Failed to retrieve ACL for {self.document.file.name}: ",
+            logs.output[0],
+        )
+
+    def test_make_public_success(self):
         acl_mock = mock.MagicMock()
+        acl_mock.put.return_value = True
+        object_mock = mock.MagicMock()
         object_mock.Acl.return_value = acl_mock
-
-        # This should make self.storage.bucket() return our customised MagicMock instance
-        self.storage._bucket = bucket_mock  # pylint: disable=protected-access
-
-        # Make the mock objects available for use in tests
-        self.bucket_mock = bucket_mock
-        self.object_mock = object_mock
-        self.acl_mock = acl_mock
-
-    def test_make_private(self):
-        self.storage.make_private(self.document.file)
-        self.bucket_mock.Object.assert_called_once_with(self.document.file.name)
-        self.object_mock.Acl.assert_called_once()
-        self.acl_mock.put.assert_called_once_with(ACL=self.storage.private_acl_name)
-
-    def test_make_public(self):
-        self.storage.make_public(self.document.file)
-        self.bucket_mock.Object.assert_called_once_with(self.document.file.name)
-        self.object_mock.Acl.assert_called_once()
-        self.acl_mock.put.assert_called_once_with(ACL=self.storage.public_acl_name)
-
-    def test_client_error_on_acl_get(self):
-        self.object_mock.Acl.side_effect = ClientError({"Error": {"Code": "AccessDenied"}}, "GetObjectAcl")
-        self.assertFalse(self.storage.make_private(self.document.file))
-
-    def test_client_error_on_acl_set(self):
-        self.acl_mock.put.side_effect = ClientError({"Error": {"Code": "AccessDenied"}}, "PutObjectAcl")
-        self.assertFalse(self.storage.make_private(self.document.file))
+        # initialize the bucket to allow it to be patched
+        self.storage.bucket  # noqa: B018 # pylint: disable=pointless-statement
+        with mock.patch.object(self.storage._bucket, "Object", return_value=object_mock):  # pylint: disable=protected-access  # noqa: SIM117
+            with self.assertLogs("cms.private_media.storages", level="INFO") as logs:
+                self.assertTrue(self.storage.make_public(self.document.file))
+        self.assertIn(
+            f"INFO:cms.private_media.storages:ACL set successfully for {self.document.file}",
+            logs.output,
+        )
