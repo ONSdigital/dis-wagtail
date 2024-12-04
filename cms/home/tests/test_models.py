636af63f6fc947425505ef09ba4769b570d7f97b
Jake Howard <jake.howard@torchbox.com>
2024-12-04 13:58:42 +0000

    Remove query assertions for homepage
    These cause issues in CI

--- cms/home/tests/test_models.py
+++ cms/home/tests/test_models.py
@@ -1,4 +1,3 @@
-from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase, override_settings
 
 from cms.home.models import HomePage
@@ -10,20 +9,15 @@ class HomePageTestCase(TestCase):
 
         self.url = self.home_page.get_url()
 
-        # Populate contenttype cache to avoid extra query
-        ContentType.objects.get_for_model(HomePage)
-
     def test_loads(self):
         """Test the homepage loads."""
-        with self.assertNumQueries(13):
-            response = self.client.get(self.url)
+        response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
 
     @override_settings(IS_EXTERNAL_ENV=True)
     def test_external_env(self):
         """Test the homepage loads in external environment."""
-        with self.assertNumQueries(9):
-            response = self.client.get(self.url)
+        response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
