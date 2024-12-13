e45793d38f5162b65111e7e6d81d0bd30be2f03f
Dan Braghiș <31622+zerolab@users.noreply.github.com>
2024-12-13 15:50:28 +0000

    Bundles: limit release calendar pages to provisional/confirmed (#50)
    * Add a custom chooser for release calendar pages that only includes upcoming release calendar pages
    * Add missing string translation on the information page template
    * Drop page status column
    * Ensure the bundle page chooser deduplicates choices

--- cms/bundles/forms.py
+++ cms/bundles/forms.py
@@ -46,6 +46,7 @@ class BundleAdminForm(WagtailAdminModelForm):
         - if we have an empty page reference, remove it form the form data
         - ensure the selected page is not in another active bundle.
         """
+        chosen = []
         for idx, form in enumerate(self.formsets["bundled_pages"].forms):
             if not form.is_valid():
                 continue
@@ -54,9 +55,18 @@ class BundleAdminForm(WagtailAdminModelForm):
             if page is None:
                 # tidy up in case the page reference is empty
                 self.formsets["bundled_pages"].forms[idx].cleaned_data["DELETE"] = True
-            else:
+                continue
+
+            if page in chosen:
+                # we saw this already, mark for removal to avoid duplicates.
+                self.formsets["bundled_pages"].forms[idx].cleaned_data["DELETE"] = True
+                continue
+
+            chosen.append(page)
+
+            if not form.cleaned_data["DELETE"]:
                 page = page.specific
-                if page.in_active_bundle and page.active_bundle != self.instance and not form.cleaned_data["DELETE"]:
+                if page.in_active_bundle and page.active_bundle != self.instance:
                     raise ValidationError(
                         _("'%(page)s' is already in an active bundle (%(bundle)s)")
                         % {
