72ead3c2d0d2b10f049d985642c8c31141199dba
Helen C <helen.chapman@torchbox.com>
2025-01-13 15:17:22 +0000

    Add tests for the embed_url in context

--- cms/core/tests/test_video_embed_block.py
+++ cms/core/tests/test_video_embed_block.py
@@ -215,3 +215,87 @@ class VideoEmbedBlockTestCase(TestCase):
         )
 
         self.assertEqual(self.video_block.clean(value), value)
+
+    def test_get_context__vimeo_embed_url_is_in_context_1(self):
+        """Check the VideoEmbedBlock correctly extracts the embed_url from a https://vimeo.com/ID link_url."""
+        value = self.video_block.to_python(
+            {
+                "link_url": "https://vimeo.com/908205163?share=copy",
+                "image": self.image.id,
+                "title": "The video",
+                "link_text": "Watch the video",
+            }
+        )
+        context = self.video_block.get_context(value)
+        self.assertEqual(context["embed_url"], "https://player.vimeo.com/video/908205163")
+
+    def test_get_context__vimeo_embed_url_is_in_context_2(self):
+        """Check the VideoEmbedBlock correctly extracts the embed_url
+        from a https://player.vimeo.com/video/ID link_url.
+        """
+        value = self.video_block.to_python(
+            {
+                "link_url": "https://player.vimeo.com/video/908205163",
+                "image": self.image.id,
+                "title": "The video",
+                "link_text": "Watch the video",
+            }
+        )
+        context = self.video_block.get_context(value)
+        self.assertEqual(context["embed_url"], "https://player.vimeo.com/video/908205163")
+
+    def test_get_context__vimeo_embed_url_is_in_context_3(self):
+        """Check the VideoEmbedBlock correctly extracts the embed_url
+        from a https://vimeo.com/showcase/7934865/video/ID link_url.
+        """
+        value = self.video_block.to_python(
+            {
+                "link_url": "https://vimeo.com/showcase/7934865/video/908205163",
+                "image": self.image.id,
+                "title": "The video",
+                "link_text": "Watch the video",
+            }
+        )
+        context = self.video_block.get_context(value)
+        self.assertEqual(context["embed_url"], "https://player.vimeo.com/video/908205163")
+
+    def test_get_context__youtube_embed_url_is_in_context(self):
+        """Check the VideoEmbedBlock correctly extracts the embed_url
+        from a https://www.youtube.com/watch?v=ID link_url.
+        """
+        value = self.video_block.to_python(
+            {
+                "link_url": "https://www.youtube.com/watch?v=ywzZXO-A7Pg&foo=bar",
+                "image": self.image.id,
+                "title": "The video",
+                "link_text": "Watch the video",
+            }
+        )
+        context = self.video_block.get_context(value)
+        self.assertEqual(context["embed_url"], "https://www.youtube.com/embed/ywzZXO-A7Pg")
+
+    def test_get_context__youtube_embed_url_is_in_context_2(self):
+        """Check the VideoEmbedBlock correctly extracts the embed_url from a https://youtu.be/ID link_url."""
+        value = self.video_block.to_python(
+            {
+                "link_url": "https://youtu.be/ywzZXO-A7Pg",
+                "image": self.image.id,
+                "title": "The video",
+                "link_text": "Watch the video",
+            }
+        )
+        context = self.video_block.get_context(value)
+        self.assertEqual(context["embed_url"], "https://www.youtube.com/embed/ywzZXO-A7Pg")
+
+    def test_get_context__youtube_embed_url_is_in_context_3(self):
+        """Check the VideoEmbedBlock correctly extracts the embed_url from a https://www.youtube.com/v/ID link_url."""
+        value = self.video_block.to_python(
+            {
+                "link_url": "https://www.youtube.com/v/ywzZXO-A7Pg",
+                "image": self.image.id,
+                "title": "The video",
+                "link_text": "Watch the video",
+            }
+        )
+        context = self.video_block.get_context(value)
+        self.assertEqual(context["embed_url"], "https://www.youtube.com/embed/ywzZXO-A7Pg")
