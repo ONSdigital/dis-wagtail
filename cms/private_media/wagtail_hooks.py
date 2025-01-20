59d7a1e1cc93a8b69f1ac621c7bccd1f7a61244f
Andy Babic <andyjbabic@gmail.com>
2025-01-20 16:24:07 +0000

    Restrict media privacy until a referencing page is published (#46)
    * Implement simplified private_media app
    
    * Add to image and document models
    
    * Enable custom image serve view
    
    * Fix unrelated linter complain on user model
    
    * Purge serve and file URLs when the privacy of an object changes
    
    * Rejig manager methods to avoid a separate update query for 'file_permissions_last_set'
    
    * Revise signal handlers to ignore references from draft pages when deciding which media to make private
    
    * Placate linters
    
    * Add tests
    
    * Use the warning log level for ACL setting issues
    
    * Add docs
    
    * Revise model docstrings to account for changes
    
    * Revise test docstrings in test_signal_handlers.py
    
    * Tweak docs based on feedback
    
    * Use more recognisable issue name in pylint: disable statement
    
    * Add a docstring to DummyPrivacySettingFileSystemStorage
    
    * Don't cache ServeImage view responses, and update PrivateImageMixin.get_privacy_controlled_serve_urls() to return an empty iterable
    
    * Update signal handlers to only fetch objects that are public or private where relevant
    
    * Add tests for PrivacySettingS3Storage
    
    * Be consistant in checking mocks across test_make_public and test_make_private
    
    * Use mock_aws instead
    
    * Add note to docs about image serve view responses not being cached
    
    * Use 'info' to log unsupported file permission setting attempts
    
    * Fix moto extras in pyproject.toml
    
    * Regenerate lock
    
    * Don't double up on publish/unpublish signals for pages
    
    * Utilize assertIsSubclass in tests
    
    * Log ACL set/put errors as exceptions
    
    * Test serve views with IS_EXTERNAL_ENV=True
    
    * Fixup for 10fdba7
    
    * Revert "Utilize assertIsSubclass in tests"
    
    This reverts commit 52267f27ca61c2f9de0ff8ef4cdb8e45cdbdf8b9.
    
    * Fix storage tests
    
    * Use InMemoryStorage for DummyPrivacySettingFileSystemStorage
    
    * Re-run only specific (read-only) tests with IS_EXTERNAL_ENV=True
    
    * Update unpublish_media_on_unpublish() to use a subset of 'referencing page ids' to figure out which pages are live
    
    * Revise the docs
    
    * Reference integration tests in docs
    
    * Fix typos
    
    * Fix another docs typo
    
    * One last round of typo fixes
    
    * Add notes on how private and public media items behave
    
    * Remove excess whitespace
    
    * PRIVATE_MEDIA_PERMISSION_SETTING_MAX_WORKERS -> PRIVATE_MEDIA_BULK_UPDATE_MAX_WORKERS
    
    * Raise an exception when the active storage backends does not implement the necessary methods
    
    * Rename PrivacySettingS3Storage to AccessControlledS3Storage and implement a version of FileSystemStorage to use in local dev (and by extension, tests)
    
    * Update tests to account for logging location and storage class name changes
    
    * intended_privacy -> privacy
    
    * Improve readability in PrivateMediaMixin.save()
    
    * Raise storage exceptions outside of threadpool excecution
    
    * Placate linters
    
    * Return early to reduce indentation in retry_file_permission_set_attempts command
    
    * Use abstract Collection type in update_file_permissions() annotation
    
    * Placate mypy
    
    * Fix bulk_operations not setting file permissions when a generator is provided
    
    * Simplify logging in AccessControlLoggingFileSystemStorage and use alternate in-memory storages to reduce need for mocking in tests
    
    * Update docs to reflect logging format changes
    
    * Revert: Don't cache ServeImage view responses, and update PrivateImageMixin.get_privacy_controlled_serve_urls() to return an empty iterable
    
    * Simplify PrivateImageMixin.get_privacy_controlled_serve_urls()
    
    * Fix rendition purge urls test
    
    * Extend error handling in storage backend to cover Boto3Error exceptions as well as instances of botocore.ClientError
    
    * Placate django-migration-linter (Add null=False to privacy_last_changed fields)
    
    * Add problematic migrations to ignore list
    
    * Update log messages in AccessControlLoggingFileSystemStorage to better communicate what would be happening in production

---
+++ cms/private_media/wagtail_hooks.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+
+from django.core.exceptions import PermissionDenied
+from wagtail import hooks
+from wagtail.documents.permissions import permission_policy
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest
+
+    from cms.private_media.models import PrivateDocumentMixin
+
+
+@hooks.register("before_serve_document")
+def protect_private_documents(document: "PrivateDocumentMixin", request: "HttpRequest") -> None:
+    """Block access to private documents if the user has insufficient permissions."""
+    if document.is_private and (
+        not request.user.is_authenticated
+        or not permission_policy.user_has_any_permission_for_instance(
+            request.user, ["choose", "add", "change"], document
+        )
+    ):
+        raise PermissionDenied
