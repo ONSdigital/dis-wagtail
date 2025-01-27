081cf27f7e30793537f6d1ce22fa07be40850412
Helen C <helen.chapman@torchbox.com>
2025-01-27 13:04:05 +0000

    Bring in some of the styling from the proof of concept

--- cms/datavis/models/charts.py
+++ cms/datavis/models/charts.py
@@ -5,22 +5,11 @@ from django.db import models
 from django.forms import Media
 from django.utils.functional import cached_property, classproperty
 from django.utils.translation import gettext_lazy as _
-from wagtail.admin.panels import (
-    FieldPanel,
-    FieldRowPanel,
-    InlinePanel,
-    MultiFieldPanel,
-    ObjectList,
-    TabbedInterface,
-)
+from wagtail.admin.panels import FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel, ObjectList, TabbedInterface
 
 from cms.core.fields import StreamField
 from cms.datavis.blocks import AnnotationBlock
-from cms.datavis.constants import (
-    HIGHCHARTS_THEMES,
-    HighchartsTheme,
-    MarkerStyle,
-)
+from cms.datavis.constants import HighchartsTheme, MarkerStyle
 from cms.datavis.fields import NonStrippingCharField
 from cms.datavis.utils import numberfy
 
@@ -105,7 +94,8 @@ class Chart(Visualisation):
 
     def get_context(self, request: Optional["HttpRequest"] = None, **kwargs: Any) -> dict[str, Any]:
         config = self.get_component_config(self.primary_data_source.headers, self.primary_data_source.rows)
-        return super().get_context(request, config=config, **kwargs)
+        theme = self.theme
+        return super().get_context(request, config=config, theme=theme, **kwargs)
 
     general_panels: ClassVar[Sequence["Panel"]] = [
         FieldPanel("name"),
@@ -204,12 +194,12 @@ class Chart(Visualisation):
             "chart": {
                 "type": self.highcharts_chart_type,
             },
-            "title": {"text": None},
-            "colors": HIGHCHARTS_THEMES[self.theme],
+            # "title": {"text": None},
+            # "colors": HIGHCHARTS_THEMES[self.theme],
             "legend": {
-                "align": "left",
+                # "align": "left",
                 "enabled": self.show_legend,
-                "verticalAlign": "top",
+                # "verticalAlign": "top",
             },
             "xAxis": self.get_x_axis_config(headers, rows),
             "yAxis": self.get_y_axis_config(headers, rows),
--- cms/static_src/javascript/components/highcharts-base-chart.js
+++ cms/static_src/javascript/components/highcharts-base-chart.js
@@ -1,4 +1,6 @@
 /* global Highcharts */
+import ChartOptions from './common-chart-options';
+
 class HighchartsBaseChart {
   static selector() {
     return '[data-highcharts-base-chart]';
@@ -6,10 +8,53 @@ class HighchartsBaseChart {
 
   constructor(node) {
     this.node = node;
+    this.chartType = this.node.dataset.highchartsType;
+    this.theme = this.node.dataset.highchartsTheme;
+    this.title = this.node.dataset.highchartsTitle;
     const chartNode = this.node.querySelector('[data-highcharts-chart]');
     const chartId = chartNode.dataset.highchartsId;
-    const config = JSON.parse(this.node.querySelector(`#config--${chartId}`).textContent);
-    Highcharts.chart(chartNode, config);
+    this.apiConfig = JSON.parse(this.node.querySelector(`#config--${chartId}`).textContent);
+    this.chartOptions = new ChartOptions(this.theme, this.title, this.chartType);
+    if (this.chartType === 'bar') {
+      this.updateBarChartHeight();
+      this.postLoadDataLabels();
+    }
+    Highcharts.setOptions(this.chartOptions.options);
+    Highcharts.chart(chartNode, this.apiConfig);
+  }
+
+  updateBarChartHeight() {
+    // dynamically set the height of the chart based on the number of categories. Bars are 30px wide, with 10px spacing, so we allow 40px per bar, with an extra 100px for margins.
+    // Todo: Needs more fine tuning, e.g. calculating the height of the legend
+    this.chartOptions.options.chart.height = this.apiConfig.xAxis.categories.length * 40 + 100;
+  }
+
+  // For this to work, we need an option to include data lables and a format for them
+  postLoadDataLabels() {
+    this.chartOptions.options.chart.events = {
+      // Move data labels inside bars if the bar is wide enough
+      load() {
+        const points = this.series[0].data;
+        const options = {
+          dataLabels: {
+            inside: true,
+            align: 'right',
+            verticalAlign: 'middle',
+            style: {
+              color: 'white',
+            },
+          },
+        };
+
+        points.forEach((point) => {
+          if (point.shapeArgs.height > 50) {
+            point.update(options, false);
+          }
+        });
+
+        this.redraw();
+      },
+    };
   }
 }
 
