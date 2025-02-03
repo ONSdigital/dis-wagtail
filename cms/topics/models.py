7bbd616275192dc263c723a7b85b1a99fac746dd
Dan Braghiș <dan.braghis@torchbox.com>
2025-02-03 17:24:50 +0000

    Add toc, featured article and processed articles/methodologies

--- cms/topics/models.py
+++ cms/topics/models.py
@@ -1,25 +1,30 @@
-from typing import TYPE_CHECKING, ClassVar
+from typing import TYPE_CHECKING, Any, ClassVar
 
 from django.conf import settings
 from django.db import models
+from django.db.models import OuterRef, QuerySet, Subquery
+from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
 from modelcluster.fields import ParentalKey
 from wagtail.admin.panels import FieldPanel, InlinePanel, PageChooserPanel
 from wagtail.fields import RichTextField
-from wagtail.models import Orderable
+from wagtail.models import Orderable, Page
 from wagtail.search import index
 
+from cms.articles.models import ArticleSeriesPage, StatisticalArticlePage
 from cms.core.fields import StreamField
 from cms.core.models import BasePage
+from cms.methodology.models import MethodologyPage
 from cms.topics.blocks import ExploreMoreStoryBlock
 
 if TYPE_CHECKING:
+    from django.http import HttpRequest
     from wagtail.admin.panels import Panel
 
 
 class TopicPageRelatedArticle(Orderable):
     parent = ParentalKey("TopicPage", on_delete=models.CASCADE, related_name="related_articles")
-    page = models.ForeignKey(
+    page = models.ForeignKey[Page](
         "wagtailcore.Page",
         on_delete=models.CASCADE,
         related_name="+",
@@ -30,7 +35,7 @@ class TopicPageRelatedArticle(Orderable):
 
 class TopicPageRelatedMethodology(Orderable):
     parent = ParentalKey("TopicPage", on_delete=models.CASCADE, related_name="related_methodologies")
-    page = models.ForeignKey(
+    page = models.ForeignKey[Page](
         "wagtailcore.Page",
         on_delete=models.CASCADE,
         related_name="+",
@@ -67,3 +72,60 @@ class TopicPage(BasePage):  # type: ignore[django-manager-missing]
     ]
 
     search_fields: ClassVar[list[index.BaseField]] = [*BasePage.search_fields, index.SearchField("summary")]
+
+    def get_context(self, request: "HttpRequest", *args: Any, **kwargs: Any) -> dict:
+        """Additional context for the template."""
+        context: dict = super().get_context(request, *args, **kwargs)
+        context["table_of_contents"] = self.table_of_contents
+        context["featured_item"] = self.latest_article_in_featured_series
+        return context
+
+    @cached_property
+    def latest_article_in_featured_series(self) -> StatisticalArticlePage | None:
+        """Returns the latest article in the featured series."""
+        article: StatisticalArticlePage | None = StatisticalArticlePage.objects.none()
+        if self.featured_series:
+            article = (
+                StatisticalArticlePage.objects.child_of(self.featured_series)
+                .live()
+                .public()
+                .order_by("-release_date")
+                .first()
+            )
+        return article
+
+    @cached_property
+    def processed_articles(self) -> QuerySet[ArticleSeriesPage]:
+        """Returns the latest articles in the series relevant for this topic."""
+        newest_qs = (
+            StatisticalArticlePage.objects.live()
+            .public()
+            .filter(path__startswith=OuterRef("path"), depth__gte=OuterRef("depth"))
+        )
+        newest_qs = newest_qs.order_by("-release_date")
+        latest_by_series = (
+            ArticleSeriesPage.objects.child_of(self)
+            .annotate(latest_child_page=Subquery(newest_qs.values("pk")[:1]))
+            .values_list("latest_child_page", flat=True)
+        )
+
+        return StatisticalArticlePage.objects.filter(pk__in=latest_by_series).order_by("-release_date")[:3]
+
+    @cached_property
+    def processed_methodologies(self) -> QuerySet[MethodologyPage]:
+        pages: QuerySet[MethodologyPage] = MethodologyPage.objects.child_of(self).live().public()[:3]
+        return pages
+
+    @cached_property
+    def table_of_contents(self) -> list[dict[str, str | object]]:
+        """Table of contents formatted to Design System specs."""
+        items = []
+        if self.latest_article_in_featured_series:
+            items += [{"url": "#featured", "text": _("Featured")}]
+        if self.processed_articles:  # pylint: disable=not-an-iterable,useless-suppression
+            items += [{"url": "#related-articles", "text": _("Related articles")}]
+        if self.processed_methodologies:
+            items += [{"url": "#related-methods", "text": _("Methods and quality information")}]
+        if self.explore_more:
+            items += [{"url": "#explore-more", "text": _("Explore more")}]
+        return items
