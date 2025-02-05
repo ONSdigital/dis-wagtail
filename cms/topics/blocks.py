c829ff59689f6d6e5425fee81b64d52d1c41273f
Dan Braghiș <dan.braghis@torchbox.com>
2025-02-05 14:33:19 +0000

    Wire in the Explore More block

--- cms/topics/blocks.py
+++ cms/topics/blocks.py
@@ -1,6 +1,12 @@
+from typing import TYPE_CHECKING
+
 from django.utils.translation import gettext_lazy as _
 from wagtail.blocks import CharBlock, PageChooserBlock, StreamBlock, StructBlock, URLBlock
 from wagtail.images.blocks import ImageChooserBlock
+
+if TYPE_CHECKING:
+    from wagtail.blocks import StreamValue, StructValue
+    from wagtail.models import Page
 
 
 class ExploreMoreExternalLinkBlock(StructBlock):
@@ -11,6 +17,21 @@ class ExploreMoreExternalLinkBlock(StructBlock):
 
     class Meta:
         icon = "link"
+
+    def get_formatted_value(self, value: "StructValue", context: dict | None = None) -> dict[str, str | dict]:  # pylint: disable=unused-argument
+        """Returns the value formatted for the Design System onsDocumentList macro."""
+        renditions = value["thumbnail"].get_renditions("fill-144x100", "fill-288x200")
+        return {
+            "thumbnail": {
+                "smallSrc": renditions["fill-144x100"].url,
+                "largeSrc": renditions["fill-288x200"].url,
+            },
+            "title": {
+                "text": value["title"],
+                "url": value["url"],
+            },
+            "description": value["description"],
+        }
 
 
 class ExploreMoreInternalLinkBlock(StructBlock):
@@ -28,7 +49,43 @@ class ExploreMoreInternalLinkBlock(StructBlock):
     class Meta:
         icon = "doc-empty-inverse"
 
+    def get_formatted_value(self, value: "StructValue", context: dict | None = None) -> dict[str, str | dict]:
+        """Returns the value formatted for the Design System onsDocumentList macro."""
+        page: Page = value["page"].specific_deferred
+        if not page.live:
+            return {}
+
+        formatted_value = {
+            "title": {
+                "text": value["title"] or getattr(page, "display_title", page.title),
+                "url": page.get_url(request=context.get("request") if context else None),
+            },
+            "description": value["description"] or getattr(page, "listing_summary", "") or getattr(page, "summary", ""),
+        }
+        image = value["thumbnail"] or page.listing_image
+        if image:
+            renditions = image.get_renditions("fill-144x100", "fill-288x200")
+            formatted_value["thumbnail"] = {
+                "smallSrc": renditions["fill-144x100"].url,
+                "largeSrc": renditions["fill-288x200"].url,
+            }
+        return formatted_value
+
 
 class ExploreMoreStoryBlock(StreamBlock):
     external_link = ExploreMoreExternalLinkBlock()
     internal_link = ExploreMoreInternalLinkBlock()
+
+    class Meta:
+        template = "templates/components/streamfield/explore_more_stream_block.html"
+
+    def get_context(self, value: "StreamValue", parent_context: dict | None = None) -> dict:
+        context: dict = super().get_context(value, parent_context=parent_context)
+
+        formatted_items = []
+        for child in value:
+            if formatted_item := child.block.get_formatted_value(child.value, context=context):
+                formatted_items.append(formatted_item)
+
+        context["formatted_items"] = formatted_items
+        return context
