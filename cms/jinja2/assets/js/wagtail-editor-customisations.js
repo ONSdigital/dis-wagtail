de3d23708f50f28261cab4beba9b2bc776f33ef4
Kacper Prywata <kacper.prywata@ons.gov.uk>
2025-02-21 14:15:23 +0000

    Change formatting ignore rules + format the new .js file

--- cms/jinja2/assets/js/wagtail-editor-customisations.js
+++ cms/jinja2/assets/js/wagtail-editor-customisations.js
@@ -1,15 +1,14 @@
-function setInitialMinimapExpandedByDefault(){
-    if (localStorage.getItem("wagtail:minimap-expanded") === null){
-      localStorage.setItem('wagtail:minimap-expanded', true);
-    }
-  };
-  
+function setInitialMinimapExpandedByDefault() {
+  if (localStorage.getItem('wagtail:minimap-expanded') === null) {
+    localStorage.setItem('wagtail:minimap-expanded', true);
+  }
+}
 
-  function setInitialRichtextToolbarStickyByDefault(){
-    if (localStorage.getItem("wagtail:draftail-toolbar") === null){
-      localStorage.setItem('wagtail:draftail-toolbar', 'sticky');
-    }
-  };
-  
+function setInitialRichtextToolbarStickyByDefault() {
+  if (localStorage.getItem('wagtail:draftail-toolbar') === null) {
+    localStorage.setItem('wagtail:draftail-toolbar', 'sticky');
+  }
+}
+
 setInitialMinimapExpandedByDefault();
 setInitialRichtextToolbarStickyByDefault();
