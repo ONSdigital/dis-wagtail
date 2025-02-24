0302a9d1bece52e22ff0c14e71a56d1ad5bcf026
Kacper Prywata <113175775+kacperpONS@users.noreply.github.com>
2025-02-24 09:32:30 +0000

    Methodology page UI tests (#102)
    * Add fix from PR #101
    
    * Added the test
    
    * Ignore pylint errors
    
    * Make them pass
    
    * Fix an issue with tests failing in headless and passing in headed mode
    
    * Rewording to match steps pulled from main
    
    * Formatting + rewording
    
    * Revert a change to make tests pass
    
    * A bunch of improvements
    
    * Remove no longer needed file for theme page set up
    
    * Remove duplicated step definition
    
    * Add missing assertion
    
    * Topic page test fix
    
    * Added missing checks + where I got with testing preview (failing so commented out)
    
    * Formatting - all green locally
    
    * Test
    
    * Attempt to fix headless failures
    
    * Bump the wait time a bit
    
    * Refactor the fix above into it's own step
    
    * Uncommented the preview test
    
    * Added a test for the draft functionality of the page
    
    * Refactoring
    
    * Renamed a test function
    
    * Experiment - removed the delay in preview test
    
    * ^ the same for the mandatory fields scenario 🤔
    
    * Add back the delay for mandatory fields scenario + make step wording more precise
    
    * Experiment - move the delay to the 'then' step of the mandatory fields scenario
    
    * ^ revert the above
    
    * Experiment - replace delay with wait_for_load_state
    
    * ^ the same but after clicking the button
    
    * Revert to using delay
    
    * Test improvements
    
    ---------
    
    Co-authored-by: Dan Braghiș <dan.braghis@torchbox.com>

--- functional_tests/features/information_page.feature
+++ functional_tests/features/information_page.feature
@@ -5,7 +5,7 @@ Feature: A general use information page
         When the user navigates to the pages menu
         And the user clicks add child page and chooses information page type
         And the user adds content to the new information page
-        And the user clicks publish page
+        And the user clicks "Publish page"
         And the user clicks "View Live" on the publish confirmation banner
         Then the new information page with the added content is displayed
         And the user can see the breadcrumbs
--- functional_tests/steps/page_editor.py
+++ functional_tests/steps/page_editor.py
@@ -1,9 +1,10 @@
-from behave import when  # pylint: disable=no-name-in-module
+from behave import then, when  # pylint: disable=no-name-in-module
 from behave.runner import Context
 from django.urls import reverse
+from playwright.sync_api import expect
 
 
-@when("the user clicks publish page")
+@when('the user clicks "Publish page"')
 @when("publishes the page")
 def user_clicks_publish_page(context: Context) -> None:
     context.page.get_by_role("button", name="More actions").click()
@@ -15,15 +16,33 @@ def user_clicks_view_live_on_publish_confirmation_banner(context: Context) -> No
     context.page.get_by_role("link", name="View live").click()
 
 
+@when('the user clicks the "Save Draft" button')
+def click_save_draft_with_delay(context: Context):
+    # add a small delay to allow any client-side JS to initialize.
+    context.page.wait_for_timeout(500)
+    context.page.get_by_role("button", name="Save Draft").click()
+
+
 @when('clicks the "{button_text}" button')
 def clicks_the_given_button(context: Context, button_text: str):
     context.page.get_by_role("button", name=button_text).click()
 
 
 @when("the user edits the {page} page")
-def the_user_edits_the_topic_page(context: Context, page: str) -> None:
+def the_user_edits_a_page(context: Context, page: str) -> None:
     the_page = page.lower().replace(" ", "_")
     if not the_page.endswith("_page"):
         the_page += "_page"
     edit_url = reverse("wagtailadmin_pages:edit", args=[getattr(context, the_page).pk])
     context.page.goto(f"{context.base_url}{edit_url}")
+
+
+@when("the user navigates to the page history menu")
+def user_navigates_to_the_history_menu(context: Context):
+    context.page.get_by_role("link", name="History").click()
+
+
+@then("the saved draft version is visible")
+def saved_draft_version_is_visible(context: Context):
+    expect(context.page.get_by_role("button", name="Just now").first).to_be_visible()
+    expect(context.page.get_by_text("Draft saved")).to_be_visible()
