41423e0c6cd513b50efd390be7ac847a661d4c79
Dan Braghiș <dan.braghis@torchbox.com>
2025-02-26 16:58:18 +0000

    Improve table-only behaviour

--- cms/tiny_table_block/static/tiny_table_block/js/tiny-table-block.js
+++ cms/tiny_table_block/static/tiny_table_block/js/tiny-table-block.js
@@ -7,19 +7,64 @@ class TinyTableBlockDefinition extends window.wagtailStreamField.blocks.StructBl
         selector: "#" + prefix + "-data",
         plugins: "table autoresize",
         menubar: "",
-        toolbar: "undo redo | table | tablerowheader tablecolheader tablemergecells tablesplitcells | tableinsertcolbefore tableinsertcolafter tableinsertrowbefore tableinsertrowafter",
-        valid_elements: 'table[border|width|height|align|summary],tr[align|valign],td[align|valign|width|colspan|rowspan],th[align|valign|width|colspan|rowspan],a[href|target|rel],caption',
+        toolbar: "undo redo | tablerowheader tablecolheader tablemergecells tablesplitcells | tableinsertcolbefore tableinsertcolafter tableinsertrowbefore tableinsertrowafter",
+        valid_elements: 'br,table[border|width|height|align|summary],tr[align|valign],td[align|valign|width|colspan|rowspan],th[align|valign|width|colspan|rowspan],a[href|target|rel],caption',
         table_toolbar: "",  // disable the floating toolbar
         table_advtab: false,
+        table_appearance_options: false,
         table_cell_advtab: false,
         table_row_advtab: false,
-        table_clone_elements: '',
+        table_sizing_mode: 'responsive',
+        table_resize_bars: false,
+        object_resizing: false,
         contextmenu_never_use_native: true,
         skin: (window.matchMedia("(prefers-color-scheme: dark)").matches ? "oxide-dark" : "oxide"),
         content_css: (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "default"),
+        statusbar: false,
         branding: false,
         promotion: false,
         license_key: "gpl",
+        setup: (editor) => {
+          let tableExists = false;
+
+          editor.on("init", () => {
+            tableExists = editor.getBody().getElementsByTagName("table").length > 0;
+            if (!tableExists) {
+                editor.execCommand("mceInsertTable", false, { rows: 2, columns: 2 });
+            }
+          });
+
+          editor.on("change NodeChange SetContent", () => {
+            // Check for tables after content changes
+            tableExists = editor.getBody().getElementsByTagName('table').length > 0;
+            // propagate the changes to the hidden input.
+            editor.save();
+          });
+
+          editor.on("BeforeExecCommand", (e) => {
+            // Don't allow inserting a table if one exists.
+            if (e.command === 'mceInsertTable' && tableExists) {
+              e.preventDefault();
+            }
+          });
+
+          editor.on("ExecCommand", (e) => {
+              if (e.command === "mceTableDelete") {
+                  // re-add empty table on delete
+                  editor.execCommand("mceInsertTable", false, {rows: 2, columns: 2});
+              }
+          });
+
+          editor.on("PastePreProcess keydown", (e) => {
+              // Only allow adding content into the one table
+              const activeNode = editor.selection.getNode();
+              const isCell = activeNode.nodeName === "TD" || activeNode.nodeName === "TH";
+              const isParentCell = activeNode.parentNode.nodeName === "TD" || activeNode.parentNode.nodeName === "TH";
+              if (!isCell && !isParentCell) {
+                  e.preventDefault();
+              }
+          });
+        }
     });
 
     return block;
