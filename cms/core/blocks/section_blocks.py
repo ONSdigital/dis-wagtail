5ff6012d0b58d89d6f5d3d56b6c34b63bfc6ff83
Kacper Prywata <kacper.prywata@ons.gov.uk>
2025-03-05 14:05:41 +0000

    Move logic from template to get_context

--- cms/core/blocks/section_blocks.py
+++ cms/core/blocks/section_blocks.py
@@ -21,7 +21,7 @@ from cms.core.blocks import (
 from cms.core.models import GlossaryTerm
 
 if TYPE_CHECKING:
-    from wagtail.blocks import StructValue
+    from wagtail.blocks import StreamValue, StructValue
 
 
 class SectionContentBlock(StreamBlock):
@@ -54,6 +54,18 @@ class GlossarySectionBlock(StructBlock):
     def to_table_of_contents_items(self, value: "StructValue") -> list[dict[str, str]]:
         """Convert the value to the table of contents component macro format."""
         return [{"url": "#" + slugify(value["title"]), "text": value["title"]}]
+
+    def get_context(self, value: "StreamValue", parent_context: dict | None = None) -> dict:
+        """Inject formatted glossary terms to be used with ONS Accordion component."""
+        context: dict = super().get_context(value, parent_context)
+        context["formatted_glossary_terms"] = [
+            {
+                "title": glossary_term.name,
+                "content": glossary_term.definition,
+            }
+            for glossary_term in value["content"]
+        ]
+        return context
 
 
 class SectionBlock(StructBlock):
