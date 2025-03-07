297d4a3428ad4b3124a357e3d856467499661ae8
nehakerung <nehakerung@gmail.com>
2025-03-07 15:14:52 +0000

    Update UI test

--- functional_tests/features/footer_menu.feature
+++ functional_tests/features/footer_menu.feature
@@ -1,27 +1,39 @@
-Feature: CMS users can manage the footer menu
-    Scenario: A CMS user can create and preview a footer menu
+Feature: CMS users can create the footer menu
+    Scenario: A CMS user can create footer menu instance
         Given a CMS user logs into the admin site
-        When the user creates a footer menu
+        And the user creates a footer menu
         And the user populates the footer menu
-        And the user clicks "save draft"
+        And the user clicks the "Save Draft" button
         And the user clicks "View Live"
         Then the preview of the footer menu is displayed with the populated data
-    
+
+    Scenario: Validation errors are raised when footer menu is left empty
+        Given a CMS user logs into the admin site
+        When the user creates a footer menu
+        And the user inputs empty footer
+        And the user clicks the "Save Draft" button
+        Then an error message is presented
+
     Scenario: A CMS user can edit the footer menu
         Given a CMS user logs into the admin site
-        And pre-populated footer menu exists
+        And a pre-populated footer menu exists
         When the user edits a footer menu
-        And the user clicks "Publish page"
+        And the user clicks the "Save Draft" button
         And the user clicks "View Live"
         Then the preview of the footer menu is displayed with the edited data
 
     Scenario: Validation errors are raised when duplicate links are entered on the footer menu
         Given a CMS user logs into the admin site
-        And pre-populated footer menu exists
+        And a pre-populated footer menu exist
         When user adds a duplicate link
         And the user clicks the "Save Draft" button
         Then an error message is displayed next to the duplicated link field
 
-    Scenario: Validation errors are raised when footer menu is left empty
+    Scenario: a CMS user can assign footer menu in navigation settings
         Given a CMS user logs into the admin site
-        When the user creates a footer menu
+        And a footer menu exists
+        And a CMS user navigates to "Navigation settings"
+        When no footer menu is selected
+        And chooses footer menu
+        And clicks "saves"
+        Then footer menu is assigned
--- functional_tests/steps/footer_menu.py
+++ functional_tests/steps/footer_menu.py
@@ -1,5 +1,6 @@
 from behave import step, then, when  # pylint: disable=no-name-in-module
 from behave.runner import Context
+from playwright.sync_api import expect
 
 
 @step("the user creates a footer menu")
@@ -14,19 +15,12 @@ def user_populates_footer_menu(context: Context):
     context.page.get_by_role("button", name="Insert a block").click()
     context.page.get_by_role("textbox", name="Column title*").click()
     context.page.get_by_role("textbox", name="Column title*").press("CapsLock")
-    context.page.get_by_role("textbox", name="Column title*").fill("")
-    context.page.get_by_role("textbox", name="Column title*").press("CapsLock")
     context.page.get_by_role("textbox", name="Column title*").fill("About")
     context.page.get_by_role("textbox", name="or External Link").click()
     context.page.get_by_role("textbox", name="or External Link").fill("https://www.ons.gov.uk/help/accessibility")
     context.page.get_by_role("textbox", name="Title", exact=True).click()
     context.page.get_by_role("textbox", name="Title", exact=True).press("CapsLock")
     context.page.get_by_role("textbox", name="Title", exact=True).fill("Accessibility")
-
-
-@when('the user clicks "save draft"')
-def user_clicks_publish_menu(context: Context):
-    context.page.get_by_role("button", name="Save draft").click()
 
 
 @when('the user clicks "View Live"')
@@ -38,11 +32,77 @@ def user_clicks_view_live(context: Context):
 
 @then("the preview of the footer menu is displayed with the populated data")
 def preview_footer_menu(context: Context):
-    context.page()
+    expect(context.page.get_by_role("heading", name="About")).to_be_visible()
+    expect(context.page.get_by_role("link", name="Accessibility")).to_be_visible()
 
 
-# @given("pre-populated footer menu exists")
-# def step_impl(context):
+@when("the user inputs empty footer")
+def empty_footer_menu(context: Context):
+    context.page.get_by_role("button", name="Insert a block").click()
+    context.page.get_by_role("button", name="Save draft").click()
+
+
+@then("an error message is presented")
+def error_message_footer_menu(context: Context):
+    expect(context.page.get_by_text("The footer menu could not be")).to_be_visible()
+    expect(context.page.get_by_text("This field is required.")).to_be_visible()
+    expect(context.page.get_by_text("Missing required fields")).to_be_visible()
+
+
+@when("a pre-populated footer menu exists")
+def populated_footer(context: Context):
+    context.page.get_by_role("link", name="Snippets").click()
+    context.page.get_by_role("link", name="Footer menus").click()
+    context.page.get_by_role("link", name="Footer Menu", exact=True).click()
+
+
+@when("a footer menu exists")
+def footer_menu_exists(context: Context):
+    context.page.get_by_role("link", name="Snippets").click()
+    context.page.get_by_role("link", name="Footer menus").click()
+    context.page.get_by_role("link", name="Footer Menu", exact=True).click()
+
+
+@step("no footer menu is selected")
+def no_footer_menu_assigned(context: Context):
+    context.page.get_by_role("button", name="Choose footer menu", exact=False)
+
+
+@when('a CMS user navigates to "Navigation settings"')
+def navigation_settings(context: Context):
+    context.page.get_by_role("button", name="Settings").click()
+    context.page.get_by_role("link", name="Navigation settings").click()
+
+
+@when("chooses footer menu")
+def footer_menu_nav_set(context: Context):
+    context.page.get_by_role("button", name="Choose footer menu").click()
+    context.page.get_by_role("link", name="Footer Menu", exact=True).click()
+
+
+@when('clicks "saves"')
+def click_save(context: Context):
+    context.page.get_by_role("button", name="Save").click()
+
+
+@step("footer menu is assigned")
+def assign_footer_menu(context: Context):
+    context.page.get_by_role("button", name="Choose footer menu", exact=False)
+
+
+@when("the user edits a footer menu")
+def edit_footer_menu(context: Context):
+    context.page.get_by_role("textbox", name="Column title*").click()
+    context.page.get_by_role("textbox", name="Column title*").fill("More")
+    context.page.get_by_role("textbox", name="or External Link").click()
+    context.page.get_by_role("textbox", name="or External Link").fill("https://www.ons.gov.uk")
+    context.page.get_by_role("textbox", name="Title", exact=True).click()
+    context.page.get_by_role("textbox", name="Title", exact=True).fill("Site")
+
+
+@when("the preview of the footer menu is displayed with the edited data")
+def edited_footer_menu(context: Context):
+    expect(context.page.get_by_role("link", name="Site")).to_be_visible()
 
 
 # @when("the user edits a footer menu")
