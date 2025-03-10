3e03fb464cd6f70ae8bbfd3047bee8f0d3466415
Dan Braghiș <dan.braghis@torchbox.com>
2025-03-10 11:54:43 +0000

    Add the new table block to core streamfield

--- cms/core/blocks/stream_blocks.py
+++ cms/core/blocks/stream_blocks.py
@@ -10,6 +10,7 @@ from cms.core.blocks import (
     HeadingBlock,
     InformationPanelBlock,
     ONSEmbedBlock,
+    ONSTableBlock,
     QuoteBlock,
     RelatedLinksBlock,
     VideoEmbedBlock,
@@ -51,6 +52,7 @@ class CoreStoryBlock(StreamBlock):
     image = ImageChooserBlock(group="Media")
     documents = DocumentsBlock(group="Media")
     related_links = RelatedLinksBlock()
+    table = ONSTableBlock(group="DataVis")
     equation = MathBlock(group="DataVis", icon="decimal")
     ons_embed = ONSEmbedBlock(group="DataVis", label="ONS General Embed")
 
