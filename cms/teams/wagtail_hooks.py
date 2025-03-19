56c84893237e9f523b0ac7d6b869a3d10de207f1
Dan Braghiș <31622+zerolab@users.noreply.github.com>
2025-03-19 11:58:10 +0000

    Add basic preview teams model (#120)
    Co-authored-by: Mebin Abraham <mebin95+work@gmail.com>

---
+++ cms/teams/wagtail_hooks.py
@@ -0,0 +1,19 @@
+from typing import TYPE_CHECKING
+
+from wagtail import hooks
+
+from cms.teams.viewsets import team_chooser_viewset, teams_viewset
+
+if TYPE_CHECKING:
+    from .viewsets import TeamChooserViewSet, TeamsViewSet
+
+
+@hooks.register("register_admin_viewset")
+def register_teams_viewset() -> "TeamsViewSet":
+    """Register the teams viewset."""
+    return teams_viewset
+
+
+@hooks.register("register_admin_viewset")
+def register_teams_chooser_viewset() -> "TeamChooserViewSet":
+    return team_chooser_viewset
--- cms/users/models.py
+++ cms/users/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     """Barebones custom user model."""
+
+    teams = models.ManyToManyField("teams.Team", related_name="users", blank=True)
