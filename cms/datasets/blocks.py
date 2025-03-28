badaa6c9229d6a7bae15bf7bce131efea52afa0b
AdamHawtin <adam.hawtin@ons.gov.uk>
2025-03-28 11:01:56 +0000

    Add comments

--- cms/datasets/blocks.py
+++ cms/datasets/blocks.py
@@ -44,6 +44,10 @@ class DatasetStoryBlock(StreamBlock):
     def clean(self, value: StreamValue) -> StreamValue:
         cleaned_value = super().clean(value)
 
+        # Validate there are no duplicate datasets, including between manual and looked up datasets referencing the same
+        # URL path
+
+        # For each dataset URL path, record the indices of the blocks it appears in
         url_paths = defaultdict(set)
         for block_index, block in enumerate(cleaned_value):
             url_path = (
@@ -53,6 +57,8 @@ class DatasetStoryBlock(StreamBlock):
 
         block_errors = {}
         for block_indices in url_paths.values():
+            # Add a block error for any index which contains a duplicate a URL path, so that the validation error
+            # messages appear on the actual duplicate entries
             if len(block_indices) > 1:
                 for index in block_indices:
                     block_errors[index] = ValidationError("Duplicate datasets are not allowed")
