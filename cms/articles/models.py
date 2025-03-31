8983278eca924af048bef668a80498d7a9d5cf49
Maciek Baron <thebezet@gmail.com>
2025-03-31 13:29:40 +0100

    Ensure article label is translatable
    Co-authored-by: Dan Braghiș <31622+zerolab@users.noreply.github.com>

--- cms/articles/models.py
+++ cms/articles/models.py
@@ -91,7 +91,7 @@ class StatisticalArticlePage(BundledPageMixin, RoutablePageMixin, BasePage):  # 
     parent_page_types: ClassVar[list[str]] = ["ArticleSeriesPage"]
     subpage_types: ClassVar[list[str]] = []
     template = "templates/pages/statistical_article_page.html"
-    label = "Article"
+    label = _("Article")
 
     # Fields
     news_headline = models.CharField(max_length=255, blank=True)
