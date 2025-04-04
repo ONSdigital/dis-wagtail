7b6261c6325512c1b14504a1e26bd5b0bec763a9
Jake Howard <jake.howard@torchbox.com>
2025-04-04 16:04:51 +0100

    Remove duplicate setting in tests

--- cms/settings/test.py
+++ cms/settings/test.py
@@ -73,4 +73,3 @@ SLACK_NOTIFICATIONS_WEBHOOK_URL = None
 
 ONS_API_BASE_URL = "https://dummy_base_api"
 ONS_WEBSITE_DATASET_BASE_URL = "https://dummy_datasets/datasets"
-ONS_API_BASE_URL = "dummy_url"
