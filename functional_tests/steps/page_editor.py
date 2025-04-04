cd484f8896a537100413dfab83e99792d04bde87
Neha <148205641+nehakerung@users.noreply.github.com>
2025-04-04 10:48:22 +0100

    Footer Menu UI Tests (#130)
    * Add Footer menu UI tests features, scenarios and steps
    
    * Fix steps
    
    * Update UI tests
    
    * Update UI tests
    
    * Update UI test
    
    * Add comments and  max links scenario
    
    * Update UI tests
    
    * Working UI test
    
    * Update scenarios
    
    * Refactor footer menu feature scenarios for clarity and consistency
    
    * Update UI test to match up to refactoring
    
    * Refactored feature file and footer_menu step definitions
    
    * Enhance footer menu feature tests with deletion confirmation and final verification scenarios, feature file and step definitions made more reusable, maintainable and refactored.
    
    * Code cleanup
    
    * Address review comments
    
    * Address review comments
    
    * retrigger checks
    
    * Refactored per review comments
    
    * Updated wording to be clearer
    
    ---------
    
    Co-authored-by: sanjeevz3009 <sanjeevz1717@gmail.com>
    Co-authored-by: Sanjeev Srithevan <41544303+sanjeevz3009@users.noreply.github.com>

--- functional_tests/steps/page_editor.py
+++ functional_tests/steps/page_editor.py
@@ -6,10 +6,11 @@ from playwright.sync_api import expect
 from cms.themes.tests.factories import ThemePageFactory
 
 
-@when('the user clicks "Publish page"')
+@when('the user clicks "Publish"')
 @when("publishes the page")
-def user_clicks_publish_page(context: Context) -> None:
-    publish_page(context)
+def user_clicks_publish(context: Context) -> None:
+    context.page.get_by_role("button", name="More actions").click()
+    context.page.get_by_role("button", name="Publish").click()
 
 
 @when('the user clicks "View Live" on the publish confirmation banner')
@@ -20,7 +21,7 @@ def user_clicks_view_live_on_publish_confirmation_banner(context: Context) -> No
 @step('the user clicks the "{button_text}" button')
 def click_the_given_button(context: Context, button_text: str) -> None:
     if button_text in ("Save Draft", "Preview"):
-        # add a small delay to allow any client-side JS to initialize.
+        # add a small delay to allow any client-side JS to initialise.
         context.page.wait_for_timeout(500)
     context.page.get_by_role("button", name=button_text).click()
 
@@ -162,3 +163,13 @@ def the_user_can_bulk_delete_a_theme_page_and_its_children(context: Context):
     expect(context.page.get_by_text("Are you sure you want to")).to_be_visible()
     context.page.get_by_role("button", name="Yes, delete it").click()
     expect(context.page.get_by_text(f"Page '{context.theme_page.title}' deleted.")).to_be_visible()
+
+
+@when('the user clicks "Save" to save the Snippet')
+def user_saves_in_navigation_settings(context: Context):
+    context.page.get_by_role("button", name="Save").click()
+
+
+@when("the user clicks toggle preview")
+def user_clicks_view_live(context: Context):
+    context.page.get_by_role("button", name="Toggle preview").click()
