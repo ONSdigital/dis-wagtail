42ad88b6ee1308f119f3eeb32e5ea9167cedc0e7
Maciej Baron <MaciekBaron@users.noreply.github.com>
2025-04-08 11:57:25 +0100

    Add additional functional tests corrections

--- functional_tests/steps/statistical_article_page.py
+++ functional_tests/steps/statistical_article_page.py
@@ -48,6 +48,7 @@ def user_updates_the_statistical_article_page_content(context: Context):
 @step('the user clicks on "View superseded version"')
 def user_clicks_on_view_superseded_version(context: Context):
     page = context.page
+    page.locator(".ons-corrections-notices__banner").click()
     page.get_by_role("link", name="View superseded version").click()
 
 
@@ -169,8 +170,8 @@ def user_adds_a_notice(context: Context):
 @then("the published statistical article page has the added correction")
 def the_published_statistical_article_page_has_the_added_correction(context: Context):
     expect(context.page.get_by_role("heading", name="Corrections")).to_be_visible()
-    expect(context.page.get_by_text("13 March 2025 1:59p.m.")).to_be_visible()
-    expect(context.page.get_by_text("Correction text")).to_be_visible()
+    expect(context.page.get_by_text("13 March 2025 1:59p.m.")).to_be_hidden()
+    expect(context.page.get_by_text("Correction text")).to_be_hidden()
 
 
 @then("the published statistical article page has corrections in chronological order")
@@ -182,8 +183,8 @@ def the_published_statistical_article_page_has_corrections_in_chronological_orde
 @then("the published statistical article page has the added notice")
 def the_published_statistical_article_page_has_the_added_notice(context: Context):
     expect(context.page.get_by_role("heading", name="Notices")).to_be_visible()
-    expect(context.page.get_by_text("13 March 2025 1:59p.m.")).to_be_visible()
-    expect(context.page.get_by_text("Notice text")).to_be_visible()
+    expect(context.page.get_by_text("13 March 2025 1:59p.m.")).to_be_hidden()
+    expect(context.page.get_by_text("Notice text")).to_be_hidden()
 
 
 @then("the user can edit the correction")
