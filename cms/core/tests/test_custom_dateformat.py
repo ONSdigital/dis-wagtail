33aab399d1d4ad3da36a8aa330a56bbc3947a962
nehakerung <nehakerung@gmail.com>
2025-04-10 12:13:15 +0100

    Add tests

--- cms/core/tests/test_custom_dateformat.py
+++ cms/core/tests/test_custom_dateformat.py
@@ -1,1 +1,21 @@
 # Tests to check the custom date format in the CMS
+
+from datetime import datetime
+
+from django.test import SimpleTestCase
+
+from cms.core.custom_dateformat import new_date_format
+
+
+class NewDateFormatTests(SimpleTestCase):
+    # Tests that am input outputs "am"
+    def test_am(self):
+        am = datetime(2025, 3, 1, 7, 30)
+        result = new_date_format(am, "a")
+        self.assertEqual(result, "am")
+
+    # Test that an pm input outputs "pm"
+    def test_pm(self):
+        pm = datetime(2025, 3, 1, 18, 30)
+        result = new_date_format(pm, "a")
+        self.assertEqual(result, "pm")
--- cms/core/tests/test_jinja2tags.py
+++ cms/core/tests/test_jinja2tags.py
@@ -1,1 +1,37 @@
 # Tests to check the filter for the custom date format in the CMS
+
+from datetime import datetime
+
+from django.test import SimpleTestCase
+
+from cms.core.templatetags.util_tags import custom_date_format
+
+
+class CustomDateFormatTests(SimpleTestCase):
+    def setUp(self):
+        self.am = datetime(2013, 3, 1, 9, 30)
+        self.pm = datetime(1990, 3, 1, 18, 45)
+
+    # tests datetime format for am
+    def test_datetime_format_am(self):
+        result = custom_date_format(self.am, "DATETIME_FORMAT")
+        self.assertEqual(result, "1 March 2013 9:30am")
+
+    # tests datetime format for pm
+    def test_datetime_format_pm(self):
+        result = custom_date_format(self.pm, "DATETIME_FORMAT")
+        self.assertEqual(result, "1 March 1990 6:45pm")
+
+    # tests format string 'a' for am
+    def test_format_string_a_am(self):
+        result = custom_date_format(self.am, "a")
+        self.assertEqual(result, "am")
+
+    # tests format string 'a' for pm
+    def test_format_string_a_pm(self):
+        result = custom_date_format(self.pm, "a")
+        self.assertEqual(result, "pm")
+
+    # tests for empty input
+    def test_value_none(self):
+        self.assertEqual(custom_date_format(None, "a"), "")
