df9423701d799b560150c8b220e99550736807ac
Maciej Baron <MaciekBaron@users.noreply.github.com>
2025-04-15 15:49:30 +0100

    Prepopulate statistical article pages

---
+++ cms/articles/wagtail_hooks.py
@@ -0,0 +1,23 @@
+from typing import TYPE_CHECKING
+
+from wagtail import hooks
+from wagtail.admin import messages
+
+from cms.articles.models import StatisticalArticlePage
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest
+    from wagtail.models import Page
+
+
+@hooks.register("before_create_page")
+def before_create_page(
+    request: "HttpRequest",
+    parent_page: "Page",  # pylint: disable=unused-argument
+    page_class: type["Page"],
+) -> None:
+    if page_class == StatisticalArticlePage:
+        messages.info(
+            request,
+            "This page has been prepopulated with the content from the latest page in the series.",
+        )
