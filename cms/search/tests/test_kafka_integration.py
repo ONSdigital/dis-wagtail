68c1e549ee66428786ae20cc8d341f484632e7a4
sanjeevz3009 <sanjeevz1717@gmail.com>
2025-04-17 01:02:26 +0100

    Refactor Kafka integration tests to use settings for bootstrap servers and update Docker configurations

--- cms/search/tests/test_kafka_integration.py
+++ cms/search/tests/test_kafka_integration.py
@@ -1,19 +1,16 @@
 import json
 import logging
-import os
 import time
 
+from django.conf import settings
 from django.test import TestCase, override_settings
 from kafka import KafkaConsumer
 
 from cms.search.publishers import KafkaPublisher
 from cms.standard_pages.tests.factories import InformationPageFactory
 
-KAFKA_SERVER = os.environ.get("KAFKA_SERVER", "localhost:19092")
-
 
 @override_settings(
-    KAFKA_SERVER=KAFKA_SERVER,
     KAFKA_CHANNEL_CREATED_OR_UPDATED="search-content-updated",
     KAFKA_CHANNEL_DELETED="search-content-deleted",
 )
@@ -28,7 +25,7 @@ class KafkaIntegrationTests(TestCase):
         # so we can verify messages that come in.
         cls.consumer_created = KafkaConsumer(
             "search-content-updated",
-            bootstrap_servers=KAFKA_SERVER,
+            bootstrap_servers=settings.KAFKA_SERVER,
             auto_offset_reset="earliest",
             enable_auto_commit=True,
             group_id="test-group-created-updated",
@@ -36,7 +33,7 @@ class KafkaIntegrationTests(TestCase):
 
         cls.consumer_deleted = KafkaConsumer(
             "search-content-deleted",
-            bootstrap_servers=KAFKA_SERVER,
+            bootstrap_servers=settings.KAFKA_SERVER,
             auto_offset_reset="earliest",
             enable_auto_commit=True,
             group_id="test-group-deleted",
