8fc95bdff57361040cae6f707a0f8383de5164ca
Kacper Prywata <kacper.prywata@ons.gov.uk>
2025-04-24 11:19:53 +0100

    Exclude migration from linting

--- cms/settings/dev.py
+++ cms/settings/dev.py
@@ -101,5 +101,6 @@ MIGRATION_LINTER_OPTIONS = {
         "0003_releasecalendarpage_datasets",
         "0003_footermenu_locale_footermenu_translation_key_and_more",  # Ignoring NOT NULL constraint on columns
         "0007_remove_glossaryterm_core_glossary_term_name_unique_and_more",  # Ignoring NOT NULL constraint
+        "0004_make_release_date_mandatory_and_rename_next_release_text",  # Ignoring NOT NULL and RENAMING constraints
     ],
 }
