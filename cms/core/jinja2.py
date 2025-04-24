92d519120c9517a276165a7b4c9dcce16bf93535
Nick Smith <nick.smith@torchbox.com>
2025-04-24 16:26:59 +0100

    docs: Add link to Python docs example
    Add link to example use of json.dumps(foo, default=callable)

--- cms/core/jinja2.py
+++ cms/core/jinja2.py
@@ -6,7 +6,12 @@ from jinja2 import ChainableUndefined
 
 
 def undefined_json_handler(obj: Any) -> Any:
-    """Custom JSON handler that can handle Nunjucks's ChainableUndefined objects."""
+    """Custom JSON handler that can handle Nunjucks's ChainableUndefined objects.
+
+    Python documentation, showing an example use of this pattern, passed as the
+    `default` parameter to json.dumps:
+    https://docs.python.org/3/library/json.html#:~:text=Specializing%20JSON%20object%20encoding
+    """
     if isinstance(obj, ChainableUndefined):
         return None
     raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
