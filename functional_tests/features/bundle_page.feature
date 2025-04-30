c02c27c3ddb06225bd1d8d560fdccbb89fa93429
AWitcherONS <ann.witcher@ons.gov.uk>
2025-04-30 15:43:18 +0100

    fix functional test errors

--- functional_tests/features/bundle_page.feature
+++ functional_tests/features/bundle_page.feature
@@ -1,24 +1,18 @@
 Feature: Publishing Officer can draft, edit, and publish bundles
-# Viewer
-# Publishing Admin
-# Publishing Officer
-    Background:
+
+
+    Scenario: A Publishing Officer can add Bundles with Publishing Officer as the creator.  The bundle can be seen and accessed.
+    Once the creator is removed from the bundle then the bundle can still be seen and accessed
+
         Given a Publishing Officer logs into the admin site
         And the user can see the Bundles menu item
-        When the user clicks the Bundles menu item
-        Then the user can add Bundles
-
-    Scenario: A Viewer can inspect Bundles details
-        When a Viewer logs into the admin site
-        And the viewer is in the preview team
-        Then the user can see the Bundle menu item
+        And the user can add Bundles created by user
+        And is ready for review
+        And has a preview team
+        And the user can see the Bundles menu item
+        And the user can inspect Bundle details
+        Then Bundle has the creator removed
+        And the user can see the Bundles menu item
         And the user can inspect Bundle details
 
-    Scenario: A Viewer can inspect Bundles details
-        Given a Publishing Admin logs into the admin site
-        And Bundle has the creator removed
-        When a Viewer logs into the admin site
-        And the Viewer is in the preview team
-        Then the user can see the Bundle menu item
-        And the user can inspect Bundle details
 
--- functional_tests/steps/bundles.py
+++ functional_tests/steps/bundles.py
@@ -5,7 +5,7 @@ from cms.bundles.enums import BundleStatus
 from cms.bundles.models import BundleTeam
 from cms.bundles.tests.factories import BundleFactory
 from cms.teams.models import Team
-from cms.users.models import User
+from functional_tests.step_helpers.users import create_user
 
 
 @step("a bundle has been created")
@@ -31,17 +31,6 @@ def the_viewer_is_in_the_preview_team(context: Context) -> None:
     user.teams.add(context.team)
 
 
-@step("a bundle has been created by '{user}'")
-def a_bundle_with_name_item_name_has_been_created_by_username(context: Context, user: User) -> None:
-    context.bundle = BundleFactory(created_by=user)
-
-
-@step("Bundle has the creator removed")
-def created_by_has_been_deleted(context: Context) -> None:
-    context.bundle.created_by = None
-    context.bundle.save(update_fields=["created_by"])
-
-
 @step("the user goes to the bundle creation page")
 def the_user_goes_to_the_bundle_creation_page(context: Context) -> None:
     context.page.goto(context.base_url + "/admin/bundle/new/")
@@ -65,3 +54,30 @@ def the_user_opens_page_chooser(context: Context) -> None:
 def the_locale_column_is_displayed(context: Context) -> None:
     modal = context.page.locator(".modal-body")
     modal.get_by_role("columnheader", name="Locale").is_visible()
+
+
+@step("the user can add Bundles created by user")
+def a_bundle_with_name_item_name_has_been_created_by_username(context: Context) -> None:
+    context.bundle = BundleFactory(created_by=context.user_data["user"])
+
+
+@step("Bundle has the creator removed")
+def created_by_has_been_deleted(context: Context) -> None:
+    context.bundle.created_by = None
+    context.bundle.save(update_fields=["created_by"])
+
+
+@step('a "{user_type}" logs into the admin site with username "{username}"')
+def user_logs_in_with_username(context: Context, user_type: str, username: str) -> None:
+    context.user_data = create_user(user_type)
+    context.page.goto(f"{context.base_url}/admin/login/")
+    context.page.get_by_placeholder("Enter your username").fill(context.user_data[username])
+    context.page.get_by_placeholder("Enter password").fill(context.user_data["password"])
+    context.page.get_by_role("button", name="Sign in").click()
+
+
+@step("the user can inspect Bundle details")
+def the_user_can_see_the_bundle_details(context: Context) -> None:
+    context.page.get_by_role("link", name="Bundles", exact=True).click()
+    context.page.get_by_role("button", name=f"More options for '{context.bundle.name}'").click()
+    context.page.get_by_role("link", name=f"Inspect '{context.bundle.name}'").click()
