2989218ffafbf29a62e8ee60b88220e3462b4b35
Jake Howard <jake.howard@torchbox.com>
2025-05-14 15:56:18 +0100

    Force `GenericRelation` targets to use the default DB connection
    This is a more complete work-around to https://code.djangoproject.com/ticket/36389 than 15395197c30df424961c595a5dcc60f710148599.
    
    This is a broad approach, but requires much less monkey-patching. Performance hopefully won't be majorly impacted.

--- cms/core/tests/test_read_only_connection.py
+++ cms/core/tests/test_read_only_connection.py
@@ -1,45 +1,10 @@
-from django.db import DEFAULT_DB_ALIAS, connections, router
+from django.db import connections
 from django.db.utils import InternalError
-from django.test import TestCase, override_settings
-from django.utils.connection import ConnectionDoesNotExist
-from wagtail_factories import ImageFactory
 
-from cms.core.db_router import READ_REPLICA_DB_ALIAS, ExternalEnvRouter
+from cms.core.db_router import READ_REPLICA_DB_ALIAS
 from cms.core.tests import TransactionTestCase
-from cms.images.models import CustomImage, Rendition
 from cms.users.models import User
 from cms.users.tests.factories import UserFactory
-
-
-@override_settings(IS_EXTERNAL_ENV=True)
-class ExternalReadOnlyConnectionTestCase(TestCase):
-    def test_cannot_write_to_disallowed_table(self):
-        """Test that disallowed models cannot be written to in external env."""
-        self.assertFalse(CustomImage.objects.exists())
-
-        with self.assertRaises(ConnectionDoesNotExist):
-            ImageFactory.create()
-
-        self.assertFalse(CustomImage.objects.exists())
-
-    def test_can_write_to_allowed_table(self):
-        """Test that allowed models can be written to in external env."""
-        self.assertEqual(Rendition.objects.count(), 0)
-
-        with override_settings(IS_EXTERNAL_ENV=False):
-            image = ImageFactory.create()
-
-        rendition = image.get_rendition("width-100")
-
-        # Confirm instance exists in DB
-        rendition.refresh_from_db()
-
-        self.assertEqual(Rendition.objects.count(), 1)
-
-    def test_uses_correct_connection(self):
-        """Test that the correct connection is used."""
-        self.assertEqual(router.db_for_read(CustomImage), DEFAULT_DB_ALIAS)  # TestCase runs in a transaction
-        self.assertEqual(router.db_for_write(CustomImage), ExternalEnvRouter.FAKE_BACKEND)
 
 
 class ReadOnlyConnectionTestCase(TransactionTestCase):
