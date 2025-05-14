01c7e0ad0d4494a7cd84555f6c62bc25285801fc
Jake Howard <jake.howard@torchbox.com>
2025-05-14 09:39:46 +0100

    Ensure functional tests use different DB port

--- cms/settings/functional_test.py
+++ cms/settings/functional_test.py
@@ -1,3 +1,5 @@
 from .test import *  # noqa: F403  # pylint: disable=wildcard-import,unused-wildcard-import
 
 REDIS_URL = "redis://localhost:16379"
+
+DATABASES["default"]["PORT"] = DATABASES["read_replica"]["PORT"] = 15432  # noqa: F405
