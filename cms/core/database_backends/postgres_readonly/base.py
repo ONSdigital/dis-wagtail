3804fe9618d97317c9b02585733e49c0e4b12215
Jake Howard <6527489+RealOrangeOne@users.noreply.github.com>
2025-05-16 11:26:30 +0100

    Force read only DB connection (#191)
    * Set replica connection to read-only in dev
    
    * Ensure pages are published and unpublished inside a transaction
    
    This works around https://code.djangoproject.com/ticket/36389. Wagtail only uses these APIs during page publishing.
    unpublishing
    
    * Ensure functional tests use different DB port
    
    * Remove duplicate database allow
    
    * Force `GenericRelation` targets to use the default DB connection
    
    This is a more complete work-around to https://code.djangoproject.com/ticket/36389 than 15395197c30df424961c595a5dcc60f710148599.
    
    This is a broad approach, but requires much less monkey-patching. Performance hopefully won't be majorly impacted.
    
    * Keep using replica in external environment
    
    The environment won't be doing write queries to the affected models
    
    * Improve doc string wording

---
+++ cms/core/database_backends/postgres_readonly/base.py
@@ -0,0 +1,11 @@
+from django.db.backends.postgresql.base import DatabaseWrapper as PostgresDatabaseWrapper
+
+
+class DatabaseWrapper(PostgresDatabaseWrapper):
+    """A modified database backend which forces the connection to be read-only."""
+
+    def init_connection_state(self) -> None:
+        super().init_connection_state()
+        with self.connection.cursor() as c:
+            c.execute("SET SESSION CHARACTERISTICS AS TRANSACTION READ ONLY;")
+        self.connection.set_read_only(True)
