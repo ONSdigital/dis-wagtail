03e8ff72175997e33c670bd7db97041b7f933e17
Dan Braghiș <dan.braghis@torchbox.com>
2025-05-23 15:50:38 +0100

    Use the DS header component
    Co-authored-by: janehughes <jane.hughes@torchbox.com>

--- cms/navigation/templatetags/navigation_tags.py
+++ cms/navigation/templatetags/navigation_tags.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Optional, TypedDict, cast
+from typing import TYPE_CHECKING, Literal, Optional, TypedDict, cast
 
 import jinja2
 from django.http import HttpRequest
@@ -14,15 +14,15 @@ BREACRUMBS_HOMEPAGE_DEPTH = 2
 
 
 class NavigationItem(TypedDict, total=False):
+    heading: str
     text: str
     url: str
     description: str
-    children: list["NavigationItem"]
+    groupItems: list["NavigationItem"]
 
 
 class ColumnData(TypedDict):
-    column: int
-    linksList: list[NavigationItem]
+    groups: list[NavigationItem]
 
 
 class FooterColumnData(TypedDict):
@@ -32,6 +32,7 @@ class FooterColumnData(TypedDict):
 
 def _extract_item(
     value: "StructValue",
+    text_key: Literal["text", "heading"],
     request: Optional["HttpRequest"] = None,
     include_description: bool = False,
 ) -> NavigationItem:
@@ -41,11 +42,11 @@ def _extract_item(
     item: NavigationItem = {}
 
     if value["external_url"]:
-        item["text"] = value["title"]
+        item[text_key] = value["title"]
         item["url"] = value["external_url"]
 
     elif value["page"] and value["page"].live:
-        item["text"] = value["title"] or value["page"].title
+        item[text_key] = value["title"] or value["page"].title
         item["url"] = value["page"].get_url(request=request)
 
     if include_description and "description" in value:
@@ -63,7 +64,9 @@ def main_menu_highlights(
 
     highlights = []
     for highlight in main_menu.highlights:
-        highlight_data = _extract_item(highlight.value, request=context.get("request"), include_description=True)
+        highlight_data = _extract_item(
+            highlight.value, request=context.get("request"), include_description=True, text_key="heading"
+        )
         if highlight_data:
             highlights.append(highlight_data)
 
@@ -78,28 +81,30 @@ def main_menu_columns(context: jinja2.runtime.Context, main_menu: Optional["Main
     def extract_section_data(
         section: "StructValue", request: Optional["HttpRequest"] = None
     ) -> Optional[NavigationItem]:
-        section_data = _extract_item(section["section_link"], request=request, include_description=False)
+        section_data = _extract_item(
+            section["section_link"], request=request, include_description=False, text_key="heading"
+        )
         if not section_data:
             return None
 
         children = []
         for link in section["links"]:
-            link_data = _extract_item(link, request=request, include_description=False)
+            link_data = _extract_item(link, request=request, include_description=False, text_key="text")
             if link_data:
                 children.append(link_data)
 
-        section_data["children"] = children
+        section_data["groupItems"] = children
         return section_data
 
     items: list[ColumnData] = []
-    for idx, column in enumerate(main_menu.columns):
-        column_data: ColumnData = {"column": idx, "linksList": []}
+    for column in main_menu.columns:
+        column_data: ColumnData = {"groups": []}
 
         for section in column.value["sections"]:
             if section_data := extract_section_data(section, context.get("request")):
-                column_data["linksList"].append(section_data)
+                column_data["groups"].append(section_data)
 
-        if column_data["linksList"]:
+        if column_data["groups"]:
             items.append(column_data)
 
     return items
