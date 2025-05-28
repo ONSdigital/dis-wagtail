605b3764b7c3205cf5032e022864ba36bbbd664d
AWitcherONS <ann.witcher@ons.gov.uk>
2025-05-28 15:15:34 +0100

    resolve comments

--- functional_tests/features/bundle_page.feature
+++ functional_tests/features/bundle_page.feature
@@ -4,27 +4,29 @@ Feature: Publishing Officer can draft, edit, and publish bundles
         Given a Publishing Officer logs into the admin site
         When a bundle has been created with name "Test 1" and creator "UserName"
         Then the user can see the Bundles menu item
-        And the bundle menu shows bundle with name "Test 1" and creator "UserName"
+        And the user goes to the bundle menu page
+        And the bundle menu shows bundle with name "Test 1" and created by is not empty
 
     Scenario: A Publishing Officer adds a bundle and the inspect page for the bundle shows the created by as populated
         Given a Publishing Officer logs into the admin site
         And the user can see the Bundles menu item
         And a bundle has been created with name "Test 1" and creator "UserName"
         And the user can see the Bundles menu item
-        When the user goes to the bundle inspect page with name "Test 1"
-        Then the user can inspect Bundle details with name "Test 1" and creator "UserName"
+        When the user goes to the bundle inspect page
+        Then the user can inspect Bundle details and creator
 
     Scenario: A Publishing Admin can see the Bundles Menu and that a bundle with creator removed
         Given a Publishing Officer logs into the admin site
         When a bundle has been created with name "Test 1" and creator "UserName"
         And the bundle has creator removed
         Then the user can see the Bundles menu item
-        And the bundle menu shows bundle with name "Test 1" and no creator
+        And the user goes to the bundle menu page
+        And the bundle menu shows bundle with name "Test 1" and created by is empty
 
     Scenario: A Publishing Admin can see the Bundle Inspect and that the bundle with creator removed
         Given a Publishing Officer logs into the admin site
         And a bundle has been created with name "Test 1" and creator "UserName"
         And the bundle has creator removed
-        When the user can see the Bundles menu item
-        Then the bundle menu shows bundle with name "Test 1" and no creator
+        When the user goes to the bundle inspect page
+        Then the user can inspect Bundle details and creator has no entry
 
--- functional_tests/steps/bundles.py
+++ functional_tests/steps/bundles.py
@@ -1,14 +1,13 @@
-import datetime
 
 from behave import step  # pylint: disable=no-name-in-module
 from behave.runner import Context
-from cms.users.models import User
+from playwright.sync_api import expect
 
 from cms.bundles.enums import BundleStatus
 from cms.bundles.models import BundleTeam
 from cms.bundles.tests.factories import BundleFactory
 from cms.teams.models import Team
-from playwright.sync_api import expect
+from cms.users.models import User
 
 
 @step("a bundle has been created")
@@ -58,47 +57,60 @@ def the_locale_column_is_displayed(context: Context) -> None:
     modal = context.page.locator(".modal-body")
     modal.get_by_role("columnheader", name="Locale").is_visible()
 
-@step('the user can inspect Bundle details with name "{bundle_name}" and creator "{creator}"')
-def the_user_can_see_the_bundle_details_with_creator(context: Context, bundle_name: str, creator: str) -> None:
-    expect(context.page.get_by_text("Created by")).to_be_visible()
-    expect(context.page.get_by_text(creator)).to_be_visible()
-
+# bundle create amend
+# ruff: noqa: S106
 @step('a bundle has been created with name "{bundle_name}" and creator "{creator}"')
-def a_bundle_has_been_created(context: Context, bundle_name: str, creator: str) -> None:
-    context.user = User.objects.create_user(username=creator,email= "email@example.com",password= "password",first_name= "first_Name",last_name= "Last_Name")
+def a_bundle_has_been_created_with_user(context: Context, bundle_name: str, creator: str) -> None:
+    context.user = User.objects.create_user(username=creator,
+                                            email= "email@example.com",
+                                            password= "password",
+                                            first_name= "First_Name",
+                                            last_name= "Last_Name")
     context.bundle = BundleFactory(name = bundle_name, created_by = context.user )
 
-@step('the bundle menu shows bundle with name "{bundle_name}" and creator "{creator}"')
-def bundle_menu_show(context: Context, bundle_name: str, creator: str) -> None:
-    context.page.goto(context.base_url + "/admin/bundle/")
-    if hasattr(context, 'user'):
-        fullname = context.user.get_full_name()
-    else:
-        fullname = context.user_data["full_name"]
-    expect(context.page.get_by_role("table")).to_contain_text(bundle_name)
-    expect(context.page.get_by_role("table")).to_contain_text(fullname)
-
-@step('the user goes to the bundle inspect page with name "{bundle_name}"')
-def go_to_bundle_inspect(context: Context, bundle_name: str) -> None:
-    context.page.goto(context.base_url + "/admin/bundle/inspect/1/")
 
 @step('the bundle has creator removed')
 def delete_bundle_creator(context: Context) -> None:
         context.bundle.created_by = None
         context.bundle.save(update_fields=["created_by"])
 
-@step('the bundle menu shows bundle with name "{bundle_name}" and no creator')
-def bundle_menu_show(context: Context, bundle_name: str) -> None:
+# bundle goto
+@step('the user goes to the bundle inspect page')
+def go_to_bundle_inspect(context: Context) -> None:
+    context.page.goto(context.base_url + "/admin/bundle/inspect/1/")
+
+
+@step('the user goes to the bundle menu page')
+def go_to_bundle_menu(context: Context) -> None:
+    context.page.goto(context.base_url + "/admin/bundle/")
+
+# bundle Menu
+@step('the bundle menu shows bundle with name "{bundle_name}" and created by is not empty')
+def bundle_menu_contains_value(context: Context, bundle_name: str) -> None:
+    fullname = context.user_data["full_name"]
     if hasattr(context, 'user'):
         fullname = context.user.get_full_name()
-    else:
-        fullname = context.user_data["full_name"]
 
-    context.page.goto(context.base_url + "/admin/bundle/")
+    expect(context.page.get_by_role("table")).to_contain_text(bundle_name)
+    expect(context.page.get_by_role("table")).to_contain_text(fullname)
+
+
+@step('the bundle menu shows bundle with name "{bundle_name}" and created by is empty')
+def bundle_menu_does_not_contain_value(context: Context, bundle_name: str) -> None:
+    fullname = context.user_data["full_name"]
+    if hasattr(context, 'user'):
+        fullname = context.user.get_full_name()
+
     expect(context.page.get_by_role("table")).to_contain_text(bundle_name)
     expect(context.page.get_by_role("table")).not_to_contain_text(fullname)
 
-@step('the user can inspect Bundle details with name "{bundle_name}" and creator has no entry')
-def bundle_inspect_show(context: Context, bundle_name: str) -> None:
+# bundle Inspect
+@step('the user can inspect Bundle details and creator')
+def the_user_can_see_the_bundle_details_with_creator(context: Context) -> None:
     expect(context.page.get_by_text("Created by")).to_be_visible()
-    expect(context.page.get_by_text(context.user.get_username())).not_to_be_visible();
+    expect(context.page.get_by_text(context.user.get_username())).to_be_visible()
+
+@step('the user can inspect Bundle details and creator has no entry')
+def bundle_inspect_show(context: Context) -> None:
+    expect(context.page.get_by_text("Created by")).to_be_visible()
+    expect(context.page.get_by_text(context.user.get_username())).not_to_be_visible()
