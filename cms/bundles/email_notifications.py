1523d5c7355956636c585d4fb934614683a26096
Kacper Prywata <kacper.prywata@ons.gov.uk>
2025-05-28 16:23:18 +0100

    Silence linter errors

--- cms/bundles/email_notifications.py
+++ cms/bundles/email_notifications.py
@@ -12,7 +12,7 @@ def send_team_added_to_bundle_in_review_email(bundle_team: BundleTeam) -> None:
     """Send email notification to the team members when a team is assigned to a bundle in review."""
     bundle = bundle_team.parent
     team = bundle_team.team
-    active_user_emails = team.users.filter(is_active=True).values_list("email", flat=True)
+    active_user_emails = team.users.filter(is_active=True).values_list("email", flat=True)  # type: ignore[attr-defined]
     send_mail(
         subject=f'Your team "{team.name}" was added to a bundle In review',  # type: ignore[attr-defined]
         message=f"The preview team {team.name} you are a member of "  # type: ignore[attr-defined]
@@ -27,12 +27,12 @@ def send_bundle_status_changed_to_in_review_email(bundle_team: BundleTeam) -> No
     """Send email notification to the team members when a bundle status changes to In Review."""
     bundle = bundle_team.parent
     team = bundle_team.team
-    active_user_emails = team.users.filter(is_active=True).values_list("email", flat=True)
+    active_user_emails = team.users.filter(is_active=True).values_list("email", flat=True)  # type: ignore[attr-defined]
     send_mail(
         subject=f'Bundle "{bundle.name}" status changed to In Review',
-        message=f'''You are a reviewer in the team "{team.name}"
-        Bundle "{bundle.name}" status changed to In Review
-        URL: {bundle.inspect_url}''',
+        message=f'You are a reviewer in the team "{team.name}"'  # type: ignore[attr-defined]
+        + 'Bundle "{bundle.name}" status changed to In Review'
+        + "URL: {bundle.inspect_url}",
         from_email=settings.FROM_EMAIL,
         recipient_list=active_user_emails,
     )
@@ -42,7 +42,7 @@ def send_bundle_published_email(bundle_team: BundleTeam) -> None:
     """Send email notification to the team members when a bundle is published."""
     bundle = bundle_team.parent
     team = bundle_team.team
-    active_user_emails = team.users.filter(is_active=True).values_list("email", flat=True)
+    active_user_emails = team.users.filter(is_active=True).values_list("email", flat=True)  # type: ignore[attr-defined]
     send_mail(
         subject=f'Bundle "{bundle.name}" has been published',
         message=f'You are a reviewer in the team "{team.name}" '  # type: ignore[attr-defined]
