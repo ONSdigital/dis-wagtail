c719657308496a8e5abe76e9494f75289de74319
Nick Smith <nick.smith@torchbox.com>
2025-05-28 17:22:57 +0100

    Configure mobile/desktop tick interval independently (#214)
    * feat: Configure mobile/desktop tick interval independently
    
    * refactor: Use assertNotIn instead of expecting errors
    
    * test: Add test of asymmetrically setting tick intervals

--- cms/datavis/tests/test_line_chart_block.py
+++ cms/datavis/tests/test_line_chart_block.py
@@ -113,3 +113,48 @@ class LineChartBlockTestCase(BaseChartBlockTestCase):
         config = self.get_component_config()
         for item in config["series"]:
             self.assertEqual(True, item["connectNulls"])
+
+    def test_tick_interval_defaults(self):
+        self.raw_data["x_axis"]["tick_interval_mobile"] = None
+        self.raw_data["x_axis"]["tick_interval_desktop"] = None
+        self.raw_data["y_axis"]["tick_interval_mobile"] = None
+        self.raw_data["y_axis"]["tick_interval_desktop"] = None
+        self.block.clean(self.get_value())
+        for axis, key in [
+            ("xAxis", "tickIntervalMobile"),
+            ("xAxis", "tickIntervalDesktop"),
+            ("yAxis", "tickIntervalMobile"),
+            ("yAxis", "tickIntervalDesktop"),
+        ]:
+            with self.subTest(axis=axis, key=key):
+                axis_config = self.get_value().block.get_component_config(self.get_value())[axis]
+                self.assertNotIn(key, axis_config)
+
+    def test_tick_interval(self):
+        self.raw_data["x_axis"]["tick_interval_mobile"] = 1
+        self.raw_data["x_axis"]["tick_interval_desktop"] = 2
+        self.raw_data["y_axis"]["tick_interval_mobile"] = 3
+        self.raw_data["y_axis"]["tick_interval_desktop"] = 4
+        self.block.clean(self.get_value())
+        for axis, key, expected in [
+            ("xAxis", "tickIntervalMobile", 1),
+            ("xAxis", "tickIntervalDesktop", 2),
+            ("yAxis", "tickIntervalMobile", 3),
+            ("yAxis", "tickIntervalDesktop", 4),
+        ]:
+            with self.subTest(axis=axis, key=key):
+                self.assertEqual(expected, self.get_value().block.get_component_config(self.get_value())[axis][key])
+
+    def test_setting_only_one_tick_interval(self):
+        self.raw_data["x_axis"]["tick_interval_mobile"] = 5
+        self.raw_data["y_axis"]["tick_interval_desktop"] = 10
+        self.block.clean(self.get_value())
+        config = self.get_value().block.get_component_config(self.get_value())
+        for axis, set_key, expected, unset_key in [
+            ("xAxis", "tickIntervalMobile", 5, "tickIntervalDesktop"),
+            ("yAxis", "tickIntervalDesktop", 10, "tickIntervalMobile"),
+        ]:
+            with self.subTest(axis=axis, key=set_key, condition="configured"):
+                self.assertEqual(expected, config[axis][set_key])
+            with self.subTest(axis=axis, key=unset_key, condition="not configured"):
+                self.assertNotIn(unset_key, config[axis])
