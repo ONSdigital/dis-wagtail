cf2fe9b987b63defc430ddb7df1e7afff1b0bb97
Maciek Baron <1668712+MaciekBaron@users.noreply.github.com>
2025-06-03 10:45:36 +0100

    Use hyphen for URLs instead of underscore
    Co-authored-by: Dan Braghiș <31622+zerolab@users.noreply.github.com>

--- cms/bundles/admin_urls.py
+++ cms/bundles/admin_urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("add/<int:page_to_add_id>/", AddToBundleView.as_view(), name="add_to_bundle"),
     path("preview/<int:bundle_id>/page/<int:page_id>/", PreviewBundleView.as_view(), name="preview"),
     path(
-        "preview/<int:bundle_id>/release_calendar/",
+        "preview/<int:bundle_id>/release-calendar/",
         PreviewBundleReleaseCalendarView.as_view(),
         name="preview_release_calendar",
     ),
