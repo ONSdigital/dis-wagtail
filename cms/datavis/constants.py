fcce18f961cf222cbb322d9b2d101ec2fd4b414c
Nick Smith <nick.smith@torchbox.com>
2025-06-06 14:39:02 +0100

    Remove unused charts preview code (#229)

--- cms/datavis/constants.py
+++ cms/datavis/constants.py
@@ -1,20 +1,6 @@
 from enum import Enum
 
 from django.db.models import TextChoices
-from django.utils.translation import gettext_lazy as _
-
-
-class AnnotationStyle(TextChoices):
-    ABOVE_LINE = "above_line", _("Above line")
-    BELOW_LINE = "below_line", _("Below line")
-
-
-class MarkerStyle(TextChoices):
-    __empty__ = _("No markers")
-    CIRCLE = "circle", _("Circle")
-    SQUARE = "square", _("Square")
-    TRIANGLE = "triangle", _("Triangle")
-    DIAMOND = "diamond", _("Diamond")
 
 
 class HighchartsTheme(TextChoices):
