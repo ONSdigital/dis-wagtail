ff655ad404b2d4b078fbaa542ceba79ca6f5a7b1
Maciek Baron <1668712+MaciekBaron@users.noreply.github.com>
2025-07-02 14:17:05 +0100

    RelatedLinksBlock and LinkBlock block improvements (#253)
    * Add initial link block changes
    
    * Use lazy gettext
    
    * Add type display, initial tests
    
    * Fix and expand tests
    
    * Update IndexPage and expand tests
    
    * Allow for no release_date
    
    * Fix page type label for featured items
    
    * Remove unnecessary check for value
    
    Co-authored-by: Dan Braghiș <31622+zerolab@users.noreply.github.com>
    
    * Update comment for more clarity
    
    Co-authored-by: Dan Braghiș <31622+zerolab@users.noreply.github.com>
    
    * Improve comment
    
    Co-authored-by: Dan Braghiș <31622+zerolab@users.noreply.github.com>
    
    * Fix issue related to no content type
    
    * Use RelatedLinksBlock
    
    * Revert "Use RelatedLinksBlock"
    
    This reverts commit 5e4c71e0d8d22dad966da16b26bd75dc0817a58f.
    
    * Make release content section use onsDocumentList
    
    * Refactor content type and release date logic
    
    * Fix tests
    
    * Change way content type for Pages is retrieved
    
    * Placate mypy
    
    * Update cms/core/blocks/base.py
    
    Co-authored-by: Dan Braghiș <31622+zerolab@users.noreply.github.com>
    
    * Update validation message and improve typing for mypy
    
    * Update type name in tests now that we use label
    
    * Add date to type
    
    * Standarise output for index page
    
    * Add publication_date property
    
    * Fix linting, revert unnecessary file
    
    * Set default value for prefix
    
    ---------
    
    Co-authored-by: Maciej Baron <MaciekBaron@users.noreply.github.com>
    Co-authored-by: Dan Braghiș <31622+zerolab@users.noreply.github.com>

---
+++ cms/core/enums.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class RelatedContentType(models.TextChoices):
+    """Related content types."""
+
+    ARTICLE = "ARTICLE", _("Article")
+    DATASET = "DATASET", _("Dataset")
+    METHODOLOGY = "METHODOLOGY", _("Methodology")
+    TIME_SERIES = "TIME_SERIES", _("Time series")
+    TOPIC = "TOPIC", _("Topic")
