67c807347886609b03bcd330b00e554d4bd10337
Dan Braghiș <31622+zerolab@users.noreply.github.com>
2025-07-21 09:33:11 +0100

    Bundles - UX/user feedback tweaks, part 1 (#313)
    * Update bundle listing columns
    - removes: added, added by, preview teams, approved at, approved by
    - ensures the "bundles in preview" dashboard panel is updated for
      consistency
    * Filter out published bundles by default, but allow showing via filters
    * Make all bundle index columns sortable, and sort by name by default
    * Remove obsolete functional test
    * Make dashboard columns order/label match the bundle index
    * Keep manual bundles last when sorting by published date, always
    * Add an updated_at field
    * Improve migration for setting Bundle.updated_at

--- cms/bundles/tests/test_viewsets.py
+++ cms/bundles/tests/test_viewsets.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from unittest import mock
 
 import time_machine
+from django.db.models import F, OrderBy
 from django.test import TestCase, override_settings
 from django.urls import reverse
 from wagtail.admin.panels import get_edit_handler
@@ -50,10 +51,10 @@ class BundleViewSetTestCaseBase(WagtailTestUtils, TestCase):
         cls.bundle_index_url = reverse("bundle:index")
         cls.bundle_add_url = reverse("bundle:add")
 
-        cls.published_bundle = BundleFactory(published=True, name="Publish Bundle")
+        cls.published_bundle = BundleFactory(published=True, name="Publish test Bundle")
         cls.published_bundle_edit_url = reverse("bundle:edit", args=[cls.published_bundle.id])
 
-        cls.approved_bundle = BundleFactory(approved=True, name="Approve Bundle")
+        cls.approved_bundle = BundleFactory(approved=True, name="Approve test Bundle")
         cls.approved_bundle_edit_url = reverse("bundle:edit", args=[cls.approved_bundle.id])
 
         cls.in_review_bundle = BundleFactory(in_review=True, name="Preview Bundle")
@@ -466,16 +467,23 @@ class BundleIndexViewTestCase(BundleViewSetTestCaseBase):
         self.assertNotContains(response, self.published_bundle_edit_url)
 
         self.assertContains(response, BundleStatus.DRAFT.label, 2)  # status + status filter
-        self.assertContains(response, BundleStatus.PUBLISHED.label, 2)  # status + status filter
         self.assertContains(response, BundleStatus.APPROVED.label, 2)  # status + status filter
+        self.assertContains(response, BundleStatus.PUBLISHED.label, 1)  # status filter
 
-        self.assertContains(response, self.published_bundle.name)
         self.assertContains(response, self.approved_bundle.name)
+        self.assertContains(response, self.bundle.name)
+        self.assertNotContains(response, self.published_bundle.name)
 
-    def test_index_view_search(self):
-        response = self.client.get(f"{self.bundle_index_url}?q=publish")
+    def test_index_view_contains_published_bundle_when_status_filter_applied(self):
+        response = self.client.get(self.bundle_index_url, query_params={"status": BundleStatus.PUBLISHED})
         self.assertContains(response, self.published_bundle.name)
         self.assertNotContains(response, self.approved_bundle.name)
+        self.assertNotContains(response, self.bundle.name)
+
+    def test_index_view_search(self):
+        response = self.client.get(self.bundle_index_url, query_params={"q": "test"})
+        self.assertContains(response, self.approved_bundle.name)
+        self.assertNotContains(response, self.published_bundle.name)
 
     def test_index_view__previewers__contains_only_relevant_bundles(self):
         self.client.force_login(self.bundle_viewer)
@@ -490,6 +498,26 @@ class BundleIndexViewTestCase(BundleViewSetTestCaseBase):
         self.assertNotContains(response, self.published_bundle.name)
         self.assertNotContains(response, self.approved_bundle.name)
         self.assertNotContains(response, self.another_in_review_bundle.name)
+
+    def test_ordering(self):
+        """Checks that the correct ordering is applied."""
+        cases = {
+            "": ("name",),
+            "name": ("name",),
+            "-name": ("-name",),
+            "scheduled_publication_date": (OrderBy(F("release_date"), descending=False, nulls_last=True),),
+            "-scheduled_publication_date": (OrderBy(F("release_date"), descending=True, nulls_last=True),),
+            "status": (OrderBy(F("status_label"), descending=False),),
+            "-status": (OrderBy(F("status_label"), descending=True),),
+            "invalid_ordering": ("name",),
+        }
+        for param, order_by in cases.items():
+            with self.subTest(param=param):
+                response = self.client.get(self.bundle_index_url, query_params={"ordering": param})
+                self.assertEqual(
+                    response.context_data["object_list"].query.order_by,
+                    order_by,
+                )
 
 
 class BundleChooserViewsetTestCase(BundleViewSetTestCaseBase):
