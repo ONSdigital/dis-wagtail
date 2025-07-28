3f7c468657a1a07948c91ca7210564bca41105f5
Dan Braghiș <dan.braghis@torchbox.com>
2025-07-28 14:29:02 +0100

    Take bundle out of ready to publish if any bundled page workflow is cancelled

--- cms/bundles/signal_handlers.py
+++ cms/bundles/signal_handlers.py
@@ -1,11 +1,20 @@
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from wagtail.log_actions import log
+from wagtail.models import Page, WorkflowState
+from wagtail.signals import workflow_cancelled
 
 from cms.bundles.enums import BundleStatus
 from cms.bundles.models import Bundle, BundleTeam
 from cms.bundles.notifications.email import send_bundle_in_review_email, send_bundle_published_email
+from cms.bundles.notifications.slack import notify_slack_of_status_change
+
+if TYPE_CHECKING:
+    from django_stubs_ext import StrOrPromise
+
+    from cms.users.models import User
 
 
 @receiver(post_save, sender=BundleTeam)
@@ -39,3 +48,46 @@ def handle_bundle_publication(instance: Bundle, **kwargs: Any) -> None:
         ]
         for bundle_team in active_bundle_teams:
             send_bundle_published_email(bundle_team=bundle_team)
+
+
+@receiver(workflow_cancelled, sender=WorkflowState)
+def check_and_unschedule_bundle_on_page_workflow_cancellation(
+    instance: WorkflowState, user: "User", **kwargs: Any
+) -> None:
+    if not isinstance(instance.content_object, Page):
+        return
+
+    page = instance.content_object.specific_deferred
+    if not page.active_bundle:
+        return
+
+    if page.active_bundle.status != BundleStatus.APPROVED:
+        return
+
+    bundle: Bundle = page.active_bundle
+    bundle.status = BundleStatus.IN_REVIEW
+    bundle.save()
+
+    original_status: StrOrPromise = BundleStatus.APPROVED.label
+    log_kwargs: dict = {
+        "content_changed": False,
+        "data": {
+            "old": original_status,
+            "new": BundleStatus.IN_REVIEW.label,
+        },
+    }
+
+    context_message = (
+        f"The bundle is no longer ready to publish because a bundled page ({page.get_admin_display_title()}) "
+        "is no longer ready to publish."
+    )
+    notify_slack_of_status_change(
+        bundle, original_status, user=user, url=bundle.full_inspect_url, context_message=context_message
+    )
+
+    # now log the status change
+    log(
+        action="bundles.update_status",
+        instance=bundle,
+        **log_kwargs,
+    )
--- cms/bundles/tests/test_signal_handlers.py
+++ cms/bundles/tests/test_signal_handlers.py
@@ -1,11 +1,14 @@
 from datetime import timedelta
+from unittest.mock import patch
 
 from django.core import mail
 from django.core.management import call_command
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 from django.utils import timezone
-from wagtail.test.utils.form_data import inline_formset, nested_form_data
+from wagtail.models import ModelLogEntry
+from wagtail.test.utils import WagtailTestUtils
+from wagtail.test.utils.form_data import inline_formset, nested_form_data, rich_text, streamfield
 
 from cms.articles.tests.factories import StatisticalArticlePageFactory
 from cms.bundles.enums import BundleStatus
@@ -13,6 +16,7 @@ from cms.bundles.models import BundleTeam
 from cms.bundles.tests.factories import BundleFactory, BundlePageFactory
 from cms.teams.tests.factories import TeamFactory
 from cms.users.tests.factories import UserFactory
+from cms.workflows.tests.utils import mark_page_as_ready_for_review, mark_page_as_ready_to_publish
 
 
 class TestNotifications(TestCase):
@@ -153,3 +157,72 @@ class TestNotifications(TestCase):
 
         # Ensure no additional notification is sent
         self.assertEqual(len(mail.outbox), 0)
+
+
+@override_settings(SLACK_NOTIFICATIONS_WEBHOOK_URL="https://slack.example.com")
+@patch("cms.bundles.signal_handlers.notify_slack_of_status_change")
+class WorkflowSignalsTestCase(WagtailTestUtils, TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.superuser = cls.create_superuser(username="admin")
+        cls.bundle = BundleFactory(name="The Bundle", status=BundleStatus.DRAFT)
+        cls.statistical_article_page = StatisticalArticlePageFactory()
+        BundlePageFactory(parent=cls.bundle, page=cls.statistical_article_page)
+
+        cls.edit_page_url = reverse("wagtailadmin_pages:edit", args=[cls.statistical_article_page.id])
+
+    def get_raw_form_data(self):
+        return {
+            "title": self.statistical_article_page.title,
+            "slug": self.statistical_article_page.slug,
+            "summary": rich_text(self.statistical_article_page.summary),
+            "main_points_summary": rich_text(self.statistical_article_page.main_points_summary),
+            "release_date": self.statistical_article_page.release_date,
+            "content": streamfield(
+                [("section", {"title": "Test", "content": streamfield([("rich_text", rich_text("text"))])})]
+            ),
+            "datasets": streamfield([]),
+            "dataset_sorting": "AS_SHOWN",
+            "corrections": streamfield([]),
+            "notices": streamfield([]),
+            "headline_figures": streamfield([]),
+            "featured_chart": streamfield([]),
+        }
+
+    def setUp(self):
+        self.client.force_login(self.superuser)
+
+    def test_cancelling_page_workflow_when_ready_to_publish_moves_linked_bundle_to_preview(self, mock_notify):
+        mark_page_as_ready_to_publish(self.statistical_article_page, self.superuser)
+        self.bundle.status = BundleStatus.APPROVED
+        self.bundle.save(update_fields=["status"])
+
+        self.assertFalse(ModelLogEntry.objects.filter(action="bundles.update_status").exists())
+
+        data = self.get_raw_form_data()
+        data["action-cancel-workflow"] = "Cancel workflow"
+
+        self.client.post(self.edit_page_url, nested_form_data(data), follow=True)
+
+        self.bundle.refresh_from_db()
+        self.assertEqual(self.bundle.status, BundleStatus.IN_REVIEW)
+
+        self.assertTrue(mock_notify.called)
+
+        self.assertTrue(ModelLogEntry.objects.filter(action="bundles.update_status").exists())
+
+    def test_cancelling_page_workflow_when_in_preview_doesnt_change_linked_bundle_status(self, mock_notify):
+        self.assertFalse(ModelLogEntry.objects.filter(action="bundles.update_status").exists())
+
+        mark_page_as_ready_for_review(self.statistical_article_page, self.superuser)
+
+        data = self.get_raw_form_data()
+        data["action-cancel-workflow"] = "Cancel workflow"
+
+        self.client.post(self.edit_page_url, nested_form_data(data), follow=True)
+
+        self.bundle.refresh_from_db()
+        self.assertEqual(self.bundle.status, BundleStatus.DRAFT)
+
+        self.assertFalse(mock_notify.called)
+        self.assertFalse(ModelLogEntry.objects.filter(action="bundles.update_status").exists())
