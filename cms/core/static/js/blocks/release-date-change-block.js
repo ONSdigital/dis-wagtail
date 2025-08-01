9880aec86d147f7831677caf5d12d4434c6e6fda
Kacper Prywata <kacper.prywata@ons.gov.uk>
2025-08-01 11:23:07 +0100

    Add comment explaining the help text

--- cms/core/static/js/blocks/release-date-change-block.js
+++ cms/core/static/js/blocks/release-date-change-block.js
@@ -29,6 +29,7 @@ class ReleaseDateChangeBlockDefinition extends ReadonlyStructBlockDefinition {
       if (this.previous_date) {
         previousDateInput.value = this.previous_date;
       } else {
+        // Indicate that the empty read-only field will be auto-populated after saving
         helpText.innerHTML = 'This field will be auto-populated once the page is saved.';
       }
     }
