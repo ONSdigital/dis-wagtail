fbf20336c19cb44728b1770f49d997de36660d03
Dan Braghiș <dan.braghis@torchbox.com>
2025-08-06 11:36:44 +0100

    Override Page._get_site_root_paths for smarter domain alternatives handling

--- cms/core/models/base.py
+++ cms/core/models/base.py
@@ -29,7 +29,7 @@ if TYPE_CHECKING:
     from wagtail.contrib.settings.models import (
         BaseSiteSetting as _WagtailBaseSiteSetting,
     )
-    from wagtail.models import Site
+    from wagtail.models.sites import Site, SiteRootPath
 
     class WagtailBaseSiteSetting(_WagtailBaseSiteSetting, models.Model):
         """Explicit class definition for type checking. Indicates we're inheriting from Django's model."""
@@ -214,6 +214,26 @@ class BasePage(PageLDMixin, ListingFieldsMixin, SocialFieldsMixin, Page):  # typ
             return cast(str, canonical_page.get_full_url(request=request) + resolver_match.route)
 
         return cast(str, canonical_page.get_full_url(request=request))
+
+    def _get_site_root_paths(self, request: Optional["HttpRequest"] = None) -> list["SiteRootPath"]:
+        """Extends the core Page._get_site_root_paths to account for alternative domains."""
+        if not getattr(settings, "CMS_USE_SUBDOMAIN_LOCALES", False):
+            return cast(list["SiteRootPath"], super()._get_site_root_paths(request=request))
+
+        from cms.locale.utils import get_mapped_site_root_paths  # pylint: disable=import-outside-toplevel
+
+        cache_object = request if request else self
+        try:
+            # pylint: disable=protected-access
+            cached_paths: list[SiteRootPath] = cache_object._wagtail_cached_site_root_paths  # type: ignore[union-attr]
+            # pylint: enable=protected-access
+            return cached_paths
+        except AttributeError:
+            paths = get_mapped_site_root_paths(request.get_host() if request is not None else None)
+            # pylint: disable=protected-access,attribute-defined-outside-init
+            cache_object._wagtail_cached_site_root_paths = paths  # type: ignore[union-attr]
+            # pylint: enable=protected-access,attribute-defined-outside-init
+            return paths
 
 
 class BaseSiteSetting(WagtailBaseSiteSetting):
