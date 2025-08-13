f71ab0834533f85d5fcc206247134fe13dffea42
Mebin Abraham <mebin95+work@gmail.com>
2025-08-13 09:33:04 +0100

    Initial set up

--- cms/demo/models.py
+++ cms/demo/models.py
@@ -1,6 +1,9 @@
+from typing import ClassVar
+
 from django.db import models
+from django.db.models import BaseConstraint
+
 
 class Member(models.Model):
     organisation = models.CharField(max_length=100)
     full_name = models.CharField(max_length=120)
-    # email will be added later
