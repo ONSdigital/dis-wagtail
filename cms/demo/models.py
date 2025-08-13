e26e0adfd49e6a11ce988bf37b65c5bb43ba115f
Mebin Abraham <mebin95+work@gmail.com>
2025-08-13 09:25:13 +0100

    Initial set up

--- cms/demo/models.py
+++ cms/demo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
-
-class Widget(models.Model):
-    name = models.CharField(max_length=100)
+class Member(models.Model):
+    organisation = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=120)
+    # email will be added later
