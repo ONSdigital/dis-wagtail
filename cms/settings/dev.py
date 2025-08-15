137c24ea6a0c651994aef88404c6094aab7433f4
Dan Braghiș <dan.braghis@torchbox.com>
2025-08-15 09:13:25 +0100

    Placate linters

--- cms/settings/dev.py
+++ cms/settings/dev.py
@@ -135,6 +135,7 @@ MIGRATION_LINTER_OPTIONS = {
         "0008_delete_systemmessagessettings",  # Ignoring dropping table
         "0007_topicpagerelatedarticle_external_url_and_more",  # Ignoring NOT NULL constraint
         "0006_bundle_bundle_api_id_bundledataset_content_api_id",  # Ignoring NOT NULL constraint
+        "0002_alter_dataset_version",
     ],
 }
 
