10cace9b8d67c3ab9afea4fb6c198301b3285b55
sanjeevz3009 <sanjeevz1717@gmail.com>
2025-09-30 12:05:44 +0100

    Broker container integration for functional-tests moved into the functional tests compose stack - docker-compose-dev.yml: broker container_name field removed so docker can automatically name the containers for local dev and running alongside for functional tests. - ci.yml: Broker container job removed. - KAFKA_SERVERS added in the functional_test.py we can run the functional tests against the functional tests broker container. - functional_tests/docker-compose-dev.yml updated to include and override broker service now. - functional_tests/docker-compose.yml to include and override broker service

--- cms/settings/functional_test.py
+++ cms/settings/functional_test.py
@@ -5,3 +5,5 @@ REDIS_URL = "redis://localhost:16379"
 DATABASES["default"]["PORT"] = DATABASES["read_replica"]["PORT"] = 15432  # noqa: F405
 
 SEARCH_INDEX_PUBLISHER_BACKEND = "kafka"
+
+KAFKA_SERVERS = os.getenv("KAFKA_SERVERS", "localhost:19094").split(",")
