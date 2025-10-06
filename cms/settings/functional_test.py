504f8a99ab264dfb2894115d95b9adabfad34be2
sanjeevz3009 <sanjeevz1717@gmail.com>
2025-10-06 15:25:55 +0100

    Testing hypothesis 2

--- cms/settings/functional_test.py
+++ cms/settings/functional_test.py
@@ -1,9 +1,12 @@
+import os
+
 from .test import *  # noqa: F403  # pylint: disable=wildcard-import,unused-wildcard-import
 
+env = os.environ.copy()
 REDIS_URL = "redis://localhost:16379"
 
 DATABASES["default"]["PORT"] = DATABASES["read_replica"]["PORT"] = 15432  # noqa: F405
 
 SEARCH_INDEX_PUBLISHER_BACKEND = "kafka"
 
-# KAFKA_SERVERS = ["localhost:19094"]
+KAFKA_SERVERS = ["localhost:19094"]
