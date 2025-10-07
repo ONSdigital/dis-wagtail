d535a3a5d8fec4f4f110a1e07c36e8c9de4a9d5b
AdamHawtin <adam.hawtin@ons.gov.uk>
2025-10-07 14:00:38 +0100

    Fix type hint

--- cms/standard_pages/utils.py
+++ cms/standard_pages/utils.py
@@ -4,8 +4,8 @@ from wagtail.models import Locale
 from cms.standard_pages.models import CookiesPage
 
 
-def get_cookies_page_url(language_code: str) -> str:
-    if language_code not in (supported_lang[0] for supported_lang in settings.LANGUAGES):
+def get_cookies_page_url(language_code: str | None) -> str:
+    if language_code not in {supported_lang[0] for supported_lang in settings.LANGUAGES}:
         # Fallback to the default english language code if the provided language code is not supported
         language_code = settings.LANGUAGE_CODE
     # Cookies pages must exist for supported languages
