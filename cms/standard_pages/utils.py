b0fd91f8b0a673d7c8bc4cc25cb2a2957fa02136
AdamHawtin <adam.hawtin@ons.gov.uk>
2025-10-08 08:35:32 +0100

    Revert accidental space change, add to comment

--- cms/standard_pages/utils.py
+++ cms/standard_pages/utils.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from wagtail.models import Locale
 
 from cms.standard_pages.models import CookiesPage
 
@@ -9,5 +8,5 @@ def get_cookies_page_url(language_code: str) -> str | None:
         # Fallback to the default english language code if the provided language code is not supported
         language_code = settings.LANGUAGE_CODE
     # Cookies pages must exist for supported languages
-    cookies_page = CookiesPage.objects.get(locale=Locale.objects.get(language_code=language_code))
+    cookies_page = CookiesPage.objects.get(locale__language_code=language_code)
     return cookies_page.get_url()
