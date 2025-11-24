8ed91e7599067806a9db5f6b89c9fd17702f5091
Maciej Baron <MaciekBaron@users.noreply.github.com>
2025-11-24 16:51:18 +0000

    Change __eq__ for enum

--- cms/bundles/enums.py
+++ cms/bundles/enums.py
@@ -16,6 +16,11 @@ class BundleContentItemState(StrEnum):
     APPROVED = "APPROVED"
     PUBLISHED = "PUBLISHED"
 
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, str):
+            return self.value.lower() == other.lower()
+        return super().__eq__(other)
+
 
 ACTIVE_BUNDLE_STATUSES = [BundleStatus.DRAFT, BundleStatus.IN_REVIEW, BundleStatus.APPROVED]
 ACTIVE_BUNDLE_STATUS_CHOICES = [
