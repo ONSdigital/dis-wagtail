92217d54c653f8a2ec17046be1478047765209f6
AdamHawtin <adam.hawtin@ons.gov.uk>
2025-12-08 14:39:05 +0000

    Add before unpublish hook to prevent unpublish of articles with currently referenced figures

--- cms/articles/wagtail_hooks.py
+++ cms/articles/wagtail_hooks.py
@@ -31,7 +31,7 @@ def before_create_page(
 @hooks.register("before_delete_page")
 def before_delete_page(request: "HttpRequest", page: "Page") -> HttpResponseRedirect | None:
     if request.method == "POST":
-        if page.specific_class == StatisticalArticlePage and page.specific.figures_used_by_ancestor:
+        if page.specific_class == StatisticalArticlePage and page.specific.figures_in_use_with_no_fallback:
             messages.warning(
                 request,
                 "This page cannot be deleted because it contains headline figures that are referenced elsewhere.",
@@ -42,7 +42,7 @@ def before_delete_page(request: "HttpRequest", page: "Page") -> HttpResponseRedi
         if (
             page.specific_class == ArticleSeriesPage
             and (latest := page.get_latest())
-            and latest.figures_used_by_ancestor
+            and latest.has_figures_in_use_by_ancestor
         ):
             message = (
                 "This page cannot be deleted because one or more of its children contain headline figures"
@@ -50,6 +50,32 @@ def before_delete_page(request: "HttpRequest", page: "Page") -> HttpResponseRedi
             )
             messages.warning(request, message)
             return redirect("wagtailadmin_pages:delete", page.pk)
+
+    return None
+
+
+@hooks.register("before_unpublish_page")
+def before_unpublish_page(request: "HttpRequest", page: "Page") -> HttpResponseRedirect | None:
+    if request.method == "POST":
+        if page.specific_class == StatisticalArticlePage and page.specific.figures_in_use_with_no_fallback:
+            messages.warning(
+                request,
+                "This page cannot be unpublished because it contains headline figures that are referenced elsewhere.",
+            )
+            # Redirect to the unpublish page (the same page) to prevent unpublish action
+            # See: https://docs.wagtail.org/en/latest/reference/hooks.html#before-delete-page
+            return redirect("wagtailadmin_pages:unpublish", page.pk)
+        if (
+            page.specific_class == ArticleSeriesPage
+            and (latest := page.get_latest())
+            and latest.has_figures_in_use_by_ancestor
+        ):
+            message = (
+                "This page cannot be unpublished because one or more of its children contain headline figures"
+                " that are referenced elsewhere."
+            )
+            messages.warning(request, message)
+            return redirect("wagtailadmin_pages:unpublish", page.pk)
 
     return None
 
