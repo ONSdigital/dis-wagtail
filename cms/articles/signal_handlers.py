10f79dbdcf4724d6c7bafe7317dfe9d00c60d8c9
Jake Howard <jake.howard@torchbox.com>
2026-01-06 16:40:31 +0000

    Convert some Wagtail hooks into signals
    This lets the signals be sent when running non-wagtail admin codepaths.

--- cms/articles/signal_handlers.py
+++ cms/articles/signal_handlers.py
@@ -1,11 +1,13 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from wagtail.admin.signals import init_new_page
 from wagtail.models import Page
 from wagtail.signals import page_published
 
-from cms.articles.models import StatisticalArticlePage
+from cms.articles.models import ArticlesIndexPage, StatisticalArticlePage
+from cms.topics.models import TopicPage
 
 if TYPE_CHECKING:
     from wagtail.admin.views.pages.create import CreateView
@@ -50,3 +52,14 @@ def prepopulate_statistical_article(
     page.search_description = latest.search_description
     page.datasets = latest.datasets
     page.dataset_sorting = latest.dataset_sorting
+
+
+@receiver(post_save, sender=TopicPage)
+def create_article_index_page(sender: Any, instance: TopicPage, created: bool, raw: bool, **kwargs: Any) -> None:  # pylint: disable=unused-argument
+    if not created or raw:
+        return
+
+    articles_index = ArticlesIndexPage(title="Articles")
+    instance.add_child(instance=articles_index)
+    # We publish a live version for the methodologies index page. This is acceptable since its URL redirects
+    articles_index.save_revision().publish()
