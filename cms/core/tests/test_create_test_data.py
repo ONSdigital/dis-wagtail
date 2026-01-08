3fe84c534c92d93536343fe52a9cccd4c8179840
Jake Howard <jake.howard@torchbox.com>
2026-01-08 17:03:59 +0000

    Improve assertions for modified objects

--- cms/core/tests/test_create_test_data.py
+++ cms/core/tests/test_create_test_data.py
@@ -4,14 +4,26 @@ from django.core.management import call_command
 from django.test import TestCase
 
 from cms.core.management.commands.create_test_data import SEEDED_DATA_PREFIX
+from cms.images.models import CustomImage
+from cms.taxonomy.models import Topic
 from cms.topics.models import TopicPage
+
+AFFECTED_MODELS = [TopicPage, Topic, CustomImage]
 
 
 class CreateTestDataTestCase(TestCase):
+    def test_creates_data(self) -> None:
+        original_counts = {model: model.objects.count() for model in AFFECTED_MODELS}
+
+        call_command("create_test_data", interactive=False)
+
+        for model, original_count in original_counts.items():
+            self.assertGreater(model.objects.count(), original_count, model)
+
     def test_creates_topics(self) -> None:
         call_command("create_test_data", interactive=False)
 
-        self.assertEqual(TopicPage.objects.count(), 6)
+        self.assertEqual(TopicPage.objects.count(), 3)
         self.assertEqual(len(set(TopicPage.objects.values_list("title", flat=True))), 3)
 
         for topic_page in TopicPage.objects.all():
@@ -24,10 +36,10 @@ class CreateTestDataTestCase(TestCase):
 
     def test_idempotent(self) -> None:
         call_command("create_test_data", interactive=False)
-        self.assertEqual(TopicPage.objects.count(), 6)
+        self.assertEqual(TopicPage.objects.count(), 3)
 
         call_command("create_test_data", interactive=False)
-        self.assertEqual(TopicPage.objects.count(), 6)
+        self.assertEqual(TopicPage.objects.count(), 3)
 
     def test_tree_is_valid(self) -> None:
         call_command("create_test_data", interactive=False)
@@ -46,29 +58,30 @@ class DeleteTestDataTestCase(TestCase):
         self.assertIn("No data to delete", output.getvalue())
 
     def test_dry_run(self) -> None:
-        original_topic_page_count = TopicPage.objects.count()
-
         call_command("create_test_data", interactive=False)
-        self.assertGreater(TopicPage.objects.count(), original_topic_page_count)
+
+        original_counts = {model: model.objects.count() for model in AFFECTED_MODELS}
 
         output = StringIO()
         call_command("delete_test_data", stdout=output, no_color=True, dry_run=True)
         self.assertIn("Found data to delete", output.getvalue())
         self.assertNotIn("Successfully deleted", output.getvalue())
 
-        self.assertGreater(TopicPage.objects.count(), original_topic_page_count)
+        for model, original_count in original_counts.items():
+            self.assertEqual(model.objects.count(), original_count, model)
 
     def test_delete_data(self) -> None:
-        original_topic_page_count = TopicPage.objects.count()
-
         call_command("create_test_data", interactive=False)
-        self.assertGreater(TopicPage.objects.count(), original_topic_page_count)
+
+        original_counts = {model: model.objects.count() for model in AFFECTED_MODELS}
 
         output = StringIO()
         call_command("delete_test_data", stdout=output, no_color=True, interactive=False)
         self.assertIn("Found data to delete", output.getvalue())
         self.assertIn("Successfully deleted", output.getvalue())
-        self.assertEqual(TopicPage.objects.count(), original_topic_page_count)
+
+        for model, original_count in original_counts.items():
+            self.assertLess(model.objects.count(), original_count, model)
 
     def test_tree_is_valid(self) -> None:
         call_command("create_test_data", interactive=False)
