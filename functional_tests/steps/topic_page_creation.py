fdf59cabf704e76a1d7a27e41b4d7c5bb5e9d68d
Maciej Baron <MaciekBaron@users.noreply.github.com>
2026-01-19 14:20:24 +0000

    Placace ruff

--- functional_tests/steps/topic_page_creation.py
+++ functional_tests/steps/topic_page_creation.py
@@ -1,6 +1,7 @@
+# pylint: disable=not-callable
 """Step definitions for topic page creation and configuration tests."""
 
-from behave import then, when  # pylint: disable=no-name-in-module
+from behave import then, when
 from behave.runner import Context
 from playwright.sync_api import expect
 
