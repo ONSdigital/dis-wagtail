66e7e531cdd363ca5dadb9a5523954c80cbf02e4
Maciej Baron <MaciekBaron@users.noreply.github.com>
2026-01-19 16:34:50 +0000

    Adjust generated GTM values

--- cms/topics/tests/test_models.py
+++ cms/topics/tests/test_models.py
@@ -735,7 +735,7 @@ class TopicPageTestCase(WagtailTestUtils, TestCase):
         self.assertEqual(analytics_values["pageTitle"], self.topic_page.title)
         self.assertEqual(analytics_values["contentType"], self.topic_page.analytics_content_type)
         self.assertEqual(analytics_values["contentGroup"], self.topic_page.analytics_content_group)
-        self.assertEqual(analytics_values["contentTheme"], self.topic_page.analytics_content_theme)
+        self.assertEqual(analytics_values["theme"], self.topic_page.analytics_content_theme)
 
 
 class TopicPageRelatedArticleValidationTests(TestCase):
