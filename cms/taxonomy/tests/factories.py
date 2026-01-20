f9250e6214f1653edf6f6e101f205b9f989bfd73
Jake Howard <jake.howard@torchbox.com>
2026-01-20 16:45:29 +0000

    Restore cursed TopicFactory
    This factory is definitely configured incorrectly, but something about the model, dependent tests or factory itself
    requires that it work this way.

--- cms/taxonomy/tests/factories.py
+++ cms/taxonomy/tests/factories.py
@@ -1,4 +1,4 @@
-from factory import BUILD_STRATEGY, faker, post_generation
+import factory
 from wagtail_factories.factories import MP_NodeFactory
 
 from cms.taxonomy.models import Topic
@@ -9,19 +9,24 @@ class SimpleTopicFactory(MP_NodeFactory):
         model = Topic
         django_get_or_create = ("id", "parent")
 
-    id: str = faker.Faker("random_number", digits=50, fix_len=True)  # Use a very long ID for uniqueness
-    slug: str = faker.Faker("slug")
-    title: str = faker.Faker("sentence", nb_words=3)
-    description: str = faker.Faker("sentence", nb_words=10)
+    parent = factory.LazyFunction(Topic.objects.root_topic)
+    id = factory.Faker("random_number", digits=50, fix_len=True)  # Use a very long ID for uniqueness
+    slug = factory.Faker("slug")
+    title = factory.Faker("sentence", nb_words=3)
+    description = factory.Faker("sentence", nb_words=10)
 
 
-class TopicFactory(SimpleTopicFactory):
+class TopicFactory(factory.Factory):
     class Meta:
         model = Topic
-        strategy = BUILD_STRATEGY  # To prevent Factory from trying to save the node
-        django_get_or_create = ("id", "parent")
+        strategy = factory.enums.BUILD_STRATEGY  # To prevent Factory from trying to save the node
 
-    @post_generation
+    id = factory.Faker("random_number", digits=50, fix_len=True)  # Use a very long ID for uniqueness
+    slug = factory.Faker("slug")
+    title = factory.Faker("sentence", nb_words=3)
+    description = factory.Faker("sentence", nb_words=10)
+
+    @factory.post_generation
     def save_topic_in_tree(obj: Topic, *_args, **_kwargs) -> None:
         """New topics need to be saved with save_topic, so we use the BUILD_STRATEGY to skip the automatic save,
         and instead save the obj ourselves in this post generation hook.
