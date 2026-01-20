895ffdac7143f2417d8fdc626d46ea8e04b08271
sanjeevz3009 <sanjeevz1717@gmail.com>
2026-01-20 15:46:52 +0000

    Add RemovedInWagtail80Warning category to warning suppression for Wagtail tables telepath deprecation

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -13,6 +13,7 @@ import dj_database_url
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import gettext_lazy as _
 from django_jinja.builtins import DEFAULT_EXTENSIONS
+from wagtail.utils.deprecation import RemovedInWagtail80Warning
 
 from cms.core.elasticache import ElastiCacheIAMCredentialProvider
 from cms.core.jinja2 import custom_json_dumps
@@ -21,6 +22,7 @@ from cms.core.jinja2 import custom_json_dumps
 # https://github.com/overcastsoftware/wagtailtables/issues/7
 warnings.filterwarnings(
     "ignore",
+    category=RemovedInWagtail80Warning,
     module=r"wagtailtables\..*",
     message=r"wagtail\.telepath has been moved to wagtail\.admin\.telepath",
 )
