845d9d87c437b1c35b91ed52177e3747af9fd1d1
Dan Braghiș <dan.braghis@torchbox.com>
2026-01-21 17:25:15 +0000

    Update permission tester logic for can_publish/can_publish_subpage

--- cms/core/permission_testers.py
+++ cms/core/permission_testers.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from wagtail.models import PagePermissionTester
 
+from cms.bundles.utils import in_active_bundle
+from cms.workflows.utils import is_page_ready_to_publish
+
 
 class BasePagePermissionTester(PagePermissionTester):
     """Base class for page permission testers in the CMS."""
@@ -38,3 +41,37 @@ class BasePagePermissionTester(PagePermissionTester):
 
         can_lock: bool = super().can_lock()
         return can_lock
+
+    def can_publish(self) -> bool:
+        """Overrides the core can_publish to extend with ONS publishing logic.
+
+        To manually or schedule publish:
+        - the page must not be in an active bundle
+        - the page must be in the approved (i.e. in the "Ready to publish" workflow step)
+        """
+        if in_active_bundle(self.page):
+            return False
+
+        if not is_page_ready_to_publish(self.page):
+            return False
+
+        can_publish: bool = super().can_publish()
+        return can_publish
+
+    def can_publish_subpage(self) -> bool:
+        """Overrides the core can_publish_subpage to extend with ONS publishing logic.
+
+        Core description:
+        Niggly special case for creating and publishing a page in one go.
+        Differs from can_publish in that we want to be able to publish subpages of root, but not
+        to be able to publish root itself. (Also, can_publish_subpage returns false if the page
+        does not allow subpages at all.)
+        """
+        if in_active_bundle(self.page):
+            return False
+
+        if not is_page_ready_to_publish(self.page):
+            return False
+
+        can_publish_subpage: bool = super().can_publish_subpage()
+        return can_publish_subpage
