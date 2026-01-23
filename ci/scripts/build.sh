9419ab01729eb3010cb7081455776f6393f6dd36
Mebin Abraham <mebin95+work@gmail.com>
2026-01-23 16:06:46 +0000

    feat(ci): build and push image to ECR

---
+++ ci/scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+
+# These directories will exist in the Concourse build environment
+SRC_DIR="pull_request"
+DST_DIR="build"
+
+# Copy contents of pull_request into build (not the directory itself)
+cp -pR "${SRC_DIR}/." "${DST_DIR}/"
+
+# Ensure Concourse-specific Dockerfile exists in build
+cp -p "${SRC_DIR}/Dockerfile" "${DST_DIR}/Dockerfile.concourse"
