6c98becf833ea1f42f2895d6a084730a3d00e711
sanjeevz3009 <sanjeevz1717@gmail.com>
2026-01-29 00:34:27 +0000

    standard_pages/tests/test_viewsets.py StandardPagesAddViewTests added to test the Index and Information page, creation and restriction logic

---
+++ cms/standard_pages/tests/test_viewsets.py
@@ -0,0 +1,76 @@
+from django.test import TestCase
+from django.urls import reverse
+from wagtail.test.utils import WagtailTestUtils
+
+from cms.home.models import HomePage
+from cms.standard_pages.tests.factories import IndexPageFactory, InformationPageFactory
+
+
+class StandardPagesAddViewTests(WagtailTestUtils, TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.permission_denied_message = "Sorry, you do not have permission to access this area."
+
+    def setUp(self):
+        self.login()
+
+    def test_index_page_cannot_have_index_page_child(self):
+        """Home -> Index Page -> Index Page should be forbidden."""
+        index_page = IndexPageFactory()
+
+        response = self.client.get(
+            reverse("wagtailadmin_pages:add", args=("standard_pages", "indexpage", index_page.pk)),
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.permission_denied_message)
+
+    def test_information_page_cannot_have_information_page_child(self):
+        """Home -> Index Page -> Information Page -> Information Page should be forbidden."""
+        index_page = IndexPageFactory()
+        information_page = InformationPageFactory(parent=index_page)
+
+        response = self.client.get(
+            reverse("wagtailadmin_pages:add", args=("standard_pages", "informationpage", information_page.pk)),
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.permission_denied_message)
+
+    def test_index_page_can_be_added_under_home(self):
+        """Home -> Index Page should be allowed."""
+        home_page = HomePage.objects.first()
+
+        response = self.client.get(
+            reverse("wagtailadmin_pages:add", args=("standard_pages", "indexpage", home_page.pk)),
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, self.permission_denied_message)
+
+    def test_information_page_can_be_added_under_home(self):
+        """Home -> Information Page should be allowed."""
+        home_page = HomePage.objects.first()
+
+        response = self.client.get(
+            reverse("wagtailadmin_pages:add", args=("standard_pages", "informationpage", home_page.pk)),
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, self.permission_denied_message)
+
+    def test_information_page_can_be_added_under_index_page(self):
+        """Home -> Index Page -> Information Page should be allowed."""
+        index_page = IndexPageFactory()
+
+        response = self.client.get(
+            reverse("wagtailadmin_pages:add", args=("standard_pages", "informationpage", index_page.pk)),
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, self.permission_denied_message)
