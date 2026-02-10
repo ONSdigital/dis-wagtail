c89dd93d57db63b397d902aa498fb89cc28ba2d6
Jake Howard <jake.howard@torchbox.com>
2026-02-10 16:09:41 +0000

    Support early spin up for page publishing command

--- cms/bundles/management/commands/publish_scheduled_without_bundles.py
+++ cms/bundles/management/commands/publish_scheduled_without_bundles.py
@@ -1,3 +1,8 @@
+import sched
+import time
+from collections.abc import Iterable
+from datetime import datetime, timedelta
+from operator import attrgetter
 from typing import TYPE_CHECKING, Any
 
 from django.apps import apps
@@ -26,81 +31,77 @@ class Command(BaseCommand):
             default=False,
             help="Dry run -- don't change anything.",
         )
+        parser.add_argument(
+            "--include-future",
+            type=int,
+            default=None,
+            help=(
+                "Number of seconds in the future to include for publishing. "
+                "Bundles in the future will be held until their publishing time."
+            ),
+        )
 
     @force_write_db()
-    def handle(self, *args: Any, **options: dict[str, Any]) -> None:
+    def handle(self, *args: Any, **options: Any) -> None:
         dry_run = False
         if options["dry-run"]:
             self.stdout.write("Will do a dry run.")
             dry_run = True
 
-        self._unpublish_expired(dry_run)
-        self._publish_scheduled_without_bundles(dry_run)
+        max_operation_date = timezone.now()
+        if include_future := options["include_future"]:
+            max_operation_date += timedelta(seconds=include_future)
 
-    def _unpublish_expired(self, dry_run: bool) -> None:
+        # Explicitly use `time.time` so enterabs can be called with absolute timestamps.
+        scheduler = sched.scheduler(timefunc=time.time)
+
+        expired_objects = list(self._models_to_unpublish(max_operation_date))
+        revisions_for_publish = list(self._models_to_publish(max_operation_date))
+
+        self.stdout.write("\n---------------------------------")
+        self.stdout.write("Expired objects to be deactivated:")
+        if expired_objects:
+            for obj in sorted(expired_objects, key=attrgetter("expire_at")):
+                self.stdout.write(f"{obj.expire_at.isoformat()}\t{obj!r}")
+        else:
+            self.stdout.write("No expired objects to be deactivated found.")
+        self.stdout.write("\n---------------------------------")
+        self.stdout.write("Revisions to be published:")
+        if revisions_for_publish:
+            for rp in sorted(revisions_for_publish, key=attrgetter("approved_go_live_at")):
+                self.stdout.write(f"{rp.approved_go_live_at.isoformat()}\t{rp.as_object()!r}")
+        else:
+            self.stdout.write("No objects to go live.")
+
+        if not dry_run:
+            for instance in expired_objects:
+                expire_ts = instance.expire_at.timestamp()
+                scheduler.enterabs(expire_ts, 1, self._unpublish_model_action, argument=(instance,))
+
+            for rp in revisions_for_publish:
+                go_live_at_ts = rp.approved_go_live_at.timestamp()
+                scheduler.enterabs(go_live_at_ts, 1, self._publish_model_action, argument=(rp,))
+
+        # 3. Run the scheduler to run publish / unpublish content (if any)
+        scheduler.run()
+
+    def _unpublish_model_action(self, instance: DraftStateMixin) -> None:
+        instance.unpublish(set_expired=True, log_action="wagtail.unpublish.scheduled")
+
+    def _publish_model_action(self, instance: Revision) -> None:
+        instance.publish(log_action="wagtail.publish.scheduled")
+
+    def _models_to_unpublish(self, max_expire_at: datetime) -> Iterable[DraftStateMixin]:
         models = [Page]
         models += [
             model for model in apps.get_models() if issubclass(model, DraftStateMixin) and not issubclass(model, Page)
         ]
-        # 1. get all expired objects with live = True
-        expired_objects = []
         for model in models:
-            expired_objects += [model.objects.filter(live=True, expire_at__lt=timezone.now()).order_by("expire_at")]
-        if dry_run:
-            self.stdout.write("\n---------------------------------")
-            if expired_objects:
-                self.stdout.write("Expired objects to be deactivated:")
-                self.stdout.write("Expiry datetime\t\tModel\t\tSlug\t\tName")
-                self.stdout.write("---------------\t\t-----\t\t----\t\t----")
-                for queryset in expired_objects:
-                    if queryset.model is Page:
-                        for obj in queryset:
-                            self.stdout.write(
-                                f"{obj.expire_at.strftime('%Y-%m-%d %H:%M')}\t"
-                                f"{obj.specific_class.__name__}\t{obj.slug}\t{obj.title}"
-                            )
-                    else:
-                        for obj in queryset:
-                            self.stdout.write(
-                                f"{obj.expire_at.strftime('%Y-%m-%d %H:%M')}\t{queryset.model.__name__}\t\t\t{obj!s}"
-                            )
-            else:
-                self.stdout.write("No expired objects to be deactivated found.")
-        else:
-            # Unpublish the expired objects
-            for queryset in expired_objects:
-                # Cast to list to make sure the query is fully evaluated
-                # before unpublishing anything
-                for obj in list(queryset):
-                    obj.unpublish(set_expired=True, log_action="wagtail.unpublish.scheduled")
+            yield from model.objects.filter(live=True, expire_at__lt=max_expire_at)
 
-    def _publish_scheduled_without_bundles(self, dry_run: bool) -> None:
-        # 2. get all revisions that need to be published
-        preliminary_revs_for_publishing = Revision.objects.filter(approved_go_live_at__lt=timezone.now()).order_by(
-            "approved_go_live_at"
-        )
-        revs_for_publishing = []
+    def _models_to_publish(self, max_approved_go_live_at: datetime) -> Iterable[Revision]:
+        preliminary_revs_for_publishing = Revision.objects.filter(approved_go_live_at__lt=max_approved_go_live_at)
         for rev in preliminary_revs_for_publishing:
             content_object = rev.as_object()
             if not isinstance(content_object, BundledPageMixin) or not content_object.in_active_bundle:
-                revs_for_publishing.append(rev)
-        if dry_run:
-            self.stdout.write("\n---------------------------------")
-            if revs_for_publishing:
-                self.stdout.write("Revisions to be published:")
-                self.stdout.write("Go live datetime\tModel\t\tSlug\t\tName")
-                self.stdout.write("----------------\t-----\t\t----\t\t----")
-                for rp in revs_for_publishing:
-                    model = rp.content_type.model_class()
-                    rev_data = rp.content
-                    self.stdout.write(
-                        f"{rp.approved_go_live_at.strftime('%Y-%m-%d %H:%M')}\t"
-                        f"{model.__name__}\t{rev_data.get('slug', '')}\t\t{rev_data.get('title', rp.object_str)}"
-                    )
-            else:
-                self.stdout.write("No objects to go live.")
-        else:
-            for rp in revs_for_publishing:
-                # just run publish for the revision -- since the approved go
-                # live datetime is before now it will make the object live
-                rp.publish(log_action="wagtail.publish.scheduled")
+                yield rev
