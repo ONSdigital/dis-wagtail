415966b211c170bc6121632f7a44364c770413dd
Dan Braghiș <dan.braghis@torchbox.com>
2026-02-11 09:23:29 +0000

    fixup! Functional tests, final, final 2

--- functional_tests/steps/methodology_page.py
+++ functional_tests/steps/methodology_page.py
@@ -14,6 +14,7 @@ def user_creates_methodology_page(context: Context) -> None:
     context.page.get_by_role("link", name=f"View child pages of '{context.topic_page.title}'").click()
     context.page.get_by_role("link", name=methodology_index.title, exact=True).click()
     context.page.get_by_role("link", name="Add child page", exact=True).click()
+    context.page.wait_for_timeout(500)
 
 
 @step("the user populates the methodology page")
@@ -106,25 +107,15 @@ def validation_error_displayed_when_incorrect_date_selected(context: Context) ->
 def mandatory_fields_raise_validation_error_when_not_set(context: Context) -> None:
     expect(context.page.get_by_text("The page could not be created due to validation errors")).to_be_visible()
 
-    expect(
-        context.page.locator("#panel-child-content-child-title-errors .error-message").get_by_text(
-            "This field is required"
-        )
-    ).to_be_visible()
+    # expect(context.page.get_by_text("This field is required")).to_be_visible()
 
-    expect(
-        context.page.locator("#panel-child-content-child-summary-errors .error-message").get_by_text(
-            "This field is required"
-        )
-    ).to_be_visible()
-
-    expect(
-        context.page.locator(
-            "#panel-child-content-child-metadata-child-panel-child-publication_date-errors .error-message"
-        ).get_by_text("This field is required")
-    ).to_be_visible()
-
-    expect(context.page.locator(".help-block.help-critical").get_by_text("This field is required")).to_be_visible()
+    for locator in [
+        "#panel-child-content-title-content .error-message",
+        "#panel-child-content-summary-content .error-message",
+        "#panel-child-content-child-metadata-child-panel-child-publication_date-errors .error-message",
+        ".help-block.help-critical",
+    ]:
+        expect(context.page.locator(locator).get_by_text("This field is required")).to_be_visible()
 
 
 @then("the preview of the methodology page is displayed with the populated data")
