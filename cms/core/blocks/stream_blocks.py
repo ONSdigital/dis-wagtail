3dbbf3eba8d6bbe5a1bda5cdaa6d56073016a99f
Sanjeev Srithevan <41544303+sanjeevz3009@users.noreply.github.com>
2026-02-11 14:16:05 +0000

    Enhance Index & Information Pages (#475)
    * CoreSectionContentBlock (StreamBlock) and CoreSectionBlock(StructBlock created.
    - CoreStoryBlock updated
    - ONS Hero and Breadcrumbs added to index_page template
    - Related links removed from index_page template
    - InformationPage model updated to remove last_updated field, nested information pages, related pages.
    - IndexPage model updated to remove content, related_links fields and featured_items
    
    * As CoreSectionContentBlock StreamBlock is under a Section Heading Block now, removed/ turned off the automatic addition of a heading for the RelatedLinksBlock.
    
    * Additional content types added so it can be utilised within the RelatedLinksBlock and ToC added to the info page.
    
    * Sort child pages by title in IndexPage model and remove unused metadata retrieval.
    
    * Migrations file generated/ written to accommodate the update CoreStoryBlock and removal of all information and index pages and tests updated to reflect this.
    
    * information_page.feature and information_page.py UI test files updated to reflect the latest changes on the PR.
    
    * information_page template updated to include a placeholder when the page has no live version, InformationPage model ToC has the gtm attributes adding enabled, de-duplication UI test added to test the de-duplication functionality (currently not working as expected/ regressed).
    
    * core/tests/test_permission_testers.py file updated to reflect the change where info page cannot be added under home and a test change changed in standard_pages/tests/test_viewsets.py to reflect the change also.
    
    * Feedback from business for now, this info page type does not need Save or print this page button. We can re-visit in the future if we need to add this back.

--- cms/core/blocks/stream_blocks.py
+++ cms/core/blocks/stream_blocks.py
@@ -1,23 +1,8 @@
-from typing import TYPE_CHECKING, ClassVar
+from typing import TYPE_CHECKING
 
-from wagtail.blocks import RichTextBlock, StreamBlock
+from wagtail.blocks import StreamBlock
 
-from cms.core.blocks import (
-    AccordionBlock,
-    AnnouncementPanelBlock,
-    DocumentsBlock,
-    HeadingBlock,
-    InformationPanelBlock,
-    ONSTableBlock,
-    QuoteBlock,
-    RelatedLinksBlock,
-    VideoEmbedBlock,
-    WarningPanelBlock,
-)
-from cms.core.blocks.embeddable import ImageBlock
-from cms.core.blocks.equation import EquationBlock
 from cms.core.blocks.section_blocks import SectionBlock
-from cms.datavis.blocks.charts import IframeBlock
 
 if TYPE_CHECKING:
     from wagtail.blocks import StreamValue
@@ -34,30 +19,3 @@ class SectionStoryBlock(StreamBlock):
     def has_equations(self, value: StreamValue) -> bool:
         """Checks if there are any equation blocks."""
         return any(block.value["content"].first_block_by_name(block_name="equation") is not None for block in value)
-
-
-class CoreStoryBlock(StreamBlock):
-    """The core StreamField block definition."""
-
-    heading = HeadingBlock()
-    rich_text = RichTextBlock()
-    quote = QuoteBlock()
-    warning_panel = WarningPanelBlock()
-    information_panel = InformationPanelBlock()
-    announcement_panel = AnnouncementPanelBlock()
-    accordion = AccordionBlock()
-    video_embed = VideoEmbedBlock(group="Media")
-    image = ImageBlock(group="Media")
-    documents = DocumentsBlock(group="Media")
-    related_links = RelatedLinksBlock(add_heading=True)  # Add a heading as this is outside of a section block
-    table = ONSTableBlock(group="DataVis", allow_links=True)
-    equation = EquationBlock(group="DataVis", icon="decimal")
-    iframe_visualisation = IframeBlock(group="DataVis", label="Iframe Visualisation")
-
-    class Meta:
-        block_counts: ClassVar[dict[str, dict]] = {"related_links": {"max_num": 1}}
-        template = "templates/components/streamfield/stream_block.html"
-
-    def has_equations(self, value: StreamValue) -> bool:
-        """Checks if there are any equation blocks."""
-        return value.first_block_by_name(block_name="equation") is not None
