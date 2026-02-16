6b630ffd58c7f9398baf68421fba4390de462c45
Dan Braghiș <dan.braghis@torchbox.com>
2026-02-16 14:25:29 +0000

    Add page publish/unpublish purging rules

--- cms/frontend_cache/cache.py
+++ cms/frontend_cache/cache.py
@@ -1,11 +1,27 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from django.conf import settings
-from wagtail.contrib.frontend_cache.utils import _get_page_cached_urls, purge_url_from_cache
+from wagtail.contrib.frontend_cache.utils import purge_url_from_cache, purge_urls_from_cache
 from wagtail.models import Page, ReferenceIndex, Site
+
+from cms.articles.models import StatisticalArticlePage
+from cms.methodology.models import MethodologyPage
+from cms.standard_pages.models import InformationPage
+from cms.topics.models import TopicPage
 
 if TYPE_CHECKING:
     from django.db.models import Model
+
+
+def get_page_cached_urls(page: Page, cache_object: Any | None = None) -> list[str]:
+    """This is a modified version of core's get_page_cached_urls taking into account our no-trailing slash setup."""
+    if (page_url := page.get_full_url(request=cache_object)) is None:
+        # nothing to be done if the page has no routable URL
+        return []
+
+    return [
+        f"{page_url.rstrip('/')}/{path.lstrip('/')}".rstrip("/") for path in page.specific_deferred.get_cached_paths()
+    ]
 
 
 def purge_cache_on_all_sites(path: str) -> None:
@@ -43,18 +59,64 @@ def get_urls_featuring_objects(objects: list[Model]) -> set[str]:
 
     object_ids = [int(obj.pk) for obj in objects]
 
+    if isinstance(objects[0], Page):
+        to_content_type_model = "page"
+        to_content_type_app_label = "wagtailcore"
+    else:
+        to_content_type_model = objects[0]._meta.model_name.lower()  # type: ignore[union-attr]
+        to_content_type_app_label = objects[0]._meta.app_label.lower()
+
     page_ids = [
         int(val)
         for val in ReferenceIndex.objects.filter(
             base_content_type__model="page",
             base_content_type__app_label="wagtailcore",
-            to_content_type__model=objects[0]._meta.model_name.lower(),  # type: ignore[union-attr]
-            to_content_type__app_label=objects[0]._meta.app_label.lower(),
+            to_content_type__model=to_content_type_model,
+            to_content_type__app_label=to_content_type_app_label,
             to_object_id__in=object_ids,
         ).values_list("object_id", flat=True)
     ]
 
     for page in Page.objects.filter(id__in=page_ids).specific(defer=True).live().iterator():
-        urls.update(_get_page_cached_urls(page))
+        urls.update(get_page_cached_urls(page))
 
     return urls
+
+
+def get_related_topic_page_urls(page: Page) -> set[str]:
+    parent_topic = TopicPage.objects.ancestor_of(page).first().specific_deferred
+    urls = set(get_page_cached_urls(parent_topic))
+
+    related_topic_pages = TopicPage.objects.filter(topic__in=page.topic_ids).exclude(pk=parent_topic.pk).live().defer()
+
+    # include parent topic translation aliases
+    for parent_topic_alias in parent_topic.get_translations().filter(alias_of__isnull=False).specific(defer=True):
+        urls.update(get_page_cached_urls(parent_topic_alias))
+        related_topic_pages = related_topic_pages.exclude(pk=parent_topic_alias.pk)
+
+    for topic_page in related_topic_pages:
+        urls.update(get_page_cached_urls(topic_page))
+
+    return urls
+
+
+def purge_page_from_frontend_cache(page: Page) -> None:
+    # get the page urls
+    urls = set(get_page_cached_urls(page))
+
+    # include translation aliases urls
+    for translation in page.get_translations().filter(alias_of__isnull=False).specific(defer=True):
+        urls.update(get_page_cached_urls(translation))
+
+    # expand to custom logic
+    if isinstance(page, StatisticalArticlePage):
+        series_page = page.get_parent().specific_deferred
+        urls.update(get_page_cached_urls(series_page))
+        urls.update(get_related_topic_page_urls(series_page))
+    elif isinstance(page, MethodologyPage):
+        urls.update(get_related_topic_page_urls(page))
+    elif isinstance(page, InformationPage):
+        urls.update(get_page_cached_urls(page.get_parent().specific_deferred))
+
+    urls.update(get_urls_featuring_object(page))
+    purge_urls_from_cache(urls)
--- cms/frontend_cache/signal_handlers.py
+++ cms/frontend_cache/signal_handlers.py
@@ -1,1 +1,49 @@
-def register_signal_handlers(): ...
+from typing import TYPE_CHECKING, Any
+
+from django.apps import apps
+from wagtail.signals import page_published, page_unpublished
+
+if TYPE_CHECKING:
+    from wagtail.models import Page
+
+
+def page_published_signal_handler(instance: Page, **kwargs: Any) -> None:
+    from .cache import purge_page_from_frontend_cache  # pylint: disable=import-outside-toplevel
+
+    purge_page_from_frontend_cache(instance)
+
+
+def page_unpublished_signal_handler(instance: Page, **kwargs: Any) -> None:
+    from .cache import purge_page_from_frontend_cache  # pylint: disable=import-outside-toplevel
+
+    purge_page_from_frontend_cache(instance)
+
+
+def _get_indexed_page_models() -> set[Page]:
+    # inlined to allow app instantiation
+    from cms.core.models import BasePage  # pylint: disable=import-outside-toplevel
+
+    # pylint: disable=invalid-name
+    ArticlesIndexPage = apps.get_model("articles", "ArticlesIndexPage")
+    HomePage = apps.get_model("home", "HomePage")
+    MethodologyIndexPage = apps.get_model("methodology", "MethodologyIndexPage")
+    ReleaseCalendarIndex = apps.get_model("release_calendar", "ReleaseCalendarIndex")
+    # pylint: enable=invalid-name
+
+    # TODO: remove HomePage from the list when serving it from Wagtail
+    # The Release Calendar, articles and methodology indexes are served by the Search service and has a short TTL
+    excluded_types = {ArticlesIndexPage, HomePage, MethodologyIndexPage, ReleaseCalendarIndex}
+    return {model for model in apps.get_models() if issubclass(model, BasePage) and model not in excluded_types}
+
+
+def register_signal_handlers() -> None:
+    for model in _get_indexed_page_models():
+        page_published.connect(page_published_signal_handler, sender=model)
+        page_unpublished.connect(page_unpublished_signal_handler, sender=model)
+
+    # TODO
+    # - Main Menu on publish, if in nav
+    # - Footer menu on publish, if in nav
+    # - Nav save, if there are changes
+    # - Contact Details on publish, all linked published pages
+    # - Definitions on publish, all linked published pages
