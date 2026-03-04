e6db7f7efbb6fdf55a25c4cc06e163bb38fb6a5a
Dan Braghiș <dan.braghis@torchbox.com>
2026-03-04 11:52:33 +0000

    Revert 8823c4e0f5efc73eee4e05664dd91d6ea71078ea

--- cms/documents/forms.py
+++ cms/documents/forms.py
@@ -3,7 +3,6 @@ from typing import Any
 from django import forms
 from django.conf import settings
 from django.core.exceptions import ValidationError
-from django.core.validators import FileExtensionValidator
 from django.template.defaultfilters import filesizeformat
 from wagtail.documents.forms import BaseDocumentForm
 
@@ -31,21 +30,7 @@ class ONSDocumentForm(BaseDocumentForm):
 
     def clean_file(self) -> Any:
         file = self.cleaned_data.get("file")
-        if not file:
-            return file
-
-        if file.size > settings.DOCUMENTS_MAX_UPLOAD_SIZE:
+        if file and file.size > settings.DOCUMENTS_MAX_UPLOAD_SIZE:
             max_upload_size_text = filesizeformat(settings.DOCUMENTS_MAX_UPLOAD_SIZE)
             raise ValidationError(f"File size must be less than {max_upload_size_text}.")
-
-        # Validate file extension
-        # TODO: revisit when https://github.com/wagtail/wagtail/issues/13973 and related are fixed
-        supported_formats_text = ", ".join(settings.WAGTAILDOCS_EXTENSIONS).upper()
-        validate = FileExtensionValidator(
-            allowed_extensions=settings.WAGTAILDOCS_EXTENSIONS,
-            message=f"Not a supported document format. Supported formats: {supported_formats_text}.",
-            code="invalid_document_extension",
-        )
-        validate(file)
-
         return file
