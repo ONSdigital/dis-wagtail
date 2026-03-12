9277bc338d5a09b56776d686ce26dc0946eee4a6
Dan Braghiș <dan.braghis@torchbox.com>
2026-03-12 12:24:09 +0000

    Remove the restart workflow action and relabel the resubmit action label

--- cms/workflows/action_menu.py
+++ cms/workflows/action_menu.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from wagtail.admin.action_menu import ActionMenuItem
+from wagtail.admin.action_menu import SubmitForModerationMenuItem as CoreSubmitForModerationMenuItem
 
 
 class UnlockWorkflowMenuItem(ActionMenuItem):
@@ -16,3 +17,14 @@ class UnlockWorkflowMenuItem(ActionMenuItem):
 
     def get_url(self, parent_context: Any) -> str:
         return self.item_url
+
+
+class SubmitForModerationMenuItem(CoreSubmitForModerationMenuItem):
+    def get_context_data(self, parent_context: dict) -> dict:
+        context = super().get_context_data(parent_context)
+
+        # update the resubmit label so it doesn't include the workflow name.
+        if context["label"].startswith("Resubmit to"):
+            context["label"] = "Resubmit for review"
+
+        return context
