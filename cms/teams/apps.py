35d5f25b6f618cd9d2795a2fb88b88f7c3f4b5f3
Maciej Baron <MaciekBaron@users.noreply.github.com>
2026-03-17 17:03:14 +0000

    Redirect Wagtail “Preview teams” admin pages to Florence

--- cms/teams/apps.py
+++ cms/teams/apps.py
@@ -6,3 +6,6 @@ class TeamsConfig(AppConfig):
 
     default_auto_field = "django.db.models.AutoField"
     name = "cms.teams"
+
+    def ready(self) -> None:
+        import cms.teams.checks  # noqa: F401  # pylint: disable=unused-import, import-outside-toplevel
