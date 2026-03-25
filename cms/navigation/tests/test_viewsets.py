e24e3b5d25eefdc42d036c7e47553c699026d0b5
sanjeevz3009 <sanjeevz1717@gmail.com>
2026-03-25 11:36:23 +0000

    Refactor menu locale retrieval to return lists instead of sets in tests to guard future bugs where the migration might accidentally create duplicate English menus etc.

--- cms/navigation/tests/test_viewsets.py
+++ cms/navigation/tests/test_viewsets.py
@@ -19,13 +19,13 @@ class MainMenuViewSetTestCase(WagtailTestUtils, TestCase):
         self.client.force_login(self.superuser)
 
     def get_main_menu_locales(self):
-        return set(MainMenu.objects.values_list("locale__language_code", flat=True))
+        return list(MainMenu.objects.values_list("locale__language_code", flat=True))
 
     def test_main_menu_exists_from_migrations(self):
         """MainMenu should exist because migrations create the default instance and its translation."""
         self.assertEqual(MainMenu.objects.count(), 2, "MainMenu should exist from migrations.")
-        self.assertSetEqual(
-            self.get_main_menu_locales(), {"en-gb", "cy"}, "Should have one English and one Welsh MainMenu."
+        self.assertListEqual(
+            self.get_main_menu_locales(), ["en-gb", "cy"], "Should have one English and one Welsh MainMenu."
         )
 
     def test_main_menu_add_view_can_be_accessed_when_none_exists(self):
@@ -40,8 +40,8 @@ class MainMenuViewSetTestCase(WagtailTestUtils, TestCase):
     def test_main_menu_add_redirects_to_dashboard_when_it_already_exists(self):
         """If a MainMenu already exists, the user should be redirected to the Wagtail dashboard."""
         self.assertEqual(MainMenu.objects.count(), 2, "MainMenu should exist from migrations.")
-        self.assertSetEqual(
-            self.get_main_menu_locales(), {"en-gb", "cy"}, "Should have one English and one Welsh MainMenu."
+        self.assertListEqual(
+            self.get_main_menu_locales(), ["en-gb", "cy"], "Should have one English and one Welsh MainMenu."
         )
 
         response = self.client.get(self.add_url)
@@ -65,13 +65,13 @@ class FooterMenuViewSetTestCase(WagtailTestUtils, TestCase):
         self.client.force_login(self.superuser)
 
     def get_footer_menu_locales(self):
-        return set(FooterMenu.objects.values_list("locale__language_code", flat=True))
+        return list(FooterMenu.objects.values_list("locale__language_code", flat=True))
 
     def test_footer_menu_exists_from_migrations(self):
         """FooterMenu should exist because migrations create the default instance and its translation."""
         self.assertEqual(FooterMenu.objects.count(), 2, "FooterMenu should exist from migrations.")
-        self.assertSetEqual(
-            self.get_footer_menu_locales(), {"en-gb", "cy"}, "Should have one English and one Welsh FooterMenu."
+        self.assertListEqual(
+            self.get_footer_menu_locales(), ["en-gb", "cy"], "Should have one English and one Welsh FooterMenu."
         )
 
     def test_footer_menu_add_view_can_be_accessed_when_none_exist(self):
@@ -86,8 +86,8 @@ class FooterMenuViewSetTestCase(WagtailTestUtils, TestCase):
     def test_footer_menu_add_redirects_to_dashboard_when_it_already_exists(self):
         """If a FooterMenu already exists, the user should be redirected to the Wagtail dashboard."""
         self.assertEqual(FooterMenu.objects.count(), 2, "FooterMenu should exist from migrations.")
-        self.assertSetEqual(
-            self.get_footer_menu_locales(), {"en-gb", "cy"}, "Should have one English and one Welsh FooterMenu."
+        self.assertListEqual(
+            self.get_footer_menu_locales(), ["en-gb", "cy"], "Should have one English and one Welsh FooterMenu."
         )
 
         response = self.client.get(self.add_url)
