d3a4730d28fed72afa8002b837c5100b1a5bf438
Benedict Jackson <jacksb@Administrators-MacBook-Pro.local>
2026-04-09 08:43:25 +0100

    fix typo in test fixture content and added extra fixtures for testing message orders

--- cms/locale/tests/test_makemessages.py
+++ cms/locale/tests/test_makemessages.py
@@ -21,6 +21,25 @@ msgstr ""
 
 msgid "Hello"
 msgstr "Helo"
+
+msgid "Goodbye"
+msgstr "Hwyl fawr"
+"""
+
+SAMPLE_PO_CONTENT_DIFFERENT_ORDER = """msgid ""
+msgstr ""
+"POT-Creation-Date: 2026-01-01 00:00+0000\\n"
+"PO-Revision-Date: 2026-01-01 00:00+0000\\n"
+"Language: cy\\n"
+"MIME-Version: 1.0\\n
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+
+msgid "Goodbye"
+msgstr "Hwyl fawr"
+
+msgid "Hello"
+msgstr "Helo"
 """
 
 SAMPLE_PO_CONTENT_NEW_DATE = """msgid ""
@@ -34,6 +53,9 @@ msgstr ""
 
 msgid "Hello"
 msgstr "Helo"
+
+msgid "Goodbye"
+msgstr "Hwyl fawr"
 """
 
 
@@ -50,7 +72,10 @@ msgid "Hello"
 msgstr "Helo"
 
 msgid "Goodbye"
-msgid "Hwyl fawr"
+msgstr "Hwyl fawr"
+
+msgid "Good Morning"
+msgstr "Bore da"
 """
 
 
