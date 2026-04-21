31d96a0532130e220cfe4fc87e44c543d46ba9d3
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-04-21 09:18:34 +0100

    move sample data for `makemessages_test` to own file.

---
+++ cms/locale/tests/po_fixtures.py
@@ -0,0 +1,145 @@
+SAMPLE_PO_CONTENT = """msgid ""
+msgstr ""
+"POT-Creation-Date: 2026-01-01 00:00+0000\\n"
+"PO-Revision-Date: 2026-01-01 00:00+0000\\n"
+"Language: cy\\n"
+"MIME-Version: 1.0\\n
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+
+msgid "Hello"
+msgstr "Helo"
+
+msgid "Goodbye"
+msgstr "Hwyl fawr"
+"""
+
+SAMPLE_PO_CONTENT_WITH_MSGCTXT = """msgid ""
+msgstr ""
+"POT-Creation-Date: 2026-01-01 00:00+0000\\n"
+"PO-Revision-Date: 2026-01-01 00:00+0000\\n"
+"Language: cy\\n"
+"MIME-Version: 1.0\\n
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+
+msgctxt "Button"
+msgid "Hello"
+msgstr "Helo"
+
+msgctxt "Body"
+msgid "Hello"
+msgstr "Helo"
+
+msgid "Goodbye"
+msgstr "Hwyl fawr"
+"""
+
+SAMPLE_PO_CONTENT_WITH_PLURAL = """msgid ""
+msgstr ""
+"POT-Creation-Date: 2026-01-01 00:00+0000\\n"
+"PO-Revision-Date: 2026-01-01 00:00+0000\\n"
+"Language: cy\\n"
+"MIME-Version: 1.0\\n
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+
+msgid "Hello"
+msgstr "Helo"
+
+msgid "Goodbye"
+msgstr "Hwyl fawr"
+
+msgid "You have %d file"
+msgid_plural "You have %d files"
+msgstr[0] ""
+msgstr[1] ""
+"""
+
+SAMPLE_PO_CONTENT_WITH_MULTILINE = """msgid ""
+msgstr ""
+"POT-Creation-Date: 2026-01-01 00:00+0000\\n"
+"PO-Revision-Date: 2026-01-01 00:00+0000\\n"
+"Language: cy\\n"
+"MIME-Version: 1.0\\n
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+
+msgid "Hello"
+msgstr "Helo"
+
+msgid "Goodbye"
+msgstr "Hwyl fawr"
+
+msgid ""
+"second line"
+"third line\\n\""
+msgstr ""
+"""
+
+SAMPLE_PO_CONTENT_MULTILINE = """msgid ""
+msgstr ""
+"POT-Creation-Date: 2026-01-01 00:00+0000\\n"
+"PO-Revision-Date: 2026-01-01 00:00+0000\\n"
+"Language: cy\\n"
+"MIME-Version: 1.0\\n
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+
+msgid ""
+"second line"
+"third line\\n\""
+msgstr ""
+"""
+
+SAMPLE_PO_CONTENT_DIFFERENT_ORDER = """msgid ""
+msgstr ""
+"POT-Creation-Date: 2026-01-01 00:00+0000\\n"
+"PO-Revision-Date: 2026-01-01 00:00+0000\\n"
+"Language: cy\\n"
+"MIME-Version: 1.0\\n
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+
+msgid "Goodbye"
+msgstr "Hwyl fawr"
+
+msgid "Hello"
+msgstr "Helo"
+"""
+
+SAMPLE_PO_CONTENT_NEW_DATE = """msgid ""
+msgstr ""
+"POT-Creation-Date: 2026-01-01 12:00+0000\\n"
+"PO-Revision-Date: 2026-01-01 00:00+0000\\n"
+"Language: cy\\n"
+"MIME-Version: 1.0\\n
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+
+msgid "Hello"
+msgstr "Helo"
+
+msgid "Goodbye"
+msgstr "Hwyl fawr"
+"""
+
+
+SAMPLE_PO_CONTENT_NEW_ENTRY = """msgid ""
+msgstr ""
+"POT-Creation-Date: 2026-01-01 00:00+0000\\n"
+"PO-Revision-Date: 2026-01-01 00:00+0000\\n"
+"Language: cy\\n"
+"MIME-Version: 1.0\\n
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+
+msgid "Hello"
+msgstr "Helo"
+
+msgid "Goodbye"
+msgstr "Hwyl fawr"
+
+msgid "Good Morning"
+msgstr "Bore da"
+"""
