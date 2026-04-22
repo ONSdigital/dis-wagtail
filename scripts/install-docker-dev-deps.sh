37dfed2703f1b96466018806cee0e72dde172b42
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-04-22 21:47:53 +0100

    run formatter, add shebang and executable permission to script

--- scripts/install-docker-dev-deps.sh
+++ scripts/install-docker-dev-deps.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 apt --quiet --yes update
 apt --quiet --yes install \
     git \
@@ -9,7 +11,7 @@ apt --quiet --yes install \
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 # Create NodeSource repository
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" > /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
 # Update lists again and install Node.js
 apt --quiet --yes update
 apt --quiet --yes install nodejs
