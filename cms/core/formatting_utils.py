fb7ae59a8bcf132fa6ba774fd50546082c77fc1c
Joseph-Percival-ONS <joseph.percival@ons.gov.uk>
2026-05-27 13:59:31 +0100

    Address mypy type checking issues

--- cms/core/formatting_utils.py
+++ cms/core/formatting_utils.py
@@ -8,6 +8,7 @@ from django.utils.translation import gettext_lazy as _
 from wagtail.models import Page
 
 from cms.core.custom_date_format import ons_date_format
+from cms.core.enums import RelatedContentType
 from cms.core.utils import get_related_content_type_label
 
 if TYPE_CHECKING:
@@ -40,10 +41,8 @@ def format_as_document_list_item(
 
 def _format_external_link(page_dict: ExternalArticleDict) -> DocumentListItem:
     """Format external link dictionary into DocumentListItem."""
-    if page_type := page_dict.get("content_type"):
-        content_type = get_related_content_type_label(page_type)
-    else:
-        content_type = _("Article")
+    page_type = page_dict.get("content_type", RelatedContentType.ARTICLE)
+    content_type = get_related_content_type_label(page_type)
 
     page_datum: DocumentListItem = format_as_document_list_item(
         title=page_dict["title"],
