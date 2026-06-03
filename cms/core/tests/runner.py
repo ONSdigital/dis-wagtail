ae718b83488e5495c7d565d3dd7a327730fce149
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-06-03 13:44:18 +0100

    add comment linking to django ticket

--- cms/core/tests/runner.py
+++ cms/core/tests/runner.py
@@ -8,6 +8,8 @@ class OldConnectionsCleanupDiscoveryRunner(DiscoverRunner):
 
         # Running tests with the --parallel flag was failing due to attribute errors in the django parallel test
         # runner setup. This adds a quick monkey patch that allows tests to run.
+        # Raised here, but will need to reopen or start a new ticket for followup
+        # https://code.djangoproject.com/ticket/37077
         for alias in connections:
             if not hasattr(connections[alias], "_test_serialized_contents"):
                 connections[alias]._test_serialized_contents = ""  # pylint: disable=W0212
