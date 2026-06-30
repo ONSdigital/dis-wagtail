10e41948086d7b9a31fdbc31ec23dbb9486dccf1
Jake Howard <jake.howard@torchbox.com>
2026-06-30 16:03:46 +0100

    Make revision optional for signal handler

--- cms/post_publish_actions/signal_handlers.py
+++ cms/post_publish_actions/signal_handlers.py
@@ -3,7 +3,7 @@ from contextlib import contextmanager
 from typing import Any
 
 from asgiref.local import Local
-from wagtail.models import Page, Revision
+from wagtail.models import Page
 from wagtail.signals import page_published
 
 from .utils import run_post_publish_actions_for
@@ -25,7 +25,7 @@ def suppress_post_publish_actions_signal() -> Generator[None]:
             _suppress_post_publish_actions_signal.value = previous_value
 
 
-def run_post_publish_actions_handler(sender: type[Page], instance: Page, revision: Revision, **kwargs: Any) -> None:  # pylint: disable=unused-argument
+def run_post_publish_actions_handler(sender: type[Page], instance: Page, **kwargs: Any) -> None:  # pylint: disable=unused-argument
     from cms.bundles.utils import get_active_bundle_for_page  # pylint: disable=import-outside-toplevel
 
     if getattr(_suppress_post_publish_actions_signal, "value", None):
