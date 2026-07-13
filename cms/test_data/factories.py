139a512006b4729d453574fd0ef83945824d4e30
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-07-13 16:54:31 +0100

    ensure generated images use root collection

--- cms/test_data/factories.py
+++ cms/test_data/factories.py
@@ -1,8 +1,12 @@
+import factory
+from wagtail.models import Collection
 from wagtail_factories import ImageFactory as BaseImageFactory
 
 
 class ImageFactory(BaseImageFactory):
     """A modified ImageFactory which uses title as a unique identifier."""
+
+    collection = factory.LazyFunction(Collection.get_first_root_node)  # ignore[no-untyped-call]
 
     class Meta:
         model = BaseImageFactory._meta.model
