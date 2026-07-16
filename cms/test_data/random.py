27a9e1ff350ec57fa653f380ec63f00e616a6c23
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-07-16 13:40:19 +0100

    rename faker locale helper

--- cms/test_data/random.py
+++ cms/test_data/random.py
@@ -9,7 +9,7 @@ def get_default_locale() -> str:
     return str(factory.Faker._DEFAULT_LOCALE)  # pylint: disable=protected-access
 
 
-def _set_default_locale(locale: str) -> None:
+def _set_default_faker_locale(locale: str) -> None:
     factory.Faker._DEFAULT_LOCALE = locale  # pylint: disable=protected-access
 
 
@@ -18,11 +18,11 @@ def seeded_randomness(seed: int, locale: str) -> Generator[None]:
     original_locale = get_default_locale()
     original_state = factory.random.get_random_state()  # type: ignore[no-untyped-call]
 
-    _set_default_locale(locale)
+    _set_default_faker_locale(locale)
     factory.random.reseed_random(seed)  # type: ignore[no-untyped-call]
 
     try:
         yield
     finally:
-        _set_default_locale(original_locale)
+        _set_default_faker_locale(original_locale)
         factory.random.set_random_state(original_state)  # type: ignore[no-untyped-call]
