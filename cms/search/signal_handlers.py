d3c02661a15501c74dfff34a584a2d7d8006c278
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-07-21 14:39:01 +0100

    add sleep to post publish action

--- cms/search/signal_handlers.py
+++ cms/search/signal_handlers.py
@@ -1,4 +1,5 @@
 import logging
+from time import sleep
 from typing import Any
 
 from django.conf import settings
@@ -18,6 +19,8 @@ logger = logging.getLogger(__name__)
 
 @post_publish_action(PostPublishActionType.SEARCH_UPDATED)
 def update_index_post_publish_action(page: Page, _bundle: Bundle | None) -> None:
+    # TODO: remove after testing
+    sleep(5)
     if is_indexable_page(page):
         get_publisher().publish_created_or_updated(page)
 
