b4d2f54b32f3d08f21822108a476660655425881
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-07-21 11:16:51 +0100

    remove slack response log

--- cms/core/slack.py
+++ cms/core/slack.py
@@ -60,7 +60,6 @@ def send_or_update_slack_message(
                 text=text,
                 attachments=attachments,
             )
-            logger.info("slack response status code: %s", response.status_code)
             # Return timestamp if response is valid
             if response and response.get("ts"):
                 return str(response["ts"])
