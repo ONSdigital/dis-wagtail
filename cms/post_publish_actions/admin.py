8934c1a5ef29c7807db1c28869ef8a79962ecae1
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-07-22 10:48:22 +0100

    keep post publish action admin but conditional on admin login enabled

--- cms/post_publish_actions/admin.py
+++ cms/post_publish_actions/admin.py
@@ -1,6 +1,7 @@
-# TODO: Remove after debugging
+from django.conf import settings
 from django.contrib import admin
 
 from .models import PostPublishAction
 
-admin.site.register(PostPublishAction)
+if settings.WAGTAIL_CORE_ADMIN_LOGIN_ENABLED:
+    admin.site.register(PostPublishAction)
