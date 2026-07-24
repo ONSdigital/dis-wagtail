215b075c6518a9309f8437cfaaef9d9796dbfe61
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-07-24 15:05:42 +0100

    remove sleep and admin model registrations from testing

--- cms/search/signal_handlers.py
+++ cms/search/signal_handlers.py
@@ -18,8 +18,6 @@ logger = logging.getLogger(__name__)
 
 @post_publish_action(PostPublishActionType.SEARCH_UPDATED)
 def update_index_post_publish_action(page: Page, _bundle: Bundle | None) -> None:
-    # TODO: remove after testing
-    # sleep(5)
     if is_indexable_page(page):
         get_publisher().publish_created_or_updated(page)
 
