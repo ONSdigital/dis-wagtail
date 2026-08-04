58fc9d2567e8eac03e41cdc19e36cd00661e068a
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-08-04 13:06:52 +0100

    fix `to_have_class` to `to_contain_class`

--- functional_tests/steps/statistical_article_page.py
+++ functional_tests/steps/statistical_article_page.py
@@ -734,7 +734,7 @@ def the_page_has_a_csv_download_link_for_the_chart(context: Context) -> None:
     download_details = context.page.locator('.ons-js-details[id^="figure-downloads--"]')
     # Wait for this class as it signifies the design system js has loaded, initialised and updated
     # element roles and styles
-    expect(download_details).to_have_class("ons-details--initialised")
+    expect(download_details).to_contain_class("ons-details--initialised")
     download_details.get_by_text("Download: line chart").click()
     csv_download_link = context.page.get_by_role("link", name="Download CSV")
     expect(csv_download_link).to_be_visible()
