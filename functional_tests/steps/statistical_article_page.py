3963ae33c40b8b54112e6da4c9141e05521071cd
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-08-04 09:22:48 +0100

    ensure design system is initialised, then look for csv download elements

--- functional_tests/steps/statistical_article_page.py
+++ functional_tests/steps/statistical_article_page.py
@@ -731,7 +731,9 @@ def a_statistical_article_page_with_configured_listing_image_exists(
 @then("the page has a CSV download link for the chart")
 def the_page_has_a_csv_download_link_for_the_chart(context: Context) -> None:
     """Check that the page has a CSV download link for the chart."""
-    context.page.get_by_role("button", name="Download: line chart").click()
+    download_details = context.page.locator('.ons-js-details[id^="figure-downloads--"]')
+    expect(download_details).to_have_class("ons-details--initialised")
+    download_details.get_by_text("Download: line chart").click()
     csv_download_link = context.page.get_by_role("link", name="Download CSV")
     expect(csv_download_link).to_be_visible()
 
