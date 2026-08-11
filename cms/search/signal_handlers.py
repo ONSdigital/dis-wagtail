821f458f210a6f4ee4dbb4f1d8e14059234102e2
benedict jackson <benedict.jackson@ext.ons.gov.uk>
2026-08-11 10:37:51 +0100

    handle alias search indexing during bundle publishing

--- cms/search/signal_handlers.py
+++ cms/search/signal_handlers.py
@@ -17,10 +17,27 @@ logger = logging.getLogger(__name__)
 
 
 @post_publish_action(PostPublishActionType.SEARCH_UPDATED)
-def update_index_post_publish_action(page: Page, _bundle: Bundle | None) -> None:
-    
+def update_index_post_publish_action(page: Page, bundle: Bundle | None) -> None:
+    publisher = get_publisher()
     if is_indexable_page(page):
-        get_publisher().publish_created_or_updated(page)
+        publisher.publish_created_or_updated(page)
+
+    if bundle is None:
+        # During bundle publishing wagtail's page_published signal is suppressed
+        # outside of bundle publishing it will handle descendants and aliases
+        return
+
+    # mimic update_aliases behaviour to walk alias tree
+    seen_ids: set[int] = set()
+    aliases = list(page.aliases.specific())
+    while aliases:
+        alias = aliases.pop()
+        if alias.id in seen_ids:
+            continue
+        seen_ids.add(alias.id)
+        if is_indexable_page(alias):
+            publisher.publish_created_or_updated(alias)
+        aliases.extend(alias.aliases.specific())
 
 
 @receiver(page_unpublished)
